991b5951d63b123e84fb6a945f2cdc0d62ae8cbe
Anthony Barbier <anthonyb@graphcore.ai>
2019-09-06 12:20:46 +0100

    Fixed frontend_attributes_test and port fuse_ops_test to ReportJSON
    Summary:
    Fixed frontend_attributes_test and port fuse_ops_test to ReportJSON
    
    Fixed conv_test failures and ported to use ReportJSON
    
    Fixed conv3d_test and ported tests to use ReportJSON
    
    Reviewers: #tensorflow, georgep
    
    Reviewed By: #tensorflow, georgep
    
    Subscribers: georgep
    
    Maniphest Tasks: T11115, T11116
    
    Differential Revision: https://phabricator.sourcevertex.net/D14295

--- tensorflow/compiler/plugin/poplar/tests/conv3d_test.py
+++ tensorflow/compiler/plugin/poplar/tests/conv3d_test.py
@@ -54,12 +54,8 @@ class IpuXlaConvTest(xla_test.XLATestCase):
         output = nn_ops.convolution(pa, pb, padding="SAME")
         output = nn_ops.bias_add(output, bi)
 
-      with ops.device('cpu'):
-        report = gen_ipu_ops.ipu_event_trace()
-
-      tu.configure_ipu_system()
-
-      sess.run(report)
+      report = tu.ReportJSON(self, sess)
+      report.reset()
 
       fd = {
           pa: np.zeros([1, 14, 14, 14, 16]),
@@ -69,16 +65,13 @@ class IpuXlaConvTest(xla_test.XLATestCase):
       result = sess.run(output, fd)
       self.assertAllClose(result, np.zeros([1, 14, 14, 14, 32]))
 
-      result = sess.run(report)
-
-      s = tu.extract_all_strings_from_event_trace(result)
-      cs_list = tu.get_compute_sets_from_report(s)
+      report.parse_log()
 
       ok = [
           '__seed*', 'host-exchange-local-copy-', 'Copy_',
           'convolution/convolution.*/Conv_3x3x3', 'BiasAdd/fusion/Op/Add'
       ]
-      self.assertTrue(tu.check_all_compute_sets_and_list(cs_list, ok))
+      report.assert_all_compute_sets_and_list(ok)
 
   def test3DConv8x8x8_WithBias(self):
     with self.session() as sess:
@@ -86,16 +79,14 @@ class IpuXlaConvTest(xla_test.XLATestCase):
         inp = array_ops.placeholder(np.float32, [1, 84, 84, 84, 2], name="inp")
         wei = array_ops.placeholder(np.float32, [8, 8, 8, 2, 4], name="wei")
         bia = array_ops.placeholder(np.float32, [4], name="bia")
-        output = nn_ops.conv3d(
-            inp, wei, strides=[1, 4, 4, 4, 1], padding="VALID")
+        output = nn_ops.conv3d(inp,
+                               wei,
+                               strides=[1, 4, 4, 4, 1],
+                               padding="VALID")
         output = nn_ops.bias_add(output, bia)
 
-      with ops.device('cpu'):
-        report = gen_ipu_ops.ipu_event_trace()
-
-      tu.configure_ipu_system()
-
-      sess.run(report)
+      report = tu.ReportJSON(self, sess)
+      report.reset()
 
       fd = {
           inp: np.zeros([1, 84, 84, 84, 2]),
@@ -105,17 +96,14 @@ class IpuXlaConvTest(xla_test.XLATestCase):
       result = sess.run(output, fd)
       self.assertAllClose(result, np.zeros([1, 20, 20, 20, 4]))
 
-      result = sess.run(report)
-
-      s = tu.extract_all_strings_from_event_trace(result)
-      cs_list = tu.get_compute_sets_from_report(s)
+      report.parse_log()
 
       ok = [
           '__seed*', 'host-exchange-local-copy-', 'Copy_',
           'Conv3D/convolution.*/Conv_8x8x8_stride4x4x4',
           'BiasAdd/fusion/Op/Add'
       ]
-      self.assertTrue(tu.check_all_compute_sets_and_list(cs_list, ok))
+      report.assert_all_compute_sets_and_list(ok)
 
   def test3DConv1x1x1_WithBias(self):
     with self.session() as sess:
@@ -123,16 +111,14 @@ class IpuXlaConvTest(xla_test.XLATestCase):
         inp = array_ops.placeholder(np.float32, [1, 1, 1, 1, 4], name="inp")
         wei = array_ops.placeholder(np.float32, [1, 1, 1, 4, 8], name="wei")
         bia = array_ops.placeholder(np.float32, [8], name="bia")
-        output = nn_ops.conv3d(
-            inp, wei, strides=[1, 1, 1, 1, 1], padding="VALID")
+        output = nn_ops.conv3d(inp,
+                               wei,
+                               strides=[1, 1, 1, 1, 1],
+                               padding="VALID")
         output = output + bia
 
-      with ops.device('cpu'):
-        report = gen_ipu_ops.ipu_event_trace()
-
-      tu.configure_ipu_system()
-
-      sess.run(report)
+      report = tu.ReportJSON(self, sess)
+      report.reset()
 
       fd = {
           inp: np.zeros([1, 1, 1, 1, 4]),
@@ -142,16 +128,13 @@ class IpuXlaConvTest(xla_test.XLATestCase):
       result = sess.run(output, fd)
       self.assertAllClose(result, np.zeros([1, 1, 1, 1, 8]))
 
-      result = sess.run(report)
-
-      s = tu.extract_all_strings_from_event_trace(result)
-      cs_list = tu.get_compute_sets_from_report(s)
+      report.parse_log()
 
       ok = [
           '__seed*', 'Copy_*actsRearranged', 'host-exchange-local-copy-',
           'Conv3D/convolution.*/Conv_1x1', 'add/fusion/Op/Add'
       ]
-      self.assertTrue(tu.check_all_compute_sets_and_list(cs_list, ok))
+      report.assert_all_compute_sets_and_list(ok)
 
   def test3DConvBackpropInput(self):
     with self.session() as sess:
@@ -160,15 +143,14 @@ class IpuXlaConvTest(xla_test.XLATestCase):
         fil = array_ops.placeholder(np.float32, [2, 2, 2, 3, 5], name="inp")
         bck = array_ops.placeholder(np.float32, [2, 8, 8, 8, 5], name="wei")
 
-        output = nn_ops.conv3d_backprop_input_v2(
-            ins, fil, bck, strides=[1, 1, 1, 1, 1], padding="SAME")
+        output = nn_ops.conv3d_backprop_input_v2(ins,
+                                                 fil,
+                                                 bck,
+                                                 strides=[1, 1, 1, 1, 1],
+                                                 padding="SAME")
 
-      with ops.device('cpu'):
-        report = gen_ipu_ops.ipu_event_trace()
-
-      tu.configure_ipu_system()
-
-      sess.run(report)
+      report = tu.ReportJSON(self, sess)
+      report.reset()
 
       fd = {
           fil: np.zeros([2, 2, 2, 3, 5]),
@@ -177,14 +159,14 @@ class IpuXlaConvTest(xla_test.XLATestCase):
       result = sess.run(output, fd)
       self.assertAllClose(result, np.zeros([2, 8, 8, 8, 3]))
 
-      result = sess.run(report)
+      report.parse_log()
 
-      s = tu.extract_all_strings_from_event_trace(result)
-      cs_list = tu.get_compute_sets_from_report(s)
+      ok = [
+          '__seed*', 'Conv3DBackpropInputV2/fusion/WeightTranspose*', 'Copy_*',
+          'Conv3DBackpropInputV2/fusion*/Conv_2x2x2'
+      ]
 
-      ok = ['__seed*', 'Conv3DBackpropInputV2/fusion*/Conv_2x2x2']
-
-      self.assertTrue(tu.check_all_compute_sets_and_list(cs_list, ok))
+      report.assert_all_compute_sets_and_list(ok)
 
   def test3DConvBackpropFilter(self):
     with self.session() as sess:
@@ -193,15 +175,14 @@ class IpuXlaConvTest(xla_test.XLATestCase):
         fil = constant_op.constant([2, 2, 2, 3, 5], np.int32)
         bck = array_ops.placeholder(np.float32, [2, 8, 8, 8, 5], name="wei")
 
-        output = nn_ops.conv3d_backprop_filter_v2(
-            inp, fil, bck, strides=[1, 1, 1, 1, 1], padding="SAME")
+        output = nn_ops.conv3d_backprop_filter_v2(inp,
+                                                  fil,
+                                                  bck,
+                                                  strides=[1, 1, 1, 1, 1],
+                                                  padding="SAME")
 
-      with ops.device('cpu'):
-        report = gen_ipu_ops.ipu_event_trace()
-
-      tu.configure_ipu_system()
-
-      sess.run(report)
+      report = tu.ReportJSON(self, sess)
+      report.reset()
 
       fd = {
           inp: np.zeros([2, 8, 8, 8, 3]),
@@ -210,19 +191,16 @@ class IpuXlaConvTest(xla_test.XLATestCase):
       result = sess.run(output, fd)
       self.assertAllClose(result, np.zeros([2, 2, 2, 3, 5]))
 
-      result = sess.run(report)
-
-      s = tu.extract_all_strings_from_event_trace(result)
-      cs_list = tu.get_compute_sets_from_report(s)
+      report.parse_log()
 
       ok = [
           '__seed*', 'host-exchange-local-copy-', 'Copy_',
           'Conv3DBackpropFilterV2/convolution.*/Conv_8x8x8'
       ]
-      self.assertTrue(tu.check_all_compute_sets_and_list(cs_list, ok))
+      report.assert_all_compute_sets_and_list(ok)
 
 
 if __name__ == "__main__":
-  os.environ['TF_XLA_FLAGS'] = (
-      '--tf_xla_min_cluster_size=1 ' + os.environ.get('TF_XLA_FLAGS', ''))
+  os.environ['TF_XLA_FLAGS'] = ('--tf_xla_min_cluster_size=1 ' +
+                                os.environ.get('TF_XLA_FLAGS', ''))
   googletest.main()
