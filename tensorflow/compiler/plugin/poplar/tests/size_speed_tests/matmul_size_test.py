888eefdcb1ff322f37e75c74d939a4fc234ddf52
George Grzegorz Pawelczak <grzegorzp@graphcore.ai>
2019-11-05 13:31:02 +0000

    Fix test
    Summary: Expected change in memory usage.
    
    Test Plan: matmul size test now passed.
    
    Reviewers: #tensorflow, tomm
    
    Reviewed By: tomm
    
    Subscribers: tomm
    
    Differential Revision: https://phabricator.sourcevertex.net/D17050

--- tensorflow/compiler/plugin/poplar/tests/size_speed_tests/matmul_size_test.py
+++ tensorflow/compiler/plugin/poplar/tests/size_speed_tests/matmul_size_test.py
@@ -126,7 +126,7 @@ class MatMulSizeTest(xla_test.XLATestCase):
 
       sess.run(train, feed_dict={x: data, y_: labels})
       report.parse_log()
-      report.assert_total_tile_memory(24218556)
+      report.assert_total_tile_memory(26275256)
 
 
 if __name__ == "__main__":
