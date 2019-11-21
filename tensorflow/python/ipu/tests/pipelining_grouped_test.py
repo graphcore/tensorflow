3f58a45d137a62513eb40d3fcc5e96c0565703ec
David Norman <davidn@graphcore.ai>
2019-11-21 08:15:43 +0000

    Move pipeline op creation into a Session context

--- tensorflow/python/ipu/tests/pipelining_grouped_test.py
+++ tensorflow/python/ipu/tests/pipelining_grouped_test.py
@@ -150,15 +150,16 @@ class PipeliningGroupedTest(test_util.TensorFlowTestCase):
     with ops.device('cpu'):
       c = array_ops.placeholder(np.float32, shape=[])
 
-    with ops.device("/device:IPU:0"):
-      r = ipu_compiler.compile(my_net, inputs=[c])
-
-    cfg = utils.create_ipu_config(profiling=True, profile_execution=True)
-    cfg = utils.auto_select_ipus(cfg, 4)
-    utils.configure_ipu_system(cfg)
-    utils.move_variable_initialization_to_cpu()
-
     with tu.ipu_session() as sess:
+
+      with ops.device("/device:IPU:0"):
+        r = ipu_compiler.compile(my_net, inputs=[c])
+
+      cfg = utils.create_ipu_config(profiling=True, profile_execution=True)
+      cfg = utils.auto_select_ipus(cfg, 4)
+      utils.configure_ipu_system(cfg)
+      utils.move_variable_initialization_to_cpu()
+
       sess.run(variables.global_variables_initializer())
       sess.run(infeed_queue.initializer)
       with self.assertRaisesRegex(
@@ -277,16 +278,18 @@ class PipeliningGroupedTest(test_util.TensorFlowTestCase):
     with ops.device('cpu'):
       c = array_ops.placeholder(np.float32, shape=[])
 
-    with ops.device("/device:IPU:0"):
-      r = ipu_compiler.compile(my_net, inputs=[c])
-
-    cfg = utils.create_ipu_config(profiling=True, profile_execution=True)
-    cfg = utils.auto_select_ipus(cfg, 4)
-    utils.configure_ipu_system(cfg)
-    utils.move_variable_initialization_to_cpu()
-
-    outfeed_op = outfeed_queue.dequeue()
     with tu.ipu_session() as sess:
+
+      with ops.device("/device:IPU:0"):
+        r = ipu_compiler.compile(my_net, inputs=[c])
+
+      cfg = utils.create_ipu_config(profiling=True, profile_execution=True)
+      cfg = utils.auto_select_ipus(cfg, 4)
+      utils.configure_ipu_system(cfg)
+      utils.move_variable_initialization_to_cpu()
+
+      outfeed_op = outfeed_queue.dequeue()
+
       report = tu.ReportJSON(self, sess, configure_device=False)
       sess.run(variables.global_variables_initializer())
       sess.run(infeed_queue.initializer)
@@ -344,16 +347,18 @@ class PipeliningGroupedTest(test_util.TensorFlowTestCase):
     with ops.device('cpu'):
       c = array_ops.placeholder(np.float32, shape=[])
 
-    with ops.device("/device:IPU:0"):
-      r = ipu_compiler.compile(my_net, inputs=[c])
-
-    cfg = utils.create_ipu_config(profiling=True, profile_execution=True)
-    cfg = utils.auto_select_ipus(cfg, 4)
-    utils.configure_ipu_system(cfg)
-    utils.move_variable_initialization_to_cpu()
-
-    outfeed_op = outfeed_queue.dequeue()
     with tu.ipu_session() as sess:
+
+      with ops.device("/device:IPU:0"):
+        r = ipu_compiler.compile(my_net, inputs=[c])
+
+      cfg = utils.create_ipu_config(profiling=True, profile_execution=True)
+      cfg = utils.auto_select_ipus(cfg, 4)
+      utils.configure_ipu_system(cfg)
+      utils.move_variable_initialization_to_cpu()
+
+      outfeed_op = outfeed_queue.dequeue()
+
       report = tu.ReportJSON(self, sess, configure_device=False)
       report.reset()
       sess.run(variables.global_variables_initializer())
@@ -408,16 +413,18 @@ class PipeliningGroupedTest(test_util.TensorFlowTestCase):
     with ops.device('cpu'):
       c = array_ops.placeholder(np.float32, shape=[])
 
-    with ops.device("/device:IPU:0"):
-      r = ipu_compiler.compile(my_net, inputs=[c])
-
-    cfg = utils.create_ipu_config(profiling=True, profile_execution=True)
-    cfg = utils.auto_select_ipus(cfg, 4)
-    utils.configure_ipu_system(cfg)
-    utils.move_variable_initialization_to_cpu()
-
-    outfeed_op = outfeed_queue.dequeue()
     with tu.ipu_session() as sess:
+
+      with ops.device("/device:IPU:0"):
+        r = ipu_compiler.compile(my_net, inputs=[c])
+
+      cfg = utils.create_ipu_config(profiling=True, profile_execution=True)
+      cfg = utils.auto_select_ipus(cfg, 4)
+      utils.configure_ipu_system(cfg)
+      utils.move_variable_initialization_to_cpu()
+
+      outfeed_op = outfeed_queue.dequeue()
+
       report = tu.ReportJSON(self, sess, configure_device=False)
       sess.run(variables.global_variables_initializer())
       sess.run(infeed_queue.initializer)
