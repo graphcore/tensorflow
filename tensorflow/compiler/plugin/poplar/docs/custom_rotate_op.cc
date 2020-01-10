a84393959a5efe207500dcdec4d43e689e5d71ab
David Norman <davidn@graphcore.ai>
2020-01-10 11:37:31 +0000

    Fix spelling, grammar and example
    Summary:
    some minor spelling and grammar fixes to the docs and examples.
    
    Fix copyright
    
    Test Plan: all tests ok.
    
    Reviewers: #tensorflow, georgep, jakeh
    
    Reviewed By: #tensorflow, georgep, jakeh
    
    Differential Revision: https://phabricator.sourcevertex.net/D19564

--- tensorflow/compiler/plugin/poplar/docs/custom_rotate_op.cc
+++ tensorflow/compiler/plugin/poplar/docs/custom_rotate_op.cc
@@ -1,18 +1,39 @@
+/* Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include <vector>
+
 #include <poplar/Graph.hpp>
 #include <poplar/Tensor.hpp>
 #include <poputil/Util.hpp>
 #include <poputil/VertexTemplates.hpp>
 #include <poputil/exceptions.hpp>
 
-#include <iostream>
-
-extern "C" bool IsElementWise() { return true; }
+extern "C" void Build_metadata(std::vector<std::int64_t>& allocating_indices,
+                               std::uint32_t& num_inplace, bool& is_elementwise,
+                               std::uint32_t num_inputs) {
+  allocating_indices.clear();
+  num_inplace = 0;
+  is_elementwise = true;
+}
 
 extern "C" poplar::program::Program Build(
     poplar::Graph& graph, const std::vector<poplar::Tensor>& inputs,
     std::vector<poplar::Tensor>& outputs, const std::string& debugPrefix) {
   if (inputs.size() != 3) {
-    throw poputil::poplibs_error("Rotate requires 2 inputs");
+    throw poputil::poplibs_error("Rotate requires 3 inputs");
   }
 
   if (inputs[0].numElements() == 0) {
