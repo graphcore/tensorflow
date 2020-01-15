dd3ea4c744720b817cc0900e10a31652af4389f7
Håkon Sandsmark <hakons@graphcore.ai>
2020-01-15 13:58:16 +0100

    reduce_scatter: New custom popops op
    Summary:
    The idea is that this can be used to optimize the hierarchical
    reductions for distributed training.
    
    Then we can do reduce_scatter() between the replicas over the IPU
    links, send the result from each replica to the host, and concatenate
    (gather) the results there to form the per-worker reduced result. This
    is then passed to an inter-worker allreduce routine to get the final
    global result.
    
    This has some benefits compared to doing cross_replica_sum()
    and sending the result from one of the IPUs to the host (which
    is what we currently do):
    
    * reduce_scatter() is less work than cross_replica_sum()
    * Each replica only needs to transfer ceil(num_elements / num_replicas)
      elements to the host (and they can be done in parallel)
    
    Fixes T14756.
    
    Test Plan: New replicated test. Existing tests pass.
    
    Reviewers: #tensorflow, jakeh
    
    Reviewed By: #tensorflow, jakeh
    
    Subscribers: jakeh
    
    Maniphest Tasks: T14756
    
    Differential Revision: https://phabricator.sourcevertex.net/D19772

---
+++ tensorflow/python/ipu/ops/reduce_scatter_op.py
@@ -0,0 +1,37 @@
+# Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+from tensorflow.compiler.plugin.poplar.ops import gen_popops_ops
+
+
+def reduce_scatter(x, replication_factor, name=None):
+  """Reduce (sum) the given replicated tensor with the result scattered across
+  the replicas. For an input of shape `[num_elements]`, the output will have
+  shape `[ceil(num_elements / replication_factor)]`. If `replication_factor`
+  does not evenly divide `num_elements`, the result is zero-padded. Example:
+  Input:  Replica0: [x0, y0, z0]
+          Replica1: [x1, y1, z1]
+  Output: Replica0: [x0 + x1, y0 + y1]
+          Replica1: [z0 + z1, 0]
+
+    Args:
+      x: The input `Tensor`. Must have rank 1.
+      replication_factor: The replication factor of the model.
+      name: Optional op name.
+
+    Returns:
+      A `Tensor` with the result for this replica.
+  """
+  return gen_popops_ops.ipu_reduce_scatter(
+      x, replication_factor=replication_factor, name=name)
