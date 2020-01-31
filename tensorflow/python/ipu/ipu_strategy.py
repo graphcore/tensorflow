ed5ea9a3a1c2e2689b293ae0804b0a10e68f7931
Håkon Sandsmark <hakons@graphcore.ai>
2020-01-31 11:54:59 +0100

    ipu_strategy: Initial version of single-worker IPUStrategy
    Summary:
    This is a single-worker distribution strategy that does a few things:
    - Places ops and variables on the IPU device.
    - Places initializers on the CPU device.
    - Compiles the function passed to `strategy.experimental_run_v2` as a single engine (when passed a function using graph mode, e.g. `tf.function`).
    
    I think of it as an "improved" (more powerful) device scope. So instead of
    ```
    with ops.device("/device:IPU:0"):
      ...
    ```
    you would use
    ```
    strategy = IPUStrategy("/device:IPU:0")
    with strategy.scope():
      ...
    ```
    
    It seems to work fairly OK for a few use cases:
    - A custom step function passed to `strategy.experimental_run_v2`. The function must be decorated with `tf.function` to avoid eager mode.
    - Using the Keras model.{compile/fit} API. This uses graph mode (unless passed `run_eagerly=True`) and compiles a single engine that is executed once for each batch. The Keras code internally calls `strategy.experimental_run_v2` with its training step function such that we compile it to a single engine.
    
    Only the absolute minimum to pass the tests is implemented, many abstract methods are left abstract and will raise exceptions if we trigger them later (useful for learning what high-level APIs call what distribution strategy methods).
    
    Reviewers: #tensorflow, davidn
    
    Reviewed By: #tensorflow, davidn
    
    Subscribers: davidn
    
    Maniphest Tasks: T14403
    
    Differential Revision: https://phabricator.sourcevertex.net/D20525

---
+++ tensorflow/python/ipu/ipu_strategy.py
@@ -0,0 +1,101 @@
+# Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===================================================================
+from tensorflow.python.distribute import distribute_lib
+from tensorflow.python.framework import ops
+from tensorflow.python.ops import control_flow_ops
+from tensorflow.python.util import nest
+
+
+class IPUStrategy(distribute_lib.StrategyV1):
+  def __init__(self, ipu_device="/device:IPU:0", cpu_device="/device:CPU:0"):
+    super().__init__(IPUExtended(self, ipu_device, cpu_device))
+
+
+def _get_variable_creator_initial_value(device, **kwargs):
+  def initial_value_fn():
+    with ops.device(device):
+      initial_value = kwargs["initial_value"]
+      if callable(initial_value):
+        initial_value = initial_value()
+      assert not callable(initial_value)
+      return ops.convert_to_tensor(initial_value,
+                                   dtype=kwargs.get("dtype", None))
+
+  return initial_value_fn
+
+
+class IPUExtended(distribute_lib.StrategyExtendedV1):  # pylint: disable=abstract-method
+  # Not all abstract methods are implemented; implement as needed.
+  # See _DefaultDistributionExtended for dummy implementations.
+
+  def __init__(self, container_strategy, ipu_device, cpu_device):
+    super().__init__(container_strategy)
+    self._ipu_device = ipu_device
+    self._cpu_device = cpu_device
+
+  def _create_variable(self, next_creator, *args, **kwargs):
+    # Place initializer on the CPU.
+    kwargs["initial_value"] = _get_variable_creator_initial_value(
+        device=self._cpu_device, **kwargs)
+
+    # Place variable on the IPU.
+    with ops.device(self._ipu_device):
+      return next_creator(*args, **kwargs)
+
+  def _local_results(self, distributed_value):
+    return (distributed_value,)
+
+  def value_container(self, value):
+    return value
+
+  def _call_for_each_replica(self, fn, args, kwargs):
+    with distribute_lib.ReplicaContext(self._container_strategy(),
+                                       replica_id_in_sync_group=0), \
+        ops.device(self._ipu_device):
+      # Make sure it is compiled as a single engine when called in graph mode.
+      # This is similar to the mechanism used by xla.compile.
+      xla_context = control_flow_ops.XLAControlFlowContext()
+      try:
+        xla_context.Enter()
+        return fn(*args, **kwargs)
+      finally:
+        xla_context.Exit()
+
+  def _reduce_to(self, reduce_op, value, destinations):
+    del reduce_op, destinations
+    return value
+
+  def _update(self, var, fn, args, kwargs, group):
+    return self._update_non_slot(var, fn, (var,) + tuple(args), kwargs, group)
+
+  def _update_non_slot(self, colocate_with, fn, args, kwargs, group):
+    with distribute_lib.UpdateContext(colocate_with):
+      result = fn(*args, **kwargs)
+      if group:
+        return result
+      return nest.map_structure(self._local_results, result)
+
+  @property
+  def _num_replicas_in_sync(self):
+    return 1
+
+  def _in_multi_worker_mode(self):
+    return False
+
+  def _global_batch_size(self):
+    return True
+
+  def _experimental_distribute_dataset(self, dataset):
+    return dataset
---
+++ tensorflow/python/ipu/tests/ipu_strategy_test.py
@@ -0,0 +1,272 @@
+# Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =============================================================================
+import numpy as np
+
+from tensorflow.compiler.plugin.poplar.driver.trace_pb2 import IpuTraceEvent
+from tensorflow.compiler.plugin.poplar.tests import test_utils as tu
+from tensorflow.python import keras
+from tensorflow.python.eager import def_function
+from tensorflow.python.eager.backprop import GradientTape
+from tensorflow.python.framework import test_util
+from tensorflow.python.ipu import ipu_strategy
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import variables
+from tensorflow.python.platform import test
+
+
+def _get_compiled_modules(trace_events):
+  compiled_modules = []
+  for evt in trace_events:
+    if evt.type == IpuTraceEvent.COMPILE_END:
+      # Skip empty reports, i.e. when compilation was skipped.
+      if evt.compile_end.compilation_report:
+        compiled_modules.append(evt.compile_end.module_name)
+  return compiled_modules
+
+
+class IPUStrategyTest(test_util.TensorFlowTestCase):
+  @test_util.run_v2_only
+  def test_create_variable(self):
+    # IPU 0 should be the default.
+    ipu0_strategy = ipu_strategy.IPUStrategy()
+    with ipu0_strategy.scope():
+      v0 = variables.Variable(1.0)
+      self.assertEqual("/job:localhost/replica:0/task:0/device:IPU:0",
+                       v0.device)
+
+    ipu1_strategy = ipu_strategy.IPUStrategy("/device:IPU:1")
+    with ipu1_strategy.scope():
+      v1 = variables.Variable(1.0)
+      self.assertEqual("/job:localhost/replica:0/task:0/device:IPU:1",
+                       v1.device)
+
+  @test_util.run_v2_only
+  def test_inference_step_fn_keras_model(self):
+    strategy = ipu_strategy.IPUStrategy()
+
+    with strategy.scope():
+
+      model = keras.Sequential([
+          keras.layers.Dense(5),
+          keras.layers.Dense(10),
+          keras.layers.Softmax(),
+      ])
+
+      @def_function.function
+      def step_fn(x):
+        return model(x)
+
+      report = tu.ReportJSON(self, eager_mode=True)
+      report.reset()
+
+      inputs = np.ones((1, 2), dtype=np.float32)
+      out = strategy.experimental_run_v2(step_fn, args=[inputs])
+      self.assertEqual("/job:localhost/replica:0/task:0/device:IPU:0",
+                       out.device)
+      self.assertAllClose(1.0, np.sum(out.numpy()))
+
+      # There should be a single engine, executed once.
+      event_counts, _ = report.get_ipu_events()
+      self.assertEqual(1, event_counts[IpuTraceEvent.EXECUTE])
+
+  @test_util.run_v2_only
+  def test_building_model_by_passing_input_shape_to_first_layer(self):
+    strategy = ipu_strategy.IPUStrategy()
+
+    with strategy.scope():
+
+      report = tu.ReportJSON(self, eager_mode=True)
+      report.reset()
+
+      # Passing input_shape to first layer builds the model.
+      model = keras.Sequential([
+          keras.layers.Dense(5, input_shape=(2,)),
+          keras.layers.Dense(10),
+          keras.layers.Softmax(),
+      ])
+
+      # The model is built, meaning shapes are known and weights allocated,
+      # but no engines should have been compiled or executed yet.
+      self.assertTrue(model.built)
+      self.assertEqual(4, len(model.variables))
+
+      event_counts, trace_events = report.get_ipu_events()
+      self.assertEqual([], _get_compiled_modules(trace_events))
+      self.assertEqual(0, event_counts[IpuTraceEvent.EXECUTE])
+
+  @test_util.run_v2_only
+  def test_building_model_explicitly(self):
+    strategy = ipu_strategy.IPUStrategy()
+
+    with strategy.scope():
+
+      report = tu.ReportJSON(self, eager_mode=True)
+      report.reset()
+
+      model = keras.Sequential([
+          keras.layers.Dense(5),
+          keras.layers.Dense(10),
+          keras.layers.Softmax(),
+      ])
+
+      self.assertFalse(model.built)
+
+      model.build(input_shape=(None, 2))
+
+      # The model is now built, meaning shapes are known and weights are
+      # allocated, but no engines should have been compiled or executed yet.
+      self.assertTrue(model.built)
+      self.assertEqual(4, len(model.variables))
+
+      event_counts, trace_events = report.get_ipu_events()
+      self.assertEqual([], _get_compiled_modules(trace_events))
+      self.assertEqual(0, event_counts[IpuTraceEvent.EXECUTE])
+
+  @test_util.run_v2_only
+  def test_model_with_autograph_loop(self):
+    strategy = ipu_strategy.IPUStrategy()
+
+    with strategy.scope():
+
+      model = keras.Sequential([
+          keras.layers.Dense(1, activation='relu'),
+      ])
+
+      @def_function.function
+      def step_fn(x):
+        while x[0] < 0.0:
+          x = model(x)
+        return x
+
+      report = tu.ReportJSON(self, eager_mode=True)
+      report.reset()
+
+      inputs = -1.0 * np.ones((1, 1), dtype=np.float32)
+      out = strategy.experimental_run_v2(step_fn, args=[inputs])
+      self.assertGreaterEqual(out, 0.0)
+
+      # There should be a single engine, executed once. If auto-clustering
+      # were enabled, it would usually produce multiple engines for the loop.
+      event_counts, _ = report.get_ipu_events()
+      self.assertEqual(1, event_counts[IpuTraceEvent.LOAD_ENGINE])
+      self.assertEqual(1, event_counts[IpuTraceEvent.EXECUTE])
+
+  @test_util.run_v2_only
+  def test_train_step_fn_keras_model(self):
+    strategy = ipu_strategy.IPUStrategy()
+
+    with strategy.scope():
+
+      model = keras.Sequential([
+          keras.layers.Dense(1),
+      ])
+
+      optimizer = keras.optimizer_v2.gradient_descent.SGD(0.01)
+
+      @def_function.function
+      def step_fn(features, labels):
+        with GradientTape() as tape:
+          predictions = model(features, training=True)
+          prediction_loss = keras.losses.mean_squared_error(
+              labels, predictions)
+          loss = math_ops.reduce_mean(prediction_loss)
+
+        grads = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+        return loss
+
+      report = tu.ReportJSON(self, eager_mode=True)
+      report.reset()
+
+      batch_size = 5
+      x_train = np.ones((batch_size, 10), dtype=np.float32)
+      y_train = np.ones((batch_size, 1), dtype=np.float32)
+
+      first_loss = strategy.experimental_run_v2(step_fn,
+                                                args=[x_train, y_train])
+      second_loss = strategy.experimental_run_v2(step_fn,
+                                                 args=[x_train, y_train])
+
+      # Check that loss is decreasing.
+      self.assertLess(second_loss, first_loss)
+
+      # There should be a single engine, loaded once, executed twice.
+      event_counts, _ = report.get_ipu_events()
+      self.assertEqual(1, event_counts[IpuTraceEvent.LOAD_ENGINE])
+      self.assertEqual(2, event_counts[IpuTraceEvent.EXECUTE])
+
+  @test_util.run_v2_only
+  def test_keras_mnist_model_compile_fit(self):
+    num_examples = 100
+    batch_size = 10
+    num_classes = 10
+    num_epochs = 3
+
+    def mnist_model():
+      model = keras.models.Sequential()
+      model.add(keras.layers.Conv2D(32, (3, 3), activation='relu'))
+      model.add(keras.layers.Conv2D(64, (3, 3), activation='relu'))
+      model.add(keras.layers.MaxPooling2D((2, 2)))
+      model.add(keras.layers.Dropout(0.25))
+      model.add(keras.layers.Flatten())
+      model.add(keras.layers.Dense(64, activation='relu'))
+      model.add(keras.layers.Dropout(0.5))
+      model.add(keras.layers.Dense(num_classes, activation='softmax'))
+      return model
+
+    (x_train, y_train), _ = keras.datasets.mnist.load_data()
+    x_train = x_train[:num_examples]
+    y_train = y_train[:num_examples]
+
+    x_train = x_train.reshape(*x_train.shape, 1)
+    x_train = x_train.astype('float32')
+    x_train /= 255
+    y_train = keras.utils.np_utils.to_categorical(y_train, num_classes)
+
+    report = tu.ReportJSON(self, eager_mode=True)
+    report.reset()
+
+    strategy = ipu_strategy.IPUStrategy()
+
+    with strategy.scope():
+
+      model = mnist_model()
+      model.compile(loss=keras.losses.categorical_crossentropy,
+                    optimizer=keras.optimizer_v2.gradient_descent.SGD(0.05))
+      history = model.fit(
+          x_train,
+          y_train,
+          batch_size=batch_size,
+          shuffle=False,  # Try to make it deterministic.
+          epochs=num_epochs,
+          verbose=1)
+
+      # Check that the loss decreased.
+      losses = history.history["loss"]
+      self.assertEqual(3, len(losses))
+      self.assertLess(losses[1], losses[0])
+      self.assertLess(losses[2], losses[1])
+
+      num_batches = num_epochs * num_examples // batch_size
+
+      # There should be be a single engine, loaded once, and executed one
+      # time for each batch.
+      event_counts, _ = report.get_ipu_events()
+      self.assertEqual(1, event_counts[IpuTraceEvent.LOAD_ENGINE])
+      self.assertEqual(num_batches, event_counts[IpuTraceEvent.EXECUTE])
+
+
+if __name__ == "__main__":
+  test.main()
