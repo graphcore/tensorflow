5b83666a9f44741b2e0223a579e51cc8aecca72e
Romand <Romand@graphcore.ai>
2020-02-17 11:48:59 +0000

    Add ipu gelu
    Summary: Fixes T15530 . Add ipu gelu.
    
    Test Plan: Add test. All tests pass.
    
    Reviewers: #tensorflow, georgep
    
    Reviewed By: #tensorflow, georgep
    
    Subscribers: georgep, davidn, hakons, #tensorflow
    
    Maniphest Tasks: T15530
    
    Differential Revision: https://phabricator.sourcevertex.net/D21194

---
+++ tensorflow/python/ipu/ops/nn_ops.py
@@ -0,0 +1,39 @@
+# Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""
+Popnn operators
+~~~~~~~~~~~~~~~
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from tensorflow.compiler.plugin.poplar.ops import gen_popnn_ops
+
+
+def gelu(x, name=None):
+  """This targets the poplibs popnn gelu operation, optimized for execution
+  on the IPU.
+
+  Args:
+    x: The input tensor.
+    name: Optional op name.
+
+  Returns:
+    A `Tensor`. Has the same type the input tensor.
+  """
+
+  return gen_popnn_ops.ipu_gelu(x, name=name)
