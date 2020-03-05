0c7ca1614451c5d96b1ea6548fb64a252a94f4d0
George Grzegorz Pawelczak <grzegorzp@graphcore.ai>
2020-03-05 11:34:32 +0000

    Add missing argument
    Summary: Ref T15698
    
    Test Plan: Tests now compile
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, davidn
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, davidn
    
    Maniphest Tasks: T15698
    
    Differential Revision: https://phabricator.sourcevertex.net/D22205

--- tensorflow/compiler/plugin/poplar/tests/deferred_visitor_test.cc
+++ tensorflow/compiler/plugin/poplar/tests/deferred_visitor_test.cc
@@ -59,7 +59,7 @@ std::unique_ptr<CompilerResources> GetMockResources(HloModule* module,
   auto resources = absl::make_unique<CompilerResources>(
       poplar::OptionFlags(), poplar::OptionFlags(), poplar::OptionFlags(),
       false, false, false, merge_infeeds, 1, 0, 0, 0, 1, 64, module,
-      IpuOptions::FloatingPointBehaviour(), false, "", false, false);
+      IpuOptions::FloatingPointBehaviour(), false, "", false, false, false);
   resources->module_call_graph = CallGraph::Build(module);
   resources->main_graph = absl::make_unique<poplar::Graph>(
       poplar::Device::createCPUDevice(), 0, poplar::replication_factor(1));
