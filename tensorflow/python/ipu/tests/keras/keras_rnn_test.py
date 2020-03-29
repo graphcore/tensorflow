9e5844625aaa6d26fb35699ef9163c949b5cddde
David Norman <davidn@graphcore.ai>
2020-03-29 15:45:03 +0100

    Port keras time major to r1.15
    Summary:
    Porting addition of time major option, and return_sequence options
    from r2.1 to r1.15.
    
    This change unifies the two code bases.
    
    Test Plan: all tests pass.
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, jakeh
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, jakeh
    
    Maniphest Tasks: T18179
    
    Differential Revision: https://phabricator.sourcevertex.net/D23558

--- tensorflow/python/ipu/tests/keras/keras_rnn_test.py
+++ tensorflow/python/ipu/tests/keras/keras_rnn_test.py
@@ -20,11 +20,11 @@ from __future__ import print_function
 
 import numpy as np
 
-from tensorflow.python.framework import test_util
+from tensorflow.python.eager import def_function
 from tensorflow.python.framework import ops
+from tensorflow.python.framework import test_util
 from tensorflow.python.platform import test
 from tensorflow.python.ops import array_ops
-from tensorflow.python.framework import ops
 from tensorflow.python.ops import init_ops
 from tensorflow.python.ops import rnn
 from tensorflow.python.ops import rnn_cell
@@ -33,14 +33,19 @@ from tensorflow.python.ops import variables
 from tensorflow.python import ipu
 
 # Test hyperparameters.
-batch_size = 8
-num_input = 2
+batch_size = 1
+num_input = 3
 timesteps = 4
-num_hidden = 18
+num_hidden = 5
 dataType = np.float32
 
 
-def _tfLSTM(instance, x_val, h_val, c_val):
+def _tfLSTM(instance,
+            x_val,
+            h_val,
+            c_val,
+            return_sequences=False,
+            time_major=False):
   with ops.device('cpu'):
     x = array_ops.placeholder(x_val.dtype, x_val.shape)
     h = array_ops.placeholder(h_val.dtype, h_val.shape)
@@ -51,16 +56,20 @@ def _tfLSTM(instance, x_val, h_val, c_val):
         forget_bias=0.,
         initializer=init_ops.ones_initializer(dtype=dataType))
     state = rnn_cell.LSTMStateTuple(c, h)
-
     output = rnn.dynamic_rnn(lstm_cell,
                              x,
                              dtype=dataType,
                              initial_state=state,
-                             time_major=True)
+                             time_major=time_major)
 
   with instance.test_session() as sess:
     sess.run(variables.global_variables_initializer())
-    return sess.run(output, {x: x_val, h: h_val, c: c_val})
+    out, outs = sess.run(output, {x: x_val, h: h_val, c: c_val})
+
+    if not return_sequences:
+      out = out[-1, :, :] if time_major else out[:, -1, :]
+
+    return out, outs[1], outs[0]
 
 
 def _new_kerasLSTM(instance,
@@ -69,12 +78,15 @@ def _new_kerasLSTM(instance,
                    c_val,
                    training=True,
                    return_state=True,
+                   return_sequences=False,
+                   time_major=False,
                    dropout=0.):
   with ops.device('/device:IPU:0'):
     x = array_ops.placeholder(x_val.dtype, x_val.shape)
     h = array_ops.placeholder(h_val.dtype, h_val.shape)
     c = array_ops.placeholder(c_val.dtype, c_val.shape)
     state = rnn_cell.LSTMStateTuple(c, h)
+
     output = ipu.layers.PopnnLSTM(
         num_hidden,
         dtype=dataType,
@@ -82,6 +94,8 @@ def _new_kerasLSTM(instance,
         recurrent_initializer=init_ops.ones_initializer(dtype=dataType),
         bias_initializer=init_ops.ones_initializer(dtype=dataType),
         dropout=dropout,
+        time_major=time_major,
+        return_sequences=return_sequences,
         return_state=return_state)(inputs=x,
                                    initial_state=state,
                                    training=training)
@@ -91,12 +105,15 @@ def _new_kerasLSTM(instance,
     return sess.run(output, {x: x_val, h: h_val, c: c_val})
 
 
-def _tfGRU(instance, x_val, initial_state_val):
+def _tfGRU(instance,
+           x_val,
+           initial_state_val,
+           return_sequences=False,
+           time_major=False):
   with ops.device('cpu'):
     x = array_ops.placeholder(x_val.dtype, x_val.shape)
     initial_state = array_ops.placeholder(initial_state_val.dtype,
                                           initial_state_val.shape)
-
     gru_cell = rnn_cell.GRUCell(
         num_hidden,
         name='gru_cell',
@@ -106,11 +123,15 @@ def _tfGRU(instance, x_val, initial_state_val):
                              x,
                              dtype=dataType,
                              initial_state=initial_state,
-                             time_major=True)
-
+                             time_major=time_major)
   with instance.test_session() as sess:
     sess.run(variables.global_variables_initializer())
-    return sess.run(output, {x: x_val, initial_state: initial_state_val})
+    out, outs = sess.run(output, {x: x_val, initial_state: initial_state_val})
+
+    if not return_sequences:
+      out = out[-1, :, :] if time_major else out[:, -1, :]
+
+    return out, outs
 
 
 def _new_kerasGRU(instance,
@@ -118,6 +139,8 @@ def _new_kerasGRU(instance,
                   initial_state_val,
                   training=True,
                   return_state=True,
+                  return_sequences=False,
+                  time_major=False,
                   dropout=0.):
   with ops.device('/device:IPU:0'):
     x = array_ops.placeholder(x_val.dtype, x_val.shape)
@@ -129,6 +152,8 @@ def _new_kerasGRU(instance,
         kernel_initializer=init_ops.ones_initializer(dtype=dataType),
         bias_initializer=init_ops.constant_initializer(2.0, dtype=dataType),
         dropout=dropout,
+        time_major=time_major,
+        return_sequences=return_sequences,
         return_state=return_state)(inputs=x,
                                    initial_state=initial_state,
                                    training=training)
@@ -139,55 +164,111 @@ def _new_kerasGRU(instance,
 
 
 class IpuLstmTest(test.TestCase):
+  def _get_random_inputs(self, time_major=False):
+    np.random.seed(42)
+    h = np.random.rand(batch_size, num_hidden).astype(dataType)
+    c = np.random.rand(batch_size, num_hidden).astype(dataType)
+    if time_major:
+      x = np.random.rand(timesteps, batch_size, num_input).astype(dataType)
+    else:
+      x = np.random.rand(batch_size, timesteps, num_input).astype(dataType)
+    return x, h, c
+
   @test_util.deprecated_graph_mode_only
-  def testLstm(self):
-    np.random.seed(42)
-    x = np.random.rand(timesteps, batch_size, num_input).astype(dataType)
+  def test_lstm(self):
+    x, h, c = self._get_random_inputs()
 
-    np.random.seed(42)
-    keras_result = _new_kerasLSTM(
-        self, x, np.ones((batch_size, num_hidden), dtype=dataType),
-        np.ones((batch_size, num_hidden), dtype=dataType))
+    keras_result = _new_kerasLSTM(self, x, h, c)
+    result_tf = _tfLSTM(self, x, h, c)
 
-    np.random.seed(42)
-    result_tf = _tfLSTM(self, x,
-                        np.ones((batch_size, num_hidden), dtype=dataType),
-                        np.ones((batch_size, num_hidden), dtype=dataType))
-    self.assertAllClose(keras_result, result_tf)
+    self.assertEqual(len(keras_result), 3)
+    self.assertEqual(len(result_tf), 3)
 
+    self.assertAllClose(keras_result[0], result_tf[0], rtol=0.01)
+    self.assertAllClose(keras_result[1], result_tf[1], rtol=0.2)
+    self.assertAllClose(keras_result[2], result_tf[2], rtol=0.2)
+
+    self.assertNotAllClose(keras_result[0], np.ones([batch_size, num_hidden]))
+    self.assertEqual(keras_result[0].shape, (batch_size, num_hidden))
+
+  @test_util.deprecated_graph_mode_only
+  def test_lstm_time_major(self):
+    x, h, c = self._get_random_inputs(time_major=True)
+
+    keras_result = _new_kerasLSTM(self, x, h, c, time_major=True)
+    result_tf = _tfLSTM(self, x, h, c, time_major=True)
+
+    self.assertEqual(len(keras_result), 3)
+    self.assertEqual(len(result_tf), 3)
+
+    self.assertAllClose(keras_result[0], result_tf[0], rtol=0.01)
+    self.assertAllClose(keras_result[1], result_tf[1], rtol=0.2)
+    self.assertAllClose(keras_result[2], result_tf[2], rtol=0.2)
+
+    self.assertNotAllClose(keras_result[0], np.ones([batch_size, num_hidden]))
+    self.assertEqual(keras_result[0].shape, (batch_size, num_hidden))
+
+  @test_util.deprecated_graph_mode_only
+  def test_lstm_all_seq(self):
+    x, h, c = self._get_random_inputs()
+
+    keras_result = _new_kerasLSTM(self, x, h, c, return_sequences=True)
+    result_tf = _tfLSTM(self, x, h, c, return_sequences=True)
+
+    self.assertEqual(len(keras_result), 3)
+    self.assertEqual(len(result_tf), 3)
+
+    self.assertAllClose(keras_result[0], result_tf[0], rtol=0.05)
+    self.assertAllClose(keras_result[1], result_tf[1], rtol=0.2)
+    self.assertAllClose(keras_result[2], result_tf[2], rtol=0.2)
+
+    self.assertNotAllClose(keras_result[0], np.ones([batch_size, num_hidden]))
+    self.assertEqual(keras_result[0].shape,
+                     (batch_size, timesteps, num_hidden))
+
+  @test_util.deprecated_graph_mode_only
   def test_lstm_no_state(self):
-    np.random.seed(42)
-    x = np.random.rand(timesteps, batch_size, num_input).astype(dataType)
+    x, h, c = self._get_random_inputs()
 
-    np.random.seed(42)
-    keras_result = _new_kerasLSTM(self,
-                                  x,
-                                  np.ones((batch_size, num_hidden),
-                                          dtype=dataType),
-                                  np.ones((batch_size, num_hidden),
-                                          dtype=dataType),
-                                  training=True,
-                                  return_state=False)
+    keras_result = _new_kerasLSTM(self, x, h, c, return_state=False)
     self.assertTrue(isinstance(keras_result, np.ndarray))
 
+  @test_util.deprecated_graph_mode_only
   def test_no_dynamic_training(self):
-    np.random.seed(42)
-    x = np.random.rand(timesteps, batch_size, num_input).astype(dataType)
+    x, h, c = self._get_random_inputs()
+
     with self.assertRaisesRegex(ValueError,
                                 'PopnnLSTM does not support a dynamic'):
-      _new_kerasLSTM(self,
-                     x,
-                     np.ones((batch_size, num_hidden), dtype=dataType),
-                     array_ops.ones((batch_size, num_hidden), dtype=dataType),
-                     training=None)
+      _new_kerasLSTM(self, x, h, c, training=None)
 
+  @test_util.deprecated_graph_mode_only
   def test_class_alias(self):
     self.assertTrue(isinstance(ipu.layers.LSTM, type))
     self.assertEqual(ipu.layers.PopnnLSTM, ipu.layers.LSTM)
 
+  @test_util.deprecated_graph_mode_only
+  def test_lstm_dropout(self):
+    x, h, c = self._get_random_inputs()
+
+    dropout_result = _new_kerasLSTM(self,
+                                    x,
+                                    h,
+                                    c,
+                                    return_state=False,
+                                    dropout=0.)
+
+    clear_result = _new_kerasLSTM(self,
+                                  x,
+                                  h,
+                                  c,
+                                  return_state=False,
+                                  dropout=1.)
+
+    self.assertNotAllClose(clear_result, dropout_result)
+
+  @test_util.run_v2_only
   def test_can_call_without_state_change(self):
-    np.random.seed(42)
-    x = np.random.rand(timesteps, batch_size, num_input).astype(dataType)
+    x, h, c = self._get_random_inputs()
 
     layer = ipu.layers.PopnnLSTM(
         num_hidden,
@@ -199,48 +280,99 @@ class IpuLstmTest(test.TestCase):
         bias_initializer=init_ops.zeros_initializer(dtype=dataType))
     layer.build(x.shape)
 
+    @def_function.function
+    def impl(x, c, h):
+      state = rnn_cell.LSTMStateTuple(c, h)
+      return layer(inputs=x, initial_state=state)
+
     self.assertEqual(layer.kernel.shape, [num_input, num_hidden * 4])
-    layer(x)
+    _ = impl(x, c, h)
     self.assertEqual(layer.kernel.shape, [num_input, num_hidden * 4])
-    layer(x)
+    _ = impl(x, c, h)
 
 
 class IpuGruTest(test.TestCase):
+  def _get_random_inputs(self, time_major=False):
+    np.random.seed(42)
+    init = np.random.rand(batch_size, num_hidden).astype(dataType)
+    if time_major:
+      x = np.random.rand(timesteps, batch_size, num_input).astype(dataType)
+    else:
+      x = np.random.rand(batch_size, timesteps, num_input).astype(dataType)
+    return x, init
+
   @test_util.deprecated_graph_mode_only
-  def testGru(self):
-    np.random.seed(42)
-    x = np.random.rand(timesteps, batch_size, num_input).astype(dataType)
-    keras_result = _new_kerasGRU(
-        self, x, np.ones((batch_size, num_hidden), dtype=dataType))
-    result_tf = _tfGRU(self, x,
-                       np.ones((batch_size, num_hidden), dtype=dataType))
+  def test_gru(self):
+    x, init = self._get_random_inputs()
+
+    keras_result = _new_kerasGRU(self, x, init)
+    result_tf = _tfGRU(self, x, init)
+
     self.assertAllClose(keras_result, result_tf)
+    self.assertNotAllClose(keras_result[0], np.ones([batch_size, num_hidden]))
+    self.assertEqual(keras_result[0].shape, (batch_size, num_hidden))
 
-  def test_lstm_no_state(self):
-    np.random.seed(42)
-    x = np.random.rand(timesteps, batch_size, num_input).astype(dataType)
+  @test_util.deprecated_graph_mode_only
+  def test_gru_seq_major(self):
+    x, init = self._get_random_inputs(True)
 
-    np.random.seed(42)
-    keras_result = _new_kerasGRU(self,
-                                 x,
-                                 np.ones((batch_size, num_hidden),
-                                         dtype=dataType),
-                                 return_state=False)
+    keras_result = _new_kerasGRU(self, x, init, time_major=True)
+    result_tf = _tfGRU(self, x, init, time_major=True)
+
+    self.assertAllClose(keras_result, result_tf)
+    self.assertNotAllClose(keras_result[0], np.ones([batch_size, num_hidden]))
+    self.assertEqual(keras_result[0].shape, (batch_size, num_hidden))
+
+  @test_util.deprecated_graph_mode_only
+  def test_gru_all_seq(self):
+    x, init = self._get_random_inputs()
+
+    keras_result = _new_kerasGRU(self, x, init, return_sequences=True)
+    result_tf = _tfGRU(self, x, init, return_sequences=True)
+
+    self.assertAllClose(keras_result, result_tf)
+    self.assertNotAllClose(keras_result[0], np.ones([batch_size, num_hidden]))
+    self.assertEqual(keras_result[0].shape,
+                     (batch_size, timesteps, num_hidden))
+
+  @test_util.deprecated_graph_mode_only
+  def test_gru_no_state(self):
+    x, init = self._get_random_inputs()
+
+    keras_result = _new_kerasGRU(self, x, init, return_state=False)
     self.assertTrue(isinstance(keras_result, np.ndarray))
 
+  @test_util.deprecated_graph_mode_only
   def test_no_dynamic_training(self):
-    np.random.seed(42)
-    x = np.random.rand(timesteps, batch_size, num_input).astype(dataType)
+    x, init = self._get_random_inputs()
+
     with self.assertRaisesRegex(ValueError,
                                 'PopnnGRU does not support a dynamic'):
-      _new_kerasGRU(self,
-                    x,
-                    np.ones((batch_size, num_hidden), dtype=dataType),
-                    training=None)
+      _new_kerasGRU(self, x, init, training=None)
 
+  @test_util.deprecated_graph_mode_only
   def test_class_alias(self):
     self.assertTrue(isinstance(ipu.layers.GRU, type))
     self.assertEqual(ipu.layers.PopnnGRU, ipu.layers.GRU)
+
+  @test_util.deprecated_graph_mode_only
+  def test_gru_dropout(self):
+    x, init = self._get_random_inputs()
+
+    dropout_result = _new_kerasGRU(self,
+                                   x,
+                                   init,
+                                   dropout=1.,
+                                   return_state=False,
+                                   return_sequences=True)
+    clear_result = _new_kerasGRU(self,
+                                 x,
+                                 init,
+                                 dropout=0.,
+                                 return_state=False,
+                                 return_sequences=True)
+
+    self.assertNotAllClose(clear_result, dropout_result)
 
 
 if __name__ == '__main__':
