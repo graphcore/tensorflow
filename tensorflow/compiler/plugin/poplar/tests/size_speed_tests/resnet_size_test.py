727fd26fe96589da1f2f97d0b144db8dee6114e0
Håkon Sandsmark <hakons@graphcore.ai>
2020-05-04 22:20:46 +0200

    resnet_size_test: Update with new lower memory bounds
    Summary:
    After D25074 the memory consumption here is quite a bit lower.
    
    Ref T17787.
    
    Test Plan: Test passes.
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep
    
    Maniphest Tasks: T17787
    
    Differential Revision: https://phabricator.sourcevertex.net/D25359

--- tensorflow/compiler/plugin/poplar/tests/size_speed_tests/resnet_size_test.py
+++ tensorflow/compiler/plugin/poplar/tests/size_speed_tests/resnet_size_test.py
@@ -187,7 +187,7 @@ class Resnet18_No_Batchnorm(xla_test.XLATestCase):
       sess.run(train, feed_dict={x: data, y_: labels})
       report.parse_log()
 
-      report.assert_total_tile_memory(42388337)
+      report.assert_total_tile_memory(40445743)
 
   def testTrainingInLoop(self):
     with self.session() as sess:
@@ -258,7 +258,7 @@ class Resnet18_No_Batchnorm(xla_test.XLATestCase):
       sess.run(train, feed_dict={x: data, y_: labels})
       report.parse_log()
 
-      report.assert_total_tile_memory(39681488)
+      report.assert_total_tile_memory(37334076)
 
   def testTrainingInLoopWithGradientAccumulation(self):
     with self.session() as sess:
@@ -331,7 +331,7 @@ class Resnet18_No_Batchnorm(xla_test.XLATestCase):
       sess.run(train, feed_dict={x: data, y_: labels})
       report.parse_log()
 
-      report.assert_total_tile_memory(41452914)
+      report.assert_total_tile_memory(39233290)
 
 
 if __name__ == "__main__":
