c00b14dbb7ee795815256b6b3dc3858d314f17ef
George Grzegorz Pawelczak <grzegorzp@graphcore.ai>
2020-05-28 17:49:37 +0100

    Remove code duplication for non-linearities
    Summary:
    Remove duplication.
    
    Ref T21328
    
    Test Plan: All tests pass
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, jakeh
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, jakeh
    
    Subscribers: jakeh
    
    Maniphest Tasks: T21328
    
    Differential Revision: https://phabricator.sourcevertex.net/D26628

--- tensorflow/compiler/plugin/poplar/driver/ops/custom_ops/popnn/non_linearity.cc
+++ tensorflow/compiler/plugin/poplar/driver/ops/custom_ops/popnn/non_linearity.cc
@@ -13,46 +13,91 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#include <popnn/NonLinearity.hpp>
+
+#include "absl/container/flat_hash_map.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/str_split.h"
 #include "tensorflow/compiler/plugin/poplar/driver/ops/custom_ops/poplar_ops.h"
 #include "tensorflow/compiler/plugin/poplar/driver/tensor.h"
 #include "tensorflow/compiler/plugin/poplar/driver/tools/custom_ops/pooling.h"
 #include "tensorflow/compiler/plugin/poplar/driver/tools/util.h"
 #include "tensorflow/compiler/plugin/poplar/kernels/custom_kernels_util.h"
-
 #include "tensorflow/compiler/xla/service/hlo_casting_utils.h"
 #include "tensorflow/compiler/xla/service/hlo_instruction.h"
 #include "tensorflow/compiler/xla/status_macros.h"
 #include "tensorflow/compiler/xla/util.h"
 #include "tensorflow/core/lib/core/errors.h"
 
-#include "absl/container/flat_hash_map.h"
-#include "absl/strings/str_cat.h"
-#include "absl/strings/str_split.h"
-
 namespace xla {
 namespace poplarplugin {
 namespace {
-class GeluOp : public PoplarOpDef {
-  StatusOr<poplar::program::Program> Creator(poplar::Graph& graph,
-                                             CompilerResources& res,
-                                             const HloInstruction* inst,
-                                             const xla::Shape& output_shape,
-                                             TensorMap& tensor_map) override {
-    return CreateGeluOp(res, inst, output_shape, tensor_map);
-  }
-};
-REGISTER_POPLAR_OP(Gelu, GeluOp);
 
-class GeluGradOp : public PoplarOpDef {
+template <popnn::NonLinearityType NLType>
+class NonLinearityOp : public PoplarOpDef {
   StatusOr<poplar::program::Program> Creator(poplar::Graph& graph,
                                              CompilerResources& res,
                                              const HloInstruction* inst,
                                              const xla::Shape& output_shape,
                                              TensorMap& tensor_map) override {
-    return CreateGeluGradOp(res, inst, output_shape, tensor_map);
+    poplar::program::Sequence seq;
+    poplar::Tensor t;
+    const bool is_inplace =
+        AreInplaceOutputTensorsWritable(tensor_map, res, inst);
+
+    if (is_inplace) {
+      TF_ASSIGN_OR_RETURN(TensorVectors inputs,
+                          FindInplaceOutputTensors(tensor_map, res, inst, seq));
+      CHECK_EQ(inputs.size(), 1);
+      CHECK_EQ(inputs[0].size(), 1);
+      t = inputs[0][0];
+      popnn::nonLinearityInPlace(graph, NLType, t, seq, GetDebugName(inst));
+    } else {
+      TF_ASSIGN_OR_RETURN(
+          t, FindInstructionInput(tensor_map, res, inst, 0, seq, false));
+
+      t = popnn::nonLinearity(graph, NLType, t, seq, GetDebugName(inst));
+    }
+
+    TF_CHECK_OK(AddOutputTensor(tensor_map, inst, 0, t));
+
+    return seq;
   }
 };
-REGISTER_POPLAR_OP(GeluGrad, GeluGradOp);
+REGISTER_POPLAR_OP(Relu, NonLinearityOp<popnn::NonLinearityType::RELU>);
+REGISTER_POPLAR_OP(Gelu, NonLinearityOp<popnn::NonLinearityType::GELU>);
+REGISTER_POPLAR_OP(Sigmoid, NonLinearityOp<popnn::NonLinearityType::SIGMOID>);
+
+template <popnn::NonLinearityType NLType>
+class NonLinearityGradOp : public PoplarOpDef {
+  StatusOr<poplar::program::Program> Creator(poplar::Graph& graph,
+                                             CompilerResources& res,
+                                             const HloInstruction* inst,
+                                             const xla::Shape& output_shape,
+                                             TensorMap& tensor_map) override {
+    poplar::program::Sequence seq;
+
+    TF_ASSIGN_OR_RETURN(
+        poplar::Tensor out,
+        FindInstructionInput(tensor_map, res, inst, 0, seq, false));
+
+    TF_ASSIGN_OR_RETURN(
+        poplar::Tensor outgrad,
+        FindInstructionInput(tensor_map, res, inst, 1, seq, false));
+
+    poplar::Tensor t = popnn::nonLinearityInputGradient(
+        graph, NLType, out, outgrad, seq, GetDebugName(inst));
+
+    TF_CHECK_OK(AddOutputTensor(tensor_map, inst, 0, t));
+
+    return seq;
+  }
+};
+REGISTER_POPLAR_OP(ReluGrad, NonLinearityGradOp<popnn::NonLinearityType::RELU>);
+REGISTER_POPLAR_OP(GeluGrad, NonLinearityGradOp<popnn::NonLinearityType::GELU>);
+REGISTER_POPLAR_OP(SigmoidGrad,
+                   NonLinearityGradOp<popnn::NonLinearityType::SIGMOID>);
+REGISTER_POPLAR_OP(TanhGrad, NonLinearityGradOp<popnn::NonLinearityType::TANH>);
 }  // namespace
 }  // namespace poplarplugin
 }  // namespace xla
