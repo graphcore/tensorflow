6773a01e5f8f631bbba45ca65797b45dc0730d80
Jake <jakeh@graphcore.ai>
2020-07-08 19:03:41 +0100

    Backout RemoteBuffer change 1/2
    Summary: This reverts commit 93a0396a1cc14f13d2446268f7edff5495101352.
    
    Test Plan: revert-hammer
    
    Reviewers:
    
    Subscribers:

--- tensorflow/compiler/plugin/poplar/driver/visitors/deferred_visitor.cc
+++ tensorflow/compiler/plugin/poplar/driver/visitors/deferred_visitor.cc
@@ -259,7 +259,8 @@ Status DeferredVisitor::HandleParameter(HloInstruction* inst) {
 
   std::vector<Shape> shapes = FlattenedXlaShape(inst->shape());
 
-  // Check whether this is a remote parameter - if so allocate it here.
+  // Check whether this is a remote parameter - if so do not allocate the tensor
+  // as the RemoteParameterLoad will allocate it and add the copy.
   if (IsRemoteParameter(inst, resources_)) {
     CHECK(IsInstructionInEntryComputation(inst));
     CHECK_EQ(shapes.size(), 1);
@@ -310,7 +311,7 @@ Status DeferredVisitor::HandleParameterTensor(TensorLocation input_location,
   const bool has_allocation_target =
       HasTensorAllocationTarget(input_location, resources_);
 
-  auto& callsite_tensor =
+  auto callsite_tensor =
       callsite_inputs_[param_num][input_location.flattened_output_tuple_index];
 
   // Function which is called when allocating this tensor.
@@ -346,7 +347,7 @@ Status DeferredVisitor::HandleParameterTensor(TensorLocation input_location,
     // for this location, then just forward the tensor.
 
     // Do not call the post process for tensors which are not allocated.
-    output = callsite_tensor->AsTensor();
+    output = *callsite_tensor;
   } else {
     // The tensor is used and/or it doesn't have a layout.
     if (has_allocation_target) {
