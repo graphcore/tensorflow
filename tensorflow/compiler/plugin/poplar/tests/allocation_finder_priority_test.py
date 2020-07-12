558a81247dfdec9ffa12175f8aad5eeec5261dc0
George Grzegorz Pawelczak <grzegorzp@graphcore.ai>
2020-07-12 18:37:49 +0100

    adjust bounds
    Summary:
    Depends D29029
    Ref T16509
    
    Test Plan: All tests pass
    
    Reviewers: #tensorflow, jakeh, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved
    
    Reviewed By: #tensorflow, jakeh, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved
    
    Maniphest Tasks: T16509
    
    Differential Revision: https://phabricator.sourcevertex.net/D29030

--- tensorflow/compiler/plugin/poplar/tests/allocation_finder_priority_test.py
+++ tensorflow/compiler/plugin/poplar/tests/allocation_finder_priority_test.py
@@ -77,7 +77,7 @@ class AllocationFinderPriorityTest(xla_test.XLATestCase,
       sess.run(output, {ids_ph: np.ones([50])})
 
       report.parse_log()
-      report.assert_total_tile_memory(50961750)
+      report.assert_total_tile_memory(49708775)
 
 
 if __name__ == "__main__":
