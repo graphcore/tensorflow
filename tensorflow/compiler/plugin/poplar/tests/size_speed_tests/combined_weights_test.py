9d653c11bc5d5cb0c88f321ff94e6b61448eb952
George Grzegorz Pawelczak <grzegorzp@graphcore.ai>
2020-07-22 21:54:29 +0100

    Adjust bounds
    Summary: Ref T16509
    
    Test Plan: CI
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, stephenm
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, stephenm
    
    Maniphest Tasks: T16509
    
    Differential Revision: https://phabricator.sourcevertex.net/D29533

--- tensorflow/compiler/plugin/poplar/tests/size_speed_tests/combined_weights_test.py
+++ tensorflow/compiler/plugin/poplar/tests/size_speed_tests/combined_weights_test.py
@@ -111,7 +111,7 @@ class CombinedWeightsTest(xla_test.XLATestCase):
           "(because the copies will have been merged)")
 
       # Also check the overall size
-      report.assert_total_tile_memory(8946089)
+      report.assert_total_tile_memory(8592650)
 
 
 if __name__ == "__main__":
