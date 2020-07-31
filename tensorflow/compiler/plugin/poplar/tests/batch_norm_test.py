545d8b988403130b7b9a80c4ea18f6990509e609
George Grzegorz Pawelczak <grzegorzp@graphcore.ai>
2020-07-31 14:24:29 +0100

    Revert mixed precision changes
    Summary: This reverts commit 78e7b037dc23482516b713959c2737594b4446b2.
    
    Test Plan: revert-hammer
    
    Reviewers:
    
    Subscribers:

--- tensorflow/compiler/plugin/poplar/tests/batch_norm_test.py
+++ tensorflow/compiler/plugin/poplar/tests/batch_norm_test.py
@@ -22,13 +22,11 @@ import numpy as np
 from tensorflow.python import ipu
 from tensorflow.compiler.tests import xla_test
 from tensorflow.python.platform import googletest
-from tensorflow.python import cast as tf_cast
 from tensorflow.python.framework import ops
 from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import init_ops
 from tensorflow.python.ops import math_ops
 from tensorflow.python.ops import nn
-from tensorflow.python.ops import gen_nn_ops
 from tensorflow.python.ops import variable_scope
 from tensorflow.python.ops import variables
 from tensorflow.python.training import gradient_descent
@@ -72,6 +70,9 @@ class IpuXlaBatchNormTest(xla_test.XLATestCase):
       self.assertAllClose(result, np.zeros([1, 4, 64, 64, 4]))
 
       report.parse_log()
+
+      bl = ['*convert*/Cast*']
+      report.assert_compute_sets_not_in_blacklist(bl)
       report.assert_tensor_input_names("input_a", "x", "y")
 
   def testBatchNormalizeFp16(self):
@@ -84,21 +85,19 @@ class IpuXlaBatchNormTest(xla_test.XLATestCase):
 
             beta = variable_scope.get_variable(
                 "x",
-                dtype=np.float32,
+                dtype=np.float16,
                 shape=[4],
                 initializer=init_ops.constant_initializer(0.0))
             gamma = variable_scope.get_variable(
                 "y",
-                dtype=np.float32,
+                dtype=np.float16,
                 shape=[4],
                 initializer=init_ops.constant_initializer(1.0))
 
             b_mean, b_var = nn.moments(a, [0, 1, 2], name='moments')
-            b_mean_32 = tf_cast(b_mean, np.float32)
-            b_var_32 = tf_cast(b_var, np.float32)
 
-            normed = nn.batch_normalization(a, b_mean_32, b_var_32, beta,
-                                            gamma, 1e-3)
+            normed = nn.batch_normalization(a, b_mean, b_var, beta, gamma,
+                                            1e-3)
             return normed
 
       report = ReportJSON(self, sess)
@@ -109,6 +108,9 @@ class IpuXlaBatchNormTest(xla_test.XLATestCase):
       self.assertAllClose(result, np.zeros([1, 4, 64, 64, 4]))
 
       report.parse_log()
+
+      bl = ['*FusedBatchNorm*/Cast*']
+      report.assert_compute_sets_not_in_blacklist(bl)
       report.assert_tensor_input_names("input_a", "x", "y")
 
   def testBatchNormalizeFused(self):
@@ -147,8 +149,11 @@ class IpuXlaBatchNormTest(xla_test.XLATestCase):
       report.reset()
       result, _, _ = sess.run(out, {a: np.zeros([4, 64, 64, 4])})
       self.assertAllClose(result, np.zeros([4, 64, 64, 4]))
-
       report.parse_log()
+
+      bl = ['*convert*/Cast*']
+      report.assert_compute_sets_not_in_blacklist(bl)
+
       report.assert_tensor_input_names("input_a", "x", "y")
 
   def testBatchNormalizeFusedFp16(self):
@@ -160,24 +165,22 @@ class IpuXlaBatchNormTest(xla_test.XLATestCase):
           def my_graph(a):
             beta = variable_scope.get_variable(
                 "x",
-                dtype=np.float32,
+                dtype=np.float16,
                 shape=[4],
                 initializer=init_ops.constant_initializer(0.0))
             gamma = variable_scope.get_variable(
                 "y",
-                dtype=np.float32,
+                dtype=np.float16,
                 shape=[4],
                 initializer=init_ops.constant_initializer(1.0))
 
             b_mean, b_var = nn.moments(a, [0, 1, 2], name='moments')
-            b_mean_32 = tf_cast(b_mean, np.float32)
-            b_var_32 = tf_cast(b_var, np.float32)
 
             normed = nn.fused_batch_norm(a,
                                          gamma,
                                          beta,
-                                         b_mean_32,
-                                         b_var_32,
+                                         b_mean,
+                                         b_var,
                                          is_training=False)
             return normed
 
@@ -190,6 +193,9 @@ class IpuXlaBatchNormTest(xla_test.XLATestCase):
         self.assertAllClose(result, np.zeros([4, 64, 64, 4]))
 
         report.parse_log()
+
+        bl = ['*FusedBatchNorm*/Cast*']
+        report.assert_compute_sets_not_in_blacklist(bl)
         report.assert_tensor_input_names("input_a", "x", "y")
 
   def testBatchNormalizeLayer(self):
@@ -208,6 +214,9 @@ class IpuXlaBatchNormTest(xla_test.XLATestCase):
       self.assertAllClose(result, np.zeros([4, 64, 64, 4]))
 
       report.parse_log()
+
+      bl = ['*convert*/Cast*']
+      report.assert_compute_sets_not_in_blacklist(bl)
       report.assert_tensor_input_names("input_a")
 
   def testBatchNormalizeLayerWithStableStatistics(self):
@@ -248,6 +257,9 @@ class IpuXlaBatchNormTest(xla_test.XLATestCase):
       self.assertAllClose(result, np.zeros([4, 64, 64, 4]))
 
       report.parse_log()
+
+      bl = ['*convert*/Cast*']
+      report.assert_compute_sets_not_in_blacklist(bl)
       report.assert_tensor_input_names("input_a")
 
   def testBatchNormalizeLayerFp16(self):
@@ -266,6 +278,9 @@ class IpuXlaBatchNormTest(xla_test.XLATestCase):
       self.assertAllClose(result, np.zeros([4, 64, 64, 4]))
 
       report.parse_log()
+
+      bl = ['*convert*/Cast*']
+      report.assert_compute_sets_not_in_blacklist(bl)
       report.assert_tensor_input_names("input_a")
 
   def testBatchNormalizeLayerFusedFp16(self):
@@ -284,6 +299,9 @@ class IpuXlaBatchNormTest(xla_test.XLATestCase):
       self.assertAllClose(result, np.zeros([4, 64, 64, 4]))
 
       report.parse_log()
+
+      bl = ['*convert*/Cast*']
+      report.assert_compute_sets_not_in_blacklist(bl)
       report.assert_tensor_input_names("input_a")
 
   def testBatchNormalizeLayerFusedTrainingFp16(self):
@@ -305,58 +323,6 @@ class IpuXlaBatchNormTest(xla_test.XLATestCase):
       sess.run(variables.global_variables_initializer())
       result = sess.run([normed, train], {a: np.zeros([4, 64, 64, 4])})
       self.assertAllClose(result[0], np.zeros([4, 64, 64, 4]))
-
-  def testBatchNormalizeFusedTypes(self):
-    def test_with_types(data_dtype, stats_dtype, sess):
-      x_np = np.random.rand(4, 64, 64, 4).astype(data_dtype)
-      scale_np = np.ones((4), dtype=stats_dtype)
-      offset_np = np.zeros((4), dtype=stats_dtype)
-      mean_np = np.ones((4), dtype=stats_dtype)
-      inv_sd_np = np.ones((4), dtype=stats_dtype)
-
-      x = array_ops.placeholder(data_dtype, x_np.shape, name="x")
-      scale = array_ops.placeholder(stats_dtype, scale_np.shape, name="scale")
-      offset = array_ops.placeholder(stats_dtype,
-                                     offset_np.shape,
-                                     name="offset")
-      mean = array_ops.placeholder(stats_dtype, mean_np.shape, name="mean")
-      inv_sd = array_ops.placeholder(stats_dtype,
-                                     inv_sd_np.shape,
-                                     name="variance")
-
-      sess.run(variables.global_variables_initializer())
-
-      # Compute forward pass.
-      fwd = gen_nn_ops.fused_batch_norm_v2(x, scale, offset, mean, inv_sd)
-      y, mu, sigma, _, _ = sess.run(
-          fwd, {
-              x: x_np,
-              scale: scale_np,
-              offset: offset_np,
-              mean: mean_np,
-              inv_sd: inv_sd_np
-          })
-
-      self.assertEqual(y.dtype, data_dtype)
-      self.assertEqual(mu.dtype, stats_dtype)
-      self.assertEqual(sigma.dtype, stats_dtype)
-
-      # Compute backward pass.
-      bwd = gen_nn_ops.fused_batch_norm_grad_v2(y, x, scale, mu, sigma)
-      d_x, d_scale, d_offset, _, _ = sess.run(bwd, {x: x_np, scale: scale_np})
-
-      self.assertEqual(d_x.dtype, data_dtype)
-      self.assertEqual(d_scale.dtype, stats_dtype)
-      self.assertEqual(d_offset.dtype, stats_dtype)
-
-    with self.session() as sess:
-      with ops.device("/device:IPU:0"):
-        test_with_types(np.float32, np.float32, sess)
-        test_with_types(np.float16, np.float32, sess)
-
-      with self.assertRaisesRegex(
-          TypeError, "DataType float16 not in list of allowed values"):
-        test_with_types(np.float16, np.float16, sess)
 
 
 if __name__ == "__main__":
