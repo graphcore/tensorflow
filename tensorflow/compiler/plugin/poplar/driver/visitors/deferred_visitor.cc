1dbfa51716c782b24cb2e0a4b219b549e9e28f6c
George Grzegorz Pawelczak <grzegorzp@graphcore.ai>
2020-08-24 08:17:31 +0100

    Use graph functions for function calls
    Summary:
    Use poplar graph functions to improve compile time.
    
    Ref T25541
    
    Test Plan: All tests pass
    
    Reviewers: #tensorflow, vladimirm, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved
    
    Reviewed By: #tensorflow, vladimirm, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved
    
    Maniphest Tasks: T25541
    
    Differential Revision: https://phabricator.sourcevertex.net/D30959

--- tensorflow/compiler/plugin/poplar/driver/visitors/deferred_visitor.cc
+++ tensorflow/compiler/plugin/poplar/driver/visitors/deferred_visitor.cc
@@ -1094,6 +1094,14 @@ poplar::program::Sequence DeferredVisitor::GetSequence(
   return seq;
 }
 
+poplar::program::Sequence DeferredVisitor::GetFunctionCall() {
+  if (!function_) {
+    poplar::program::Sequence seq = GetSequence();
+    function_ = GetMasterGraph(resources_).addFunction(seq);
+  }
+  return poplar::program::Sequence(poplar::program::Call(*function_));
+}
+
 namespace {
 ReallocateInputsInfo GetReallocateInputsInfo(const DeferredArgRBVectors& inputs,
                                              bool reallocate) {
