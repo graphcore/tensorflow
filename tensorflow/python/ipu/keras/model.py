d4b2972c121f0ef9e902deb02520986aa4b64ad3
Alfie Edwards <alfiee@graphcore.ai>
2020-09-11 11:21:14 +0100

    Reverting because of failing test
    Summary: This reverts commit 19c9d062978bf04ff128ac1974b0b16f31ed9ab0.
    
    Test Plan: revert-hammer
    
    Reviewers:
    
    Subscribers:

--- tensorflow/python/ipu/keras/model.py
+++ tensorflow/python/ipu/keras/model.py
@@ -24,6 +24,7 @@ import weakref
 from tensorflow.python.data.experimental.ops import cardinality
 from tensorflow.python.data.ops import dataset_ops
 from tensorflow.python.distribute import distribution_strategy_context
+from tensorflow.python.distribute import input_lib
 from tensorflow.python.eager import def_function
 from tensorflow.python.framework import device as tf_device
 from tensorflow.python.framework import ops
@@ -98,18 +99,10 @@ def _get_dataset_and_count(x, y, batch_size):
   size = adapter.get_size()
 
   if adapter.has_partial_batch():
-    original_dataset = dataset
+    _, _, prefetch_buffer = input_lib._get_dataset_attributes(dataset)  # pylint: disable=protected-access
     dataset = dataset.unbatch()
     # Remove the partial batch from the dataset.
     dataset = dataset.batch(batch_size, drop_remainder=True)
-
-    # Check whether the dataset should be prefetched.
-    prefetch_buffer = None
-    if isinstance(original_dataset, dataset_ops.PrefetchDataset):
-      prefetch_buffer = original_dataset._buffer_size  # pylint: disable=protected-access
-    elif (isinstance(original_dataset, dataset_ops.DatasetV1Adapter) and
-          isinstance(original_dataset._dataset, dataset_ops.PrefetchDataset)):  # pylint: disable=protected-access
-      prefetch_buffer = original_dataset._dataset._buffer_size  # pylint: disable=protected-access
 
     if prefetch_buffer is not None:
       dataset = dataset.prefetch(prefetch_buffer)
