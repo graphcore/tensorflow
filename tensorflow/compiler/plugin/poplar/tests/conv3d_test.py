a5abac2911f356f3ddae3642660ce7a57146d705
George Grzegorz Pawelczak <grzegorzp@graphcore.ai>
2020-09-30 18:49:47 +0100

    Adjust bounds
    Summary: Ref T16509
    
    Test Plan: All tests pass
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, jakeh
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, jakeh
    
    Maniphest Tasks: T16509
    
    Differential Revision: https://phabricator.sourcevertex.net/D33270

--- tensorflow/compiler/plugin/poplar/tests/conv3d_test.py
+++ tensorflow/compiler/plugin/poplar/tests/conv3d_test.py
@@ -78,7 +78,7 @@ class IpuXlaConvTest(xla_test.XLATestCase):
       report.parse_log()
 
       ok = [
-          '__seed*', 'host-exchange-local-copy-', 'Copy_',
+          '__seed*', 'host-exchange-local-copy-',
           'convolution/convolution.*/Conv_3x3x3', 'BiasAdd/fusion/Op/Add'
       ]
       report.assert_all_compute_sets_and_list(ok)
