555314a0826afc75cc64225b5962d352c1584281
Vladimir Menshakov <vladimirm@graphcore.ai>
2020-10-09 17:26:56 +0100

    Readd some compute set expressions to make them less greedy
    Summary: This reverts commit bd124591f470afc3d96411b4436574041c001b70.
    
    Test Plan: revert-hammer
    
    Reviewers:
    
    Subscribers:

--- tensorflow/compiler/plugin/poplar/tests/conv3d_test.py
+++ tensorflow/compiler/plugin/poplar/tests/conv3d_test.py
@@ -172,8 +172,9 @@ class IpuXlaConvTest(xla_test.XLATestCase):
       report.parse_log()
 
       ok = [
-          '__seed*', 'Conv3DBackpropInputV2/fusion/Conv_2x2x2/Convolve',
-          'Conv3DBackpropInputV2/fusion/Conv_2x2x2/Reduce', 'copy*/OnTileCopy-'
+          '__seed*',
+          'Copy_',
+          'Conv3DBackpropInputV2/fusion*/Conv_2x2x2',
       ]
 
       report.assert_all_compute_sets_and_list(ok)
