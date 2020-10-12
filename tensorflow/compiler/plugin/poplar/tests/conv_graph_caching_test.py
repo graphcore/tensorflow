d8f7a8a44c1618e95b0643354a9be17d77b6fe0e
Vladimir Menshakov <vladimirm@graphcore.ai>
2020-10-12 17:37:49 +0100

    Revert "Change compute set expressions to make them more explicit in some tests."
    Summary: This reverts commit 83448468a51dda8b5e7c0fe7e35a78b194188119.
    
    Test Plan: revert-hammer
    
    Reviewers:
    
    Subscribers:

--- tensorflow/compiler/plugin/poplar/tests/conv_graph_caching_test.py
+++ tensorflow/compiler/plugin/poplar/tests/conv_graph_caching_test.py
@@ -227,11 +227,11 @@ class ConvGraphCachingTest(xla_test.XLATestCase):
       # pylint: disable=line-too-long
       ok = [
           '__seed*',
-          'copy*/OnTileCopy',
+          'Copy_',
           'vs/conv1/Conv2D/convolution.*/Conv_1x1',
           'Sum/reduce.*/ReduceOnTile/InToIntermediateNoExchange/Reduce',
           'Sum/reduce.*/ReduceFinalStage/IntermediateToOutput/Reduce',
-          'gradients/vs/conv1/Conv2D_grad/Conv2DBackpropFilter/fusion.*/Conv_4x4/Convolve',
+          'gradients/vs/conv1/Conv2D_grad/Conv2DBackpropFilter/fusion.*/Conv_4x4/*',
           'gradients/vs/conv1/Conv2D_grad/Conv2DBackpropFilter/fusion.*/Transpose',
           'gradients/vs/conv1/Conv2D_grad/Conv2DBackpropFilter/fusion.*/AddTo',
           'gradients/vs/conv3/Conv2D_grad/Conv2DBackpropInput/fusion*/*Transpose',
@@ -287,10 +287,11 @@ class ConvGraphCachingTest(xla_test.XLATestCase):
           'Copy_',
           'Sum/reduce.*/ReduceOnTile/InToIntermediateNoExchange/Reduce',
           'Sum/reduce.*/ReduceFinalStage/IntermediateToOutput/Reduce',
-          'gradients/vs/conv1/Conv2D_grad/Conv2DBackpropFilter/fusion.*/Conv_4x4',
-          'gradients/vs/conv1/Conv2D_grad/Conv2DBackpropFilter/fusion.*/AddTo',
-          'gradients/vs/conv3/Conv2D_grad/Conv2DBackpropInput/fusion/WeightTranspose',
+          'gradients/vs/conv*/Conv2D_grad/Conv2DBackpropFilter/fusion.*/Conv_4x4',
+          'gradients/vs/conv*/Conv2D_grad/Conv2DBackpropFilter/fusion.*/AddTo',
           'vs/conv*/Conv2D/convolution*/Conv_1x1',
+          'gradients/vs/conv*/Conv2D_grad/Conv2DBackpropInput/fusion*/*Transpose',
+          'gradients/vs/conv*/Conv2D_grad/Conv2DBackpropFilter/fusion.*/Transpose',
       ]
       # pylint: enable=line-too-long
       report.assert_all_compute_sets_and_list(ok)
