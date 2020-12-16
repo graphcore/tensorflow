fba1f89043a5023fc3c427caccfbb06befc48631
Mark Fowden <markf@graphcore.ai>
2020-12-16 13:40:56 +0000

    Add Uniform and LogUniform IPU Candidate Sampling ops
    Summary:
    This diff adds simple implementations of the uniform and log uniform candidate sampling ops for the IPU device.
    
    The candidate samplers can either be unique or not unique (sampling with or without replacement).
    For non-unique, we call down to poprand::uniform / logUniform.
    For unique, we use a sequential sampling approach where we continually sample and discard any duplicates. We use a bitmask for constant time lookup + insertion which is N bits large for N possible classes. This algorithm is probably quadratic in the uniform case and exponential in the log uniform case as the number of samples (k) approaches the number of classes, but the candidate sampling ops are generally used for negative sampling techniques where we expect k << N to be useful. The cycle estimates are the minimum possible runtime of the vertices.
    
    The op also involves calculation of the expectation for the samples and some input tensor. These are done at run-time using hardcoded popops::Expr formulas (but they could possibly also be pre-calculated, stored in remote memory and gathered at runtime). We map the samples linearly so these map operations are efficient.
    
    Each replica takes different samples so they train differently. If the op is seeded, it will give the same samples every time it's executed in the graph (e.g. in a repeat loop), but different ones on each replica.
    
    Ref T16999
    
    Test Plan: CI. Added compile checks in CI. Since random numbers cannot be trusted on the IPUModel (and the unique vertices for this op are no-ops), will add hardware tests that check the distributions and compare vs. the CPU in a separate diff.
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep
    
    Subscribers: georgep
    
    Maniphest Tasks: T16999
    
    Differential Revision: https://phabricator.sourcevertex.net/D37754

--- tensorflow/compiler/plugin/poplar/kernels/poprand/dropout_xla.cc
+++ tensorflow/compiler/plugin/poplar/kernels/poprand/dropout_xla.cc
@@ -15,6 +15,7 @@ limitations under the License.
 
 #include "absl/container/flat_hash_set.h"
 #include "tensorflow/compiler/plugin/poplar/driver/poplar_platform.h"
+#include "tensorflow/compiler/plugin/poplar/driver/tools/xla_util.h"
 #include "tensorflow/compiler/plugin/poplar/driver/trace.pb.h"
 #include "tensorflow/compiler/plugin/poplar/driver/xla_ipu_common.h"
 #include "tensorflow/compiler/plugin/poplar/kernels/custom_kernels_util.h"
@@ -24,7 +25,7 @@ limitations under the License.
 #include "tensorflow/compiler/tf2xla/xla_helpers.h"
 #include "tensorflow/compiler/tf2xla/xla_op_kernel.h"
 #include "tensorflow/compiler/tf2xla/xla_op_registry.h"
-#include "tensorflow/compiler/xla/literal_util.h"
+
 #include "tensorflow/core/framework/node_def_util.h"
 #include "tensorflow/core/framework/op.h"
 #include "tensorflow/core/framework/op_kernel.h"
@@ -37,7 +38,6 @@ limitations under the License.
 using namespace xla::poplarplugin;
 
 namespace tensorflow {
-xla::Shape GetSeedShape() { return xla::ShapeUtil::MakeShape(xla::S32, {2}); }
 
 class DropoutOp : public XlaOpKernel, public IpuOpKernel {
  public:
@@ -71,65 +71,32 @@ class DropoutOp : public XlaOpKernel, public IpuOpKernel {
 
     // We are outputting both the output of the operation and the seed used so
     // we can reuse the seed in the backprop pass.
+    xla::XlaOp seed = GetSeed(ctx, b);
+    xla::Shape seed_shape = b->GetShape(seed).ConsumeValueOrDie();
     const xla::Shape output_tuple_shape =
-        xla::ShapeUtil::MakeTupleShape({xla_shape, GetSeedShape()});
+        xla::ShapeUtil::MakeTupleShape({xla_shape, seed_shape});
 
-    xla::XlaOp call_output = xla::CustomCall(
-        b, PoplarOp_Name(PoplarOp::Dropout), {input, GetSeed(ctx, b)},
-        output_tuple_shape, attribute_map_.Serialise());
+    xla::XlaOp call_output =
+        xla::CustomCall(b, PoplarOp_Name(PoplarOp::Dropout), {input, seed},
+                        output_tuple_shape, attribute_map_.Serialise());
 
     // The actual dropout output.
     xla::XlaOp output = xla::GetTupleElement(call_output, 0);
 
     // Save the seed used so we can reference it in the backwards pass.
-    xla::XlaOp seed = xla::GetTupleElement(call_output, 1);
+    xla::XlaOp seed_output = xla::GetTupleElement(call_output, 1);
 
     ctx->SetOutput(0, output);
-    ctx->SetOutput(1, seed);
+    ctx->SetOutput(1, seed_output);
   }
 
  protected:
   virtual xla::XlaOp GetSeed(XlaOpKernelContext* ctx,
                              xla::XlaBuilder* builder) {
-    const xla::Shape seed_shape = GetSeedShape();
-    // Create a seed and hash it with the replica index to make sure each
-    // replica has a different seed.
+    const xla::Shape seed_shape = xla::ShapeUtil::MakeShape(xla::S32, {2});
     xla::XlaOp seed = xla::CustomCall(builder, PoplarOp_Name(PoplarOp::Seed),
                                       {}, seed_shape, "");
-    xla::XlaOp replica_index =
-        xla::CustomCall(builder, PoplarOp_Name(PoplarOp::ReplicationIndex), {},
-                        xla::ShapeUtil::MakeShape(xla::S32, {}), "");
-
-    // Hashing function based on
-    // tensorflow/core/grappler/graph_analyzer/hash_tools.h
-    // seed = seed ^ (replica_index + 0x9E3779B9U + (seed << 6) + (seed >> 2))
-    seed = xla::BitcastConvertType(seed, xla::U32);
-    replica_index = xla::BitcastConvertType(replica_index, xla::U32);
-
-    xla::XlaOp large_constant = xla::ConstantLiteral(
-        builder, xla::LiteralUtil::CreateR0<uint32>(0x9E3779B9U));
-    xla::XlaOp constant_six = xla::ConstantLiteral(
-        builder, xla::LiteralUtil::CreateR1<uint32>({6U, 6U}));
-    xla::XlaOp constant_two = xla::ConstantLiteral(
-        builder, xla::LiteralUtil::CreateR1<uint32>({2U, 2U}));
-
-    // seed << 6
-    xla::XlaOp shift_left = xla::ShiftLeft(seed, constant_six);
-    // seed >> 2
-    xla::XlaOp shift_right = xla::ShiftRightLogical(seed, constant_two);
-
-    // Compute the rhs of xor.
-    // replica_index + 0x9E3779B9U
-    xla::XlaOp rhs = xla::Add(replica_index, large_constant);
-    rhs = xla::Broadcast(rhs, seed_shape.dimensions());
-
-    // replica_index + 0x9E3779B9U + (seed << 6)
-    rhs = xla::Add(rhs, shift_left);
-    // replica_index + 0x9E3779B9U + (seed << 6) + (seed >> 2)
-    rhs = xla::Add(rhs, shift_right);
-
-    seed = xla::Xor(seed, rhs);
-    return xla::BitcastConvertType(seed, seed_shape.element_type());
+    return HashSeedWithReplicaIndex(seed);
   }
 
  private:
