be252a5d5e4220a918c7c8d9d1cbb9edf15365c9
George Grzegorz Pawelczak <grzegorzp@graphcore.ai>
2021-01-06 17:46:56 +0000

    Dataflow analysis - add classes for storing buffer information
    Summary:
    Adds classes to represent buffers at Hlo level to be used by the dataflow
    analysis.
    
    Defines classes:
    HloPoplarPosition - this allows to define a fine grain position at any position in the
    Hlo graph. For most operations this is triavially the instruction itself, unless tuples
    are involed.
    
    HloPoplarUse and its derrivatives - allows us to define how a given input is handled by
    an instruction and also defines how the input is aliased in the output.
    
    HloPoplarBuffer - used to define a `poplar::Tensor` like buffer at a certain position.
    Also supports locality so that dataflow analysis can track remote buffers.
    
    HloPoplarBufferSet - A set of above when a single 'HloPoplarPosition' contains multiple
    'HloPoplarBuffer'
    
    InstructionPoplarBufferSet - Used to represent all the outputs of an instruction using
    a ShapeTree internally.
    
    Ref T10387
    
    Test Plan: Added new test
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, jackh, vladimirm
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, jackh, vladimirm
    
    Subscribers: jackh
    
    Maniphest Tasks: T10387
    
    Differential Revision: https://phabricator.sourcevertex.net/D38465

---
+++ tensorflow/compiler/plugin/poplar/driver/tools/hlo_poplar_buffer.h
@@ -0,0 +1,264 @@
+/* Copyright 2021 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef TENSORFLOW_COMPILER_PLUGIN_POPLAR_DRIVER_TOOLS_HLO_POPLAR_BUFFER_H_
+#define TENSORFLOW_COMPILER_PLUGIN_POPLAR_DRIVER_TOOLS_HLO_POPLAR_BUFFER_H_
+
+#include <string>
+#include <vector>
+
+#include "absl/types/span.h"
+#include "tensorflow/compiler/xla/service/hlo_instruction.h"
+#include "tensorflow/compiler/xla/shape_tree.h"
+#include "tensorflow/compiler/xla/shape_util.h"
+
+// This file is based on tensorflow/compiler/xla/service/hlo_value.h but adopted
+// to meet Poplar specific needs (for example the static allocations).
+
+namespace xla {
+namespace poplarplugin {
+
+// Abstraction which identifies a specific point in the XLA graph. An
+// HloPoplarPosition specifies a ShapeIndex within the output of a specific
+// instruction.
+struct HloPoplarPosition {
+  HloInstruction* instruction;
+  ShapeIndex index;
+
+  // Returns the shape at this position.
+  const Shape& shape() const;
+
+  std::string ToString() const;
+
+  bool operator==(const HloPoplarPosition& other) const;
+  bool operator!=(const HloPoplarPosition& other) const;
+  bool operator<(const HloPoplarPosition& other) const;
+};
+
+std::ostream& operator<<(std::ostream& out, const HloPoplarPosition& position);
+
+enum class HloPoplarUseKind {
+  // A use of an HloPoplarBuffer which reads a buffer, but it is not aliased by
+  // any outputs.
+  kNoAlias = 0,
+  // A use of an HloPoplarBuffer which reads a buffer and outputs alias that
+  // buffer without modifying the values in it.
+  kAliasReadOnly,
+  // A use of an HloPoplarBuffer which reads a buffer, modifies the values and
+  // outputs alias that buffer.
+  kAliasReadWrite,
+};
+
+// Base class for defining a single use of a buffer.
+class HloPoplarUse {
+ public:
+  // Instruction at which the buffer is used.
+  HloInstruction* instruction() const { return instruction_; }
+
+  // The operand number in which the buffer is appears.
+  int64 operand_number() const { return operand_number_; }
+
+  // The shape index within the operand in which the buffer appears.
+  const ShapeIndex& operand_index() const { return operand_index_; }
+
+  // Get what kind of usage this is.
+  HloPoplarUseKind kind() const { return kind_; }
+
+  virtual std::string ToString() const = 0;
+
+ protected:
+  HloPoplarUse(HloInstruction* instruction, int64 operand_number,
+               const ShapeIndex& operand_index, HloPoplarUseKind kind);
+
+ private:
+  HloInstruction* instruction_;
+  const int64 operand_number_;
+  const ShapeIndex operand_index_;
+  const HloPoplarUseKind kind_;
+};
+
+std::ostream& operator<<(std::ostream& out, const HloPoplarUse& use);
+
+// A class for defining a single use of a buffer which is not aliased by any
+// output.
+class HloPoplarNoAliasUse : public HloPoplarUse {
+ public:
+  HloPoplarNoAliasUse(HloInstruction* instruction, int64 operand_number,
+                      const ShapeIndex& operand_index);
+
+  std::string ToString() const override;
+};
+
+// A base class for defining a single use of a buffer which is aliased by some
+// outputs.
+class HloPoplarAliasUseBase : public HloPoplarUse {
+ public:
+  // The shape index within the instruction in which the buffer appears as an
+  // output.
+  const std::vector<ShapeIndex>& output_indices() const {
+    return output_indices_;
+  }
+
+  std::string ToString() const override;
+
+ protected:
+  HloPoplarAliasUseBase(HloInstruction* instruction, int64 operand_number,
+                        const ShapeIndex& operand_index,
+                        const std::vector<ShapeIndex> output_indices,
+                        HloPoplarUseKind kind);
+
+ private:
+  const std::vector<ShapeIndex> output_indices_;
+};
+
+// A class for defining a single use of a buffer which is aliased by outputs
+// however the values are *not* modified.
+class HloPoplarAliasReadOnlyUse : public HloPoplarAliasUseBase {
+ public:
+  HloPoplarAliasReadOnlyUse(HloInstruction* instruction, int64 operand_number,
+                            const ShapeIndex& operand_index,
+                            const std::vector<ShapeIndex> output_indices);
+};
+
+// A class for defining a single use of a buffer which is aliased by outputs
+// however the values are modified.
+class HloPoplarAliasReadWriteUse : public HloPoplarAliasUseBase {
+ public:
+  HloPoplarAliasReadWriteUse(HloInstruction* instruction, int64 operand_number,
+                             const ShapeIndex& operand_index,
+                             const std::vector<ShapeIndex> output_indices);
+};
+
+enum class BufferLocality {
+  // Indicates that the buffer is stored in device memory.
+  kDeviceMemory = 0,
+  // Indicated that the buffer is stored in remote memory managed by Poplar.
+  kRemoteMemory,
+};
+
+// Class used to represent a single buffer tensor.
+class HloPoplarBuffer {
+ public:
+  using Id = int64;
+  // Predicate comparing HloPoplarBuffers by increasing id, useful for
+  // std::sort.
+  static bool IdLessThan(const HloPoplarBuffer* a, const HloPoplarBuffer* b) {
+    return a->id() < b->id();
+  }
+
+  // Predicate comparing HloPoplarBuffers by equal id, useful for std::unique.
+  static bool IdEqual(const HloPoplarBuffer* a, const HloPoplarBuffer* b) {
+    return a->id() == b->id();
+  }
+
+  HloPoplarBuffer(Id id, const HloPoplarPosition& defining_position,
+                  BufferLocality locality);
+
+  Id id() const { return id_; }
+
+  const HloPoplarPosition& defining_position() const {
+    return defining_position_;
+  }
+
+  BufferLocality locality() const { return locality_; }
+
+  HloInstruction* instruction() const {
+    return defining_position().instruction;
+  }
+
+  const ShapeIndex& index() const { return defining_position().index; }
+
+  const Shape& shape() const { return instruction()->shape(); }
+
+  bool operator==(const HloPoplarBuffer& other) const;
+  bool operator!=(const HloPoplarBuffer& other) const;
+
+  std::string ToString() const;
+
+ private:
+  const Id id_;
+  const HloPoplarPosition defining_position_;
+  const BufferLocality locality_;
+};
+
+std::ostream& operator<<(std::ostream& out, const HloPoplarBuffer& buffer);
+
+// A class representing the possible set of HloPoplarBuffer at a particular
+// position in the XLA graph.
+class HloPoplarBufferSet {
+ public:
+  HloPoplarBufferSet() = default;
+  explicit HloPoplarBufferSet(absl::Span<const HloPoplarBuffer* const> buffers);
+
+  // Return the vector of HloPoplarBuffer in the set. Buffers in the vector are
+  // unique and stably sorted by buffer id.
+  const std::vector<const HloPoplarBuffer*>& buffers() const {
+    return buffers_;
+  }
+
+  int64 size() const { return buffers_.size(); }
+
+  // Returns a unique buffer (if there is one).
+  const HloPoplarBuffer& GetUniqueBuffer() const;
+
+  // Add a buffer to the current set - returns true iff a buffer was added.
+  bool AddBuffer(const HloPoplarBuffer* buffer);
+
+  bool operator==(const HloPoplarBufferSet& other) const;
+  bool operator!=(const HloPoplarBufferSet& other) const;
+
+  std::string ToString() const;
+
+ private:
+  // Sorts buffers_ and removes duplicates. This should be called after adding
+  // any elements to buffers_.
+  void SortAndUniquifyBuffers();
+
+  // HloPoplarBuffers sorted by HloPoplarBuffer::Id.
+  std::vector<const HloPoplarBuffer*> buffers_;
+};
+
+std::ostream& operator<<(std::ostream& out,
+                         const HloPoplarBufferSet& buffer_set);
+
+// A class collecting the HloPoplarBuffers which might be contained in the
+// output of an HLO instruction. For array-shaped instructions, an
+// InstructionPoplarBufferSet trivially holds a single HloPoplarBufferSet.
+// Tuple-shaped InstructionPoplarBufferSets hold multiple HloPoplarBufferSets.
+class InstructionPoplarBufferSet {
+ public:
+  explicit InstructionPoplarBufferSet(const Shape& shape);
+
+  // Set the buffer set for a particular output index.
+  void SetOutputBufferSet(const ShapeIndex& output_index,
+                          const HloPoplarBufferSet& buffer_set);
+
+  // Get the buffer set for a particular output index.
+  const HloPoplarBufferSet& GetOutputBufferSet(const ShapeIndex& output_index);
+
+  std::string ToString() const;
+
+ private:
+  const Shape shape_;
+  ShapeTree<HloPoplarBufferSet> buffer_sets_;
+};
+
+std::ostream& operator<<(
+    std::ostream& out,
+    const InstructionPoplarBufferSet& instruction_buffer_set);
+}  // namespace poplarplugin
+}  // namespace xla
+
+#endif  // TENSORFLOW_COMPILER_PLUGIN_POPLAR_DRIVER_TOOLS_HLO_POPLAR_BUFFER_H_
