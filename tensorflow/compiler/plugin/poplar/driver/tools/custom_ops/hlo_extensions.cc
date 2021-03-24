d05e7c0c5b30e0bc9f880b640ae4aa2f5cb7b4c1
Babak Khataee <babakk@graphcore.ai>
2021-03-24 18:07:35 +0000

    HloInstruction extension registration system.
    Summary:
    This change adds a system for registering extensions for HloInstructions, so that we can add custom functionality to existing HloInstructions in a single place that we can reuse elsewhere. The first application of this is AllocationFinder, which has been refactored to use the extensions instead of using custom visitors to achieve the same result. The initial extensions that have been added/implemented are based on the needs of this class.
    
    Extension implementations are stored alongside custom instructions, rather than with the hlo custom ops. This is to avoid an implicit dependency between the optimizers and those ops.
    
    The intention is for calls to extensions to go via CallHloInstructionExtension, which will dispatch it to either the appropriate registered function, or in the case of HloPoplarInstructions to the appropriate member function.
    
    HloOps can opt into providing extensions by registering themselves with REGISTER_HLO_OP_WITH_EXTENSIONS, which takes an additional function for registering any extensions.
    
    At the moment the extensions don't take any arguments (aside from the instruction pointer) but this should be easy to change in the future should we need it.
    
    TF 2.1 Only
    Fix T34013
    
    Test Plan: Added instruction_extensions_test.cc
    
    Reviewers: #tensorflow, simonl, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep, vladimirm, jakeh, hakons
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep, jakeh, hakons
    
    Maniphest Tasks: T34013
    
    Differential Revision: https://phabricator.sourcevertex.net/D41571

---
+++ tensorflow/compiler/plugin/poplar/driver/tools/custom_ops/hlo_extensions.cc
@@ -0,0 +1,122 @@
+/* Copyright 2021 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#include "tensorflow/compiler/xla/service/hlo_opcode.h"
+
+#include "tensorflow/compiler/plugin/poplar/driver/tools/hlo_instruction_extensions.h"
+#include "tensorflow/compiler/plugin/poplar/driver/tools/matcher_predicates.h"
+
+namespace xla {
+namespace poplarplugin {
+namespace {
+
+void RegisterSharedExtensions(HloOpcode opcode) {
+  auto allocating_output = [](HloInstruction*) { return true; };
+  RegisterHloInstructionExtension<AllocatingOutputExtension>(opcode,
+                                                             allocating_output);
+}
+REGISTER_HLO_INST_EXTENSIONS(kConstant, RegisterSharedExtensions);
+REGISTER_HLO_INST_EXTENSIONS(kInfeed, RegisterSharedExtensions);
+REGISTER_HLO_INST_EXTENSIONS(kParameter, RegisterSharedExtensions);
+REGISTER_HLO_INST_EXTENSIONS(kReduce, RegisterSharedExtensions);
+REGISTER_HLO_INST_EXTENSIONS(kReduceWindow, RegisterSharedExtensions);
+REGISTER_HLO_INST_EXTENSIONS(kRng, RegisterSharedExtensions);
+
+void RegisterFuseExtensions(HloOpcode opcode) {
+  auto allocating_output = [](HloInstruction* inst) {
+    return (IsWideConstant(inst) || IsReductionFusion(inst));
+  };
+  RegisterHloInstructionExtension<AllocatingOutputExtension>(opcode,
+                                                             allocating_output);
+}
+REGISTER_HLO_INST_EXTENSIONS(kFusion, RegisterFuseExtensions);
+
+void RegisterDotExtensions(HloOpcode opcode) {
+  auto allocating_indices = [](HloInstruction* inst) {
+    absl::flat_hash_set<int64> indices;
+    for (auto i = 0u; i < inst->operand_count(); ++i) {
+      indices.insert(i);
+    }
+
+    return indices;
+  };
+
+  RegisterHloInstructionExtension<AllocatingIndicesExtension>(
+      opcode, allocating_indices);
+}
+REGISTER_HLO_INST_EXTENSIONS(kDot, RegisterDotExtensions);
+
+void RegisterCholeskyExtensions(HloOpcode opcode) {
+  auto allocating_indices = [](HloInstruction*) {
+    return absl::flat_hash_set<int64>{0};
+  };
+  RegisterHloInstructionExtension<AllocatingIndicesExtension>(
+      opcode, allocating_indices);
+
+  auto allocating_output = [](HloInstruction*) { return true; };
+  RegisterHloInstructionExtension<AllocatingOutputExtension>(opcode,
+                                                             allocating_output);
+}
+REGISTER_HLO_INST_EXTENSIONS(kCholesky, RegisterCholeskyExtensions);
+
+void RegisterScatterExtensions(HloOpcode opcode) {
+  auto allocating_indices = [](HloInstruction*) {
+    return absl::flat_hash_set<int64>{0, 1, 2};
+  };
+  RegisterHloInstructionExtension<AllocatingIndicesExtension>(
+      opcode, allocating_indices);
+}
+REGISTER_HLO_INST_EXTENSIONS(kScatter, RegisterScatterExtensions);
+
+void RegisterGatherExtensions(HloOpcode opcode) {
+  auto allocating_indices = [](HloInstruction*) {
+    return absl::flat_hash_set<int64>{0, 1};
+  };
+  RegisterHloInstructionExtension<AllocatingIndicesExtension>(
+      opcode, allocating_indices);
+}
+REGISTER_HLO_INST_EXTENSIONS(kGather, RegisterGatherExtensions);
+
+void RegisterTriangularSolveExtensions(HloOpcode opcode) {
+  auto allocating_indices = [](HloInstruction*) {
+    return absl::flat_hash_set<int64>{0, 1};
+  };
+  RegisterHloInstructionExtension<AllocatingIndicesExtension>(
+      opcode, allocating_indices);
+
+  auto allocating_output = [](HloInstruction*) { return true; };
+  RegisterHloInstructionExtension<AllocatingOutputExtension>(opcode,
+                                                             allocating_output);
+}
+REGISTER_HLO_INST_EXTENSIONS(kTriangularSolve,
+                             RegisterTriangularSolveExtensions);
+
+void RegisterConvolutionExtensions(HloOpcode opcode) {
+  auto allocating_indices = [](HloInstruction* inst) {
+    absl::flat_hash_set<int64> indices;
+    for (auto i = 0u; i < inst->operand_count(); ++i) {
+      indices.insert(i);
+    }
+
+    return indices;
+  };
+
+  RegisterHloInstructionExtension<AllocatingIndicesExtension>(
+      opcode, allocating_indices);
+}
+REGISTER_HLO_INST_EXTENSIONS(kConvolution, RegisterConvolutionExtensions);
+
+}  // namespace
+}  // namespace poplarplugin
+}  // namespace xla
