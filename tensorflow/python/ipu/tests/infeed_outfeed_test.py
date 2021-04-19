c1203aa4af332da8691a0acc456bbec579ace847
George Pawelczak <grzegorzp@graphcore.ai>
2021-04-19 21:14:35 +0100

    Update bounds
    Summary: Ref T16509
    
    Test Plan: All tests pass
    
    Reviewers: jakeh, #tensorflow, simonl, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, vladimirm, jamesn
    
    Reviewed By: jakeh, #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, vladimirm, jamesn
    
    Maniphest Tasks: T16509
    
    Differential Revision: https://phabricator.sourcevertex.net/D44088

--- tensorflow/python/ipu/tests/infeed_outfeed_test.py
+++ tensorflow/python/ipu/tests/infeed_outfeed_test.py
@@ -1182,7 +1182,7 @@ class InfeedOutfeedTest(test_util.TensorFlowTestCase):
 
       report.parse_log()
       report.assert_each_tile_memory_is_less_than(4234, tolerance=0.1)
-      report.assert_total_tile_memory(373536, tolerance=0.1)
+      report.assert_total_tile_memory(333745, tolerance=0.1)
 
       total_outfeeds = 0
       for s in report.get_execution_reports()[0]['simulation']['steps']:
@@ -1238,7 +1238,7 @@ class InfeedOutfeedTest(test_util.TensorFlowTestCase):
 
       report.parse_log()
       report.assert_each_tile_memory_is_less_than(4234, tolerance=0.1)
-      report.assert_total_tile_memory(373536, tolerance=0.1)
+      report.assert_total_tile_memory(333745, tolerance=0.1)
 
       total_outfeeds = 0
       for s in report.get_execution_reports()[0]['simulation']['steps']:
