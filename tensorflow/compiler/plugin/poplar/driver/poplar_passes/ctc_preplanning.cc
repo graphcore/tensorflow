73562fb1fe6630475216c1c2aa162746935e18fa
Samuel Hornby <samuelh@graphcore.ai>
2021-04-27 13:30:29 +0100

    Provide CTC beam search functions in python
    Summary:
    Call through the the poplibs CTC beam search function from a python
    custom op
    
    Test Plan:
    included though tests can't be fully enabled until
    poplibs implementation is finished
    
    Reviewers: #tensorflow, simonl, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep, alfiee
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep
    
    Subscribers: georgep
    
    Maniphest Tasks: T36279
    
    Differential Revision: https://phabricator.sourcevertex.net/D43427

--- tensorflow/compiler/plugin/poplar/driver/poplar_passes/ctc_preplanning.cc
+++ tensorflow/compiler/plugin/poplar/driver/poplar_passes/ctc_preplanning.cc
@@ -21,42 +21,80 @@ limitations under the License.
 #include "tensorflow/compiler/plugin/poplar/driver/tools/matcher_predicates.h"
 #include "tensorflow/compiler/xla/service/hlo_casting_utils.h"
 
+#include <popnn/CTCInference.hpp>
 #include <popnn/CTCLoss.hpp>
 
 namespace xla {
 namespace poplarplugin {
 
+static bool IsCTCInferenceInst(const HloInstruction* inst) {
+  return IsPoplarInstruction(PoplarOp::CTCBeamSearchWithLogits)(inst) ||
+         IsPoplarInstruction(PoplarOp::CTCBeamSearchWithLogProbs)(inst);
+}
+
+static bool HasCTCPlan(const HloInstruction* inst) {
+  return IsPoplarInstruction(PoplarOp::CTCLossWithLogits)(inst) ||
+         IsPoplarInstruction(PoplarOp::CTCLossWithLogProbs)(inst) ||
+         IsCTCInferenceInst(inst);
+}
+
+static StatusOr<popnn::ctc::Plan> GetPlanForLossInstruction(
+    const HloInstruction* inst, CompilerResources& resources) {
+  poplar::Graph& graph = GetGraph(resources, inst);
+  const HloCTCLossInstructionBase* ctc_inst =
+      Cast<HloCTCLossInstructionBase>(inst);
+  TF_ASSIGN_OR_RETURN(poplar::Type in_dtype,
+                      PoplarDataType(ctc_inst->in_dtype()));
+  TF_ASSIGN_OR_RETURN(poplar::Type out_dtype,
+                      PoplarDataType(ctc_inst->out_dtype()));
+
+  const auto& data_shape = ctc_inst->operand(0)->shape();
+  const auto& labels_shape = ctc_inst->operand(1)->shape();
+  const int64 max_time = data_shape.dimensions(0);
+  const int64 batch_size = data_shape.dimensions(1);
+  const int64 num_classes = data_shape.dimensions(2);
+  const int64 max_label_length = labels_shape.dimensions(1);
+  return popnn::ctc::plan(graph, in_dtype, out_dtype, batch_size, max_time,
+                          max_label_length, num_classes);
+}
+
+static StatusOr<popnn::ctc::Plan> GetPlanForInferenceInstruction(
+    const HloInstruction* inst, CompilerResources& resources) {
+  poplar::Graph& graph = GetGraph(resources, inst);
+  const HloCTCInferenceInstructionBase* ctc_inst =
+      Cast<HloCTCInferenceInstructionBase>(inst);
+  TF_ASSIGN_OR_RETURN(poplar::Type in_dtype,
+                      PoplarDataType(ctc_inst->in_dtype()));
+  const auto& data_shape = ctc_inst->operand(0)->shape();
+  const int64 max_time = data_shape.dimensions(0);
+  const int64 batch_size = data_shape.dimensions(1);
+  const int64 num_classes = data_shape.dimensions(2);
+  const int64 beam_width = ctc_inst->beam_width();
+  return popnn::ctc_infer::plan(graph, in_dtype, batch_size, max_time,
+                                num_classes, beam_width);
+}
+
 /*
  * This visitor iterates over all ops in the graph, and creates a Plan for
- * every HloCTCLossInstruction found.
+ * every HloCTCLossInstruction and HloCTCInferenceInstruction found.
  */
 StatusOr<bool> CTCPreplanning::Run(HloModule* module) {
   VLOG(2) << "Preplanning CTC operations.";
 
   for (const HloComputation* comp : module->computations()) {
     for (const HloInstruction* inst : comp->instructions()) {
-      if (IsPoplarInstruction(PoplarOp::CTCLossWithLogits)(inst) ||
-          IsPoplarInstruction(PoplarOp::CTCLossWithLogProbs)(inst)) {
-        poplar::Graph& graph = GetGraph(resources_, inst);
-        const HloCTCLossInstructionBase* ctc_inst =
-            Cast<HloCTCLossInstructionBase>(inst);
-        TF_ASSIGN_OR_RETURN(poplar::Type in_dtype,
-                            PoplarDataType(ctc_inst->in_dtype()));
-        TF_ASSIGN_OR_RETURN(poplar::Type out_dtype,
-                            PoplarDataType(ctc_inst->out_dtype()));
+      if (HasCTCPlan(inst)) {
+        auto plan = IsCTCInferenceInst(inst)
+                        ? GetPlanForInferenceInstruction(inst, resources_)
+                        : GetPlanForLossInstruction(inst, resources_);
 
-        const auto& data_shape = ctc_inst->operand(0)->shape();
-        const auto& labels_shape = ctc_inst->operand(1)->shape();
-        const int64 max_time = data_shape.dimensions(0);
-        const int64 batch_size = data_shape.dimensions(1);
-        const int64 num_classes = data_shape.dimensions(2);
-        const int64 max_label_length = labels_shape.dimensions(1);
+        if (!plan.ok()) {
+          return plan.status();
+        }
 
         resources_.ctc_plans.insert(
             std::pair<const HloInstruction*, const popnn::ctc::Plan>(
-                inst,
-                popnn::ctc::plan(graph, in_dtype, out_dtype, batch_size,
-                                 max_time, max_label_length, num_classes)));
+                inst, std::move(plan.ValueOrDie())));
       }
     }
   }
