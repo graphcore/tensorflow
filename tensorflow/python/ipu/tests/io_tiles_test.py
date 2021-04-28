eeb869a99cff3c678f1983f497fdf39fe95d4187
Alfie Edwards <alfiee@graphcore.ai>
2021-04-28 17:10:53 +0100

    Only allocating IO tiles on devices with io instructions
    Summary: Iterate over instructions in the IO tileset and enumerate the devices they are sharded to. Use this information to only allocate io tiles on devices with io instructions.
    
    Test Plan: Test creates two matmuls, one on a device with an infeed, and one on a device with no io operations. Then checks the amount of tiles each gets mapped to.
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, jakeh, hakons
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, hakons
    
    Maniphest Tasks: T36242
    
    Differential Revision: https://phabricator.sourcevertex.net/D44099

--- tensorflow/python/ipu/tests/io_tiles_test.py
+++ tensorflow/python/ipu/tests/io_tiles_test.py
@@ -24,7 +24,9 @@ from tensorflow.python.framework import test_util
 from tensorflow.python.ipu import ipu_compiler
 from tensorflow.python.ipu import ipu_infeed_queue
 from tensorflow.python.ipu import ipu_outfeed_queue
+from tensorflow.python.ipu.scopes import ipu_shard
 from tensorflow.python.ipu import utils as ipu_utils
+from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import math_ops
 from tensorflow.python.platform import test
 
@@ -32,6 +34,7 @@ from tensorflow.python.platform import test
 class IoTilesTest(test_util.TensorFlowTestCase, parameterized.TestCase):
   @parameterized.parameters([True, False])
   @test_util.deprecated_graph_mode_only
+  @tu.test_may_use_ipus_or_model(num_ipus=1)
   def testTensorPlacementAndExchanges(self, buffer_fits_on_io_tiles):
     tiles_per_ipu = 1216
     num_io_tiles = 32
@@ -114,6 +117,7 @@ class IoTilesTest(test_util.TensorFlowTestCase, parameterized.TestCase):
             exchanges)
 
   @test_util.deprecated_graph_mode_only
+  @tu.test_may_use_ipus_or_model(num_ipus=1)
   def testTensorLayoutOnIoTiles(self):
     tiles_per_ipu = 1216
     num_io_tiles = 32
@@ -165,6 +169,201 @@ class IoTilesTest(test_util.TensorFlowTestCase, parameterized.TestCase):
       for t in io_tensors:
         self.assertEqual(len(t.tiles), 1)
         self.assertEqual(t.tiles[0].num_elements, len(data))
+
+  @test_util.deprecated_graph_mode_only
+  @tu.test_may_use_ipus_or_model(num_ipus=2)
+  def testIoTilesOnlyCreatedOnDevicesWithIoInstructions(self):
+    tiles_per_ipu = 1216
+    num_io_tiles = 32
+    num_compute_tiles = tiles_per_ipu - num_io_tiles
+
+    data = np.ones((tiles_per_ipu, tiles_per_ipu), dtype=np.float32)
+    dataset = dataset_ops.Dataset.from_tensors(data)
+    infeed = ipu_infeed_queue.IPUInfeedQueue(dataset, feed_name="infeed")
+
+    def my_net():
+      with ipu_shard(0):
+        with ops.name_scope("infeed"):
+          a = infeed._dequeue()  # pylint: disable=protected-access
+        # Matmul on the same device as an infeed should be limited to
+        # num_compute_tiles tiles as IO tiles are allocated on this device.
+        with ops.name_scope("matmul1"):
+          aa = math_ops.matmul(a, a)
+      with ipu_shard(1):
+        with ops.name_scope("matmul2"):
+          # Matmul on a device with no IO should limited to tiles_per_ipu tiles
+          # as no IO tiles should be allocated on this device.
+          aaa = math_ops.matmul(aa, a)
+      return aaa
+
+    with ops.device("/device:IPU:0"):
+      compiled_net = ipu_compiler.compile(my_net, inputs=[])
+
+    cfg = ipu_utils.create_ipu_config(profiling=True)
+    cfg = ipu_utils.set_ipu_model_options(cfg,
+                                          compile_ipu_code=False,
+                                          tiles_per_ipu=tiles_per_ipu)
+    cfg = ipu_utils.set_io_tile_options(cfg,
+                                        num_io_tiles=num_io_tiles,
+                                        place_ops_on_io_tiles=True)
+    cfg = ipu_utils.auto_select_ipus(cfg, num_ipus=2)
+    ipu_utils.configure_ipu_system(cfg)
+
+    with session.Session() as sess:
+      report = tu.ReportJSON(self, sess, configure_device=False)
+      report.reset()
+
+      sess.run(infeed.initializer)
+      result, = sess.run(compiled_net)
+      report.parse_log()
+
+      # Sanity check results.
+      self.assertAllEqual(np.dot(np.dot(data, data), data), result)
+
+      # Find matmul operations.
+      tensors = report.get_tensor_map().all_tensors()
+      matmuls = [t for t in tensors if t.id.startswith("dot")]
+      self.assertEqual(len(matmuls), 2)
+
+      # Matmul on ipu0 mapped to a smaller set of tiles.
+      self.assertLessEqual(len(matmuls[0].tiles), num_compute_tiles)
+
+      # Matmul on ipu1 device allowed to use all tiles.
+      self.assertGreater(len(matmuls[1].tiles), num_compute_tiles)
+      self.assertLessEqual(len(matmuls[1].tiles), tiles_per_ipu)
+
+  @test_util.deprecated_graph_mode_only
+  @tu.test_may_use_ipus_or_model(num_ipus=2)
+  def testCopyFromDeviceWithIoTilesToDeviceWithoutIoTiles(self):
+    tiles_per_ipu = 1216
+    num_io_tiles = 32
+    num_compute_tiles = tiles_per_ipu - num_io_tiles
+
+    data = np.ones((tiles_per_ipu, tiles_per_ipu), dtype=np.float32)
+    dataset = dataset_ops.Dataset.from_tensors(data)
+    infeed = ipu_infeed_queue.IPUInfeedQueue(dataset, feed_name="infeed")
+
+    def my_net():
+      with ipu_shard(0):
+        with ops.name_scope("infeed"):
+          a = infeed._dequeue()  # pylint: disable=protected-access
+        with ops.name_scope("matmul1"):
+          a = math_ops.matmul(a, a)
+      with ipu_shard(1):
+        with ops.name_scope("matmul2"):
+          return math_ops.matmul(a, a)
+
+    with ops.device("/device:IPU:0"):
+      compiled_net = ipu_compiler.compile(my_net, inputs=[])
+
+    cfg = ipu_utils.create_ipu_config(profiling=True)
+    cfg = ipu_utils.set_ipu_model_options(cfg,
+                                          compile_ipu_code=False,
+                                          tiles_per_ipu=tiles_per_ipu)
+    cfg = ipu_utils.set_io_tile_options(cfg,
+                                        num_io_tiles=num_io_tiles,
+                                        place_ops_on_io_tiles=True)
+    cfg = ipu_utils.auto_select_ipus(cfg, num_ipus=2)
+    ipu_utils.configure_ipu_system(cfg)
+
+    with session.Session() as sess:
+      report = tu.ReportJSON(self, sess, configure_device=False)
+      report.reset()
+
+      sess.run(infeed.initializer)
+      result, = sess.run(compiled_net)
+      report.parse_log()
+
+      # Sanity check results.
+      expected_result = np.dot(data, data)
+      expected_result = np.dot(expected_result, expected_result)
+      self.assertAllEqual(expected_result, result)
+
+      matmuls = []
+      iics = []
+      for t in report.get_tensor_map().all_tensors():
+        # Find matmul operations.
+        if t.id.startswith("dot"):
+          matmuls.append(t)
+        # Find ipu-inter-copy operations.
+        elif t.id.startswith("ipu-inter-copy"):
+          iics.append(t)
+
+      self.assertEqual(len(matmuls), 2)
+      self.assertEqual(len(iics), 1)
+      iic = iics[0]
+
+      # Copy to device without I/O tiles should be mapped to more tiles than
+      # the source tensor from a device with I/O tiles.
+      self.assertGreater(len(iic.tiles), len(matmuls[0].tiles))
+      self.assertGreater(len(iic.tiles), num_compute_tiles)
+      self.assertLessEqual(len(iic.tiles), tiles_per_ipu)
+
+  @test_util.deprecated_graph_mode_only
+  @tu.test_may_use_ipus_or_model(num_ipus=2)
+  def testCopyFromDeviceWithoutIoTilesToDeviceWithIoTiles(self):
+    tiles_per_ipu = 1216
+    num_io_tiles = 32
+    num_compute_tiles = tiles_per_ipu - num_io_tiles
+
+    data = np.ones((tiles_per_ipu, tiles_per_ipu), dtype=np.float32)
+    a = array_ops.placeholder(np.float32, data.shape)
+    outfeed = ipu_outfeed_queue.IPUOutfeedQueue(feed_name="outfeed")
+
+    def my_net(a):
+      with ipu_shard(0):
+        with ops.name_scope("matmul1"):
+          a = math_ops.matmul(a, a)
+      with ipu_shard(1):
+        with ops.name_scope("matmul2"):
+          a = math_ops.matmul(a, a)
+        with ops.name_scope("outfeed"):
+          return outfeed.enqueue(a)
+
+    with ops.device("/device:IPU:0"):
+      compiled_net = ipu_compiler.compile(my_net, inputs=[a])
+
+    cfg = ipu_utils.create_ipu_config(profiling=True)
+    cfg = ipu_utils.set_ipu_model_options(cfg,
+                                          compile_ipu_code=False,
+                                          tiles_per_ipu=tiles_per_ipu)
+    cfg = ipu_utils.set_io_tile_options(cfg,
+                                        num_io_tiles=num_io_tiles,
+                                        place_ops_on_io_tiles=True)
+    cfg = ipu_utils.auto_select_ipus(cfg, num_ipus=2)
+    ipu_utils.configure_ipu_system(cfg)
+
+    with session.Session() as sess:
+      report = tu.ReportJSON(self, sess, configure_device=False)
+      report.reset()
+
+      sess.run(compiled_net, {a: data})
+      result, = sess.run(outfeed.dequeue())
+      report.parse_log()
+
+      # Sanity check results.
+      expected_result = np.dot(data, data)
+      expected_result = np.dot(expected_result, expected_result)
+      self.assertAllEqual(expected_result, result)
+
+      matmuls = []
+      iics = []
+      for t in report.get_tensor_map().all_tensors():
+        # Find matmul operations.
+        if t.id.startswith("dot"):
+          matmuls.append(t)
+        # Find ipu-inter-copy operations.
+        elif t.id.startswith("ipu-inter-copy"):
+          iics.append(t)
+
+      self.assertEqual(len(matmuls), 2)
+      self.assertEqual(len(iics), 1)
+      iic = iics[0]
+
+      # Copy to device with I/O tiles should be mapped to fewer tiles than the
+      # source tensor from a device without I/O tiles.
+      self.assertLess(len(iic.tiles), len(matmuls[0].tiles))
+      self.assertLess(len(iic.tiles), num_compute_tiles)
 
 
 if __name__ == "__main__":
