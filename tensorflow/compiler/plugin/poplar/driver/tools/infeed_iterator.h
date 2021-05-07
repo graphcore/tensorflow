fed4efd34e2c3bb47944773d6db4fed37de21e99
Babak Khataee <babakk@graphcore.ai>
2021-05-07 17:35:32 +0100

    Deprecating replication_factor argument of IPUInfeedQueue
    Summary: This change deprecates the replication_factor argument of the IPUInfeedQueue. Previously when set with a value > 1 a BufferDataset would be used to wrap the dataset being used by the InfeedQueue. I've replaced this with an updated InfeedIterator, which buffers its internal iterator in the same way that the BufferDataset iterator did. It is configured by the PoplarExecutor during execution. This seemed simpler than creating the BufferDatasetOp internally as it wasn't obvious to me when/how we could fit that into the execution process.
    
    Test Plan: CI + new python test.
    
    Reviewers: #tensorflow, simonl, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep, markf
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep
    
    Maniphest Tasks: T19060
    
    Differential Revision: https://phabricator.sourcevertex.net/D44631

--- tensorflow/compiler/plugin/poplar/driver/tools/infeed_iterator.h
+++ tensorflow/compiler/plugin/poplar/driver/tools/infeed_iterator.h
@@ -120,7 +120,7 @@ class InfeedIterator {
   InfeedIterator(tensorflow::FunctionLibraryRuntime* flr,
                  tensorflow::data::IteratorContext::Params params,
                  tensorflow::data::DatasetBase* dataset,
-                 InfeedAllocator* infeed_allocator_, int64 replication_factor,
+                 InfeedAllocator* infeed_allocator_,
                  const std::vector<xla::Shape>& shapes,
                  const std::string& feed_id);
 
@@ -135,8 +135,13 @@ class InfeedIterator {
 
   void SignalAllQueuesToEnd();
 
+  bool HasReplicationFactor() const;
+
+  int64 ReplicationFactor() const;
+  void SetReplicationFactor(int64 replication_factor);
+
  private:
-  const int64 replication_factor_;
+  int64 replication_factor_;
   std::vector<Shape> shapes_;
 
   // Not owned.
@@ -174,6 +179,12 @@ class InfeedIterator {
   std::vector<std::vector<InfeedQueueStorage>> infeed_queues_;
   // Used by the accessor.
   std::vector<std::vector<InfeedQueue*>> infeed_queues_ptrs_;
+
+  // Stores the next position in the buffer we should read from. If equal to
+  // buffer_size we need to load more data.
+  size_t buffer_position_ = -1;
+  // Internal storage.
+  std::vector<std::vector<tensorflow::Tensor>> buffer_;
 };
 
 }  // namespace poplarplugin
