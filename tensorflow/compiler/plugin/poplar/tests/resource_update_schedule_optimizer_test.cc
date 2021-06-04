61585b738a166267e65428e5441c738683954f27
George Pawelczak <grzegorzp@graphcore.ai>
2021-06-04 18:43:55 +0100

    Make sure resource update outputs have same sharding as parent outputs
    Summary:
    Ensure that resource update outputs have the same sharding to prevent
    inter ipu copies from being inserted between resource update and the root
    instruction. This makes sure that the copies are inside of the resource
    update computation.
    
    Fix T39900
    
    Test Plan: CI, added a test which failed before.
    
    Reviewers: jakeh, hakons, #tensorflow, simonl, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved
    
    Reviewed By: hakons, #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved
    
    Maniphest Tasks: T39900
    
    Differential Revision: https://phabricator.sourcevertex.net/D46361

--- tensorflow/compiler/plugin/poplar/tests/resource_update_schedule_optimizer_test.cc
+++ tensorflow/compiler/plugin/poplar/tests/resource_update_schedule_optimizer_test.cc
@@ -79,17 +79,16 @@ pipeline {
   in0 = f32[1,4,4,2] get-tuple-element(infeed), index=0
   in1 = f32[1,4,4,2] parameter(0)
   in2 = f32[1,4,4,2] parameter(1)
-  in3 = f32[1,4,4,2] parameter(2)
-  in4 = f32[] parameter(3)
-  stage_0 = (f32[1,4,4,2], f32[1,4,4,2], f32[]) call(in1, in0, in4), to_apply=stage_0_fwd, backend_config="{\"callConfig\":{\"type\":\"PipelineStage\",\"pipelineStageConfig\":{\"stageId\":\"0\"}}}", sharding={maximal device=0}
+  in3 = f32[] parameter(2)
+  stage_0 = (f32[1,4,4,2], f32[1,4,4,2], f32[]) call(in1, in0, in3), to_apply=stage_0_fwd, backend_config="{\"callConfig\":{\"type\":\"PipelineStage\",\"pipelineStageConfig\":{\"stageId\":\"0\"}}}", sharding={maximal device=0}
   stage_0_0 = f32[1,4,4,2] get-tuple-element(stage_0), index=0
   stage_0_1 = f32[1,4,4,2] get-tuple-element(stage_0), index=1
   stage_0_2 = f32[] get-tuple-element(stage_0), index=2
-  stage_1 = (f32[1,4,4,2], f32[1,4,4,2], f32[]) call(stage_0_0, stage_0_1, stage_0_2), to_apply=stage_1_fwd, backend_config="{\"callConfig\":{\"type\":\"PipelineStage\",\"pipelineStageConfig\":{\"stageId\":\"1\"}}}", sharding={maximal device=1}
+  stage_1 = (f32[1,4,4,2], f32[1,4,4,2], f32[]) call(stage_0_0, in2, stage_0_2), to_apply=stage_1_fwd, backend_config="{\"callConfig\":{\"type\":\"PipelineStage\",\"pipelineStageConfig\":{\"stageId\":\"1\"}}}", sharding={maximal device=1}
   stage_1_1 = f32[1,4,4,2] get-tuple-element(stage_1), index=0
   stage_1_2 = f32[1,4,4,2] get-tuple-element(stage_1), index=1
   stage_1_3 = f32[] get-tuple-element(stage_1), index=2
-  stage_1_bwd = (f32[1,4,4,2], f32[1,4,4,2]) call(stage_1_1, stage_1_2), to_apply=stage_1_bwd, backend_config="{\"callConfig\":{\"type\":\"PipelineStageBackward\",\"pipelineStageConfig\":{\"stageId\":\"1\"}}}", sharding={maximal device=0}
+  stage_1_bwd = (f32[1,4,4,2], f32[1,4,4,2]) call(stage_1_1, stage_1_2), to_apply=stage_1_bwd, backend_config="{\"callConfig\":{\"type\":\"PipelineStageBackward\",\"pipelineStageConfig\":{\"stageId\":\"1\"}}}", sharding={maximal device=1}
   stage_1_bwd_1 = f32[1,4,4,2] get-tuple-element(stage_1_bwd), index=0
   stage_1_bwd_2 = f32[1,4,4,2] get-tuple-element(stage_1_bwd), index=1
   stage_0_bwd = (f32[1,4,4,2], f32[1,4,4,2]) call(stage_1_bwd_1, stage_0_0), to_apply=stage_0_bwd, backend_config="{\"callConfig\":{\"type\":\"PipelineStageBackward\",\"pipelineStageConfig\":{\"stageId\":\"0\"}}}", sharding={maximal device=0}
@@ -97,16 +96,14 @@ pipeline {
   call_ru = (f32[1,4,4,2], f32[1,4,4,2], f32[1,4,4,2]) call(stage_0_bwd_0, stage_1_bwd_1, stage_1_bwd_2, stage_1_3), to_apply=resource_update, frontend_attributes={CALL_CONFIG_TYPE="ResourceUpdate"}, backend_config="{\"callConfig\":{\"type\":\"ResourceUpdate\"}}"
   gte0 = f32[1,4,4,2] get-tuple-element(call_ru), index=0
   gte1 = f32[1,4,4,2] get-tuple-element(call_ru), index=1
-  gte2 = f32[1,4,4,2] get-tuple-element(call_ru), index=2
-  ROOT tuple = (f32[1,4,4,2], f32[1,4,4,2], f32[1,4,4,2]) tuple(gte0, gte1, gte2, in4)
+  ROOT tuple = (f32[1,4,4,2], f32[1,4,4,2], f32[]) tuple(gte0, gte1, in3)
 }
 
 ENTRY e {
-  e.in0 = f32[1,4,4,2] parameter(0), parameter_replication={false}
-  e.in1 = f32[1,4,4,2] parameter(1), parameter_replication={false}
-  e.in2 = f32[1,4,4,2] parameter(2), parameter_replication={false}
-  e.in3 = f32[] parameter(3), parameter_replication={false}
-  ROOT e.call = (f32[1,4,4,2], f32[1,4,4,2], f32[1,4,4,2], f32[]) call(e.in0, e.in1, e.in2, e.in3), to_apply=pipeline, backend_config="{\"callConfig\":{\"type\":\"Pipeline\"}}"
+  e.in0 = f32[1,4,4,2] parameter(0)
+  e.in1 = f32[1,4,4,2] parameter(1)
+  e.in2 = f32[] parameter(2)
+  ROOT e.call = (f32[1,4,4,2], f32[1,4,4,2], f32[]) call(e.in0, e.in1, e.in2), to_apply=pipeline, backend_config="{\"callConfig\":{\"type\":\"Pipeline\"}}"
 }
 )";
   auto config = GetModuleConfigForTest();
