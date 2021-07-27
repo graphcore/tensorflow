153911b20462fe22a69e3a7f9a369b8b448dca55
Gautham Ganapathy <gauthamg@graphcore.ai>
2021-07-27 12:54:21 +0100

    Store information about the runtime the executable was compiled for
    Summary: FIX T41132
    
    Reviewers: #tensorflow, simonl, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, jakeh, georgep
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep
    
    Maniphest Tasks: T41132
    
    Differential Revision: https://phabricator.sourcevertex.net/D48456

--- tensorflow/compiler/plugin/poplar/driver/poplar_executable.h
+++ tensorflow/compiler/plugin/poplar/driver/poplar_executable.h
@@ -40,6 +40,12 @@ struct CompilerAnnotations;
 struct CompilerResources;
 
 struct PoplarExecutableInfo {
+  int64 num_IPUs;
+  std::string target_type;
+  std::string target_arch;
+  bool gateway_mode;
+  bool supports_remote_buffers;
+
   uint32 replication_factor;
   CanonicalInfeedInfos infeed_infos;
   CanonicalOutfeedInfos outfeed_infos;
@@ -152,12 +158,8 @@ class PoplarExecutableCore {
 
   static Status Serialize(const ModuleFilenames& filenames,
                           const poplar::Executable& executable,
-                          const CompilerAnnotations& annotations,
-                          uint32 replication_count,
                           const poplar::OptionFlags& opts,
-                          bool logging_cycle_count,
-                          const VerifiedStreamsIndices::KeyIdMappings& mappings,
-                          const std::vector<string>& checkpoint_feeds_order);
+                          const PoplarExecutableInfo& info);
 
   static Status Export(const ModuleFilenames& filenames,
                        const poplar::Executable& executable,
