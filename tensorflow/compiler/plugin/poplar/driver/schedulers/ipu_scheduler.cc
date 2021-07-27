graphcore/tensorflow

commit 74c3d6ae414619b4e2ccc4faef313d51fe6e42a8

Improve auto scheduler selection and include SHORTEST_PATH
Summary:
Run the HLO alias analysis and pass the result to the heap simulator.
With the empty alias analysis that we used before the heap simulator
always returned 0 and we chose the first scheduler (POST_ORDER),
while now we let it choose among the three POST_ORDER,
CLUSTERING and SHORTEST_PATH.

LOOK_AHEAD is still not added in auto mode because of its time
complexity (it took 4 minutes with default settings on BERT-Base).

The scheduler is hard coded in some tests that relied on a specific schedule.

Ref. T40113
Ref. T37032

TF2.1 Only.

Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, jakeh, georgep, markf

Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep, markf

Maniphest Tasks: T40113

Differential Revision: https://phabricator.sourcevertex.net/D48010

tensorflow/compiler/plugin/poplar/driver/schedulers/ipu_scheduler.cc
@@ -15,6 +15,7 @@ limitations under the License.
 #include "tensorflow/compiler/plugin/poplar/driver/schedulers/ipu_scheduler.h"
 
 #include <map>
+#include <memory>
 #include <queue>
 #include <tuple>
 #include <utility>
@@ -46,8 +47,8 @@ StatusOr<HloSchedule> IpuScheduleModule(
   HloSchedule schedule(module);
   TF_ASSIGN_OR_RETURN(std::unique_ptr<TuplePointsToAnalysis> points_to_analysis,
                       TuplePointsToAnalysis::Run(module));
-  std::unique_ptr<HloAliasAnalysis> alias_analysis =
-      HloAliasAnalysis::NewEmptyAnalysis(module);
+  TF_ASSIGN_OR_RETURN(std::unique_ptr<HloAliasAnalysis> alias_analysis,
+                      HloAliasAnalysis::Run(module));
   absl::flat_hash_map<const HloComputation*, int64> memory_by_computation;
   for (auto* computation : module->MakeComputationPostOrder()) {
     if (!computation->IsFusionComputation()) {
@@ -113,8 +114,8 @@ StatusOr<IpuSchedulerAlgorithm> BestIpuSchedule(
           const LogicalBuffer::SizeFunction& size_function,
           const absl::flat_hash_map<const HloComputation*, int64>&
               memory_by_computation) -> StatusOr<HloInstructionSequence> {
-        std::unique_ptr<HloAliasAnalysis> alias_analysis =
-            HloAliasAnalysis::NewEmptyAnalysis(computation->parent());
+        TF_ASSIGN_OR_RETURN(std::unique_ptr<HloAliasAnalysis> alias_analysis,
+                            HloAliasAnalysis::Run(computation->parent()));
 
         struct ScheduleResult {
           int64 schedule_memory;

tensorflow/compiler/plugin/poplar/driver/poplar_compiler.cc
@@ -846,7 +846,9 @@ Status CreatePoplarGraphs(CompilerResources& resources, const HloModule* module,
 StatusOr<std::vector<NamedIpuSchedulerAlgorithm>> GetSchedulerList(
     CompilerResources& res) {
   std::vector<NamedIpuSchedulerAlgorithm> schedulers;
-  bool all = res.scheduler_selection == IpuSchedulingAlgorithm::CHOOSE_BEST;
+
+  const bool all =
+      res.scheduler_selection == IpuSchedulingAlgorithm::CHOOSE_BEST;
   if (all || res.scheduler_selection == IpuSchedulingAlgorithm::POST_ORDER) {
     schedulers.push_back(
         {IpuSchedulingAlgorithm_Name(IpuSchedulingAlgorithm::POST_ORDER),
@@ -857,22 +859,25 @@ StatusOr<std::vector<NamedIpuSchedulerAlgorithm>> GetSchedulerList(
         {IpuSchedulingAlgorithm_Name(IpuSchedulingAlgorithm::CLUSTERING),
          CreateClusteringMemoryScheduler(res.information)});
   }
-  if (res.scheduler_selection == IpuSchedulingAlgorithm::LOOK_AHEAD) {
-    schedulers.push_back(
-        {IpuSchedulingAlgorithm_Name(IpuSchedulingAlgorithm::LOOK_AHEAD),
-         CreateLivenessLookAheadMemoryScheduler(res.information)});
-  }
-  if (res.scheduler_selection == IpuSchedulingAlgorithm::SHORTEST_PATH) {
+  if (all || res.scheduler_selection == IpuSchedulingAlgorithm::SHORTEST_PATH) {
     schedulers.push_back(
         {IpuSchedulingAlgorithm_Name(IpuSchedulingAlgorithm::SHORTEST_PATH),
          CreateShortestPathScheduler(res.information)});
   }
 
-  if (!schedulers.size()) {
+  // Not enabled with CHOOSE_BEST because of its time complexity.
+  if (res.scheduler_selection == IpuSchedulingAlgorithm::LOOK_AHEAD) {
+    schedulers.push_back(
+        {IpuSchedulingAlgorithm_Name(IpuSchedulingAlgorithm::LOOK_AHEAD),
+         CreateLivenessLookAheadMemoryScheduler(res.information)});
+  }
+
+  if (schedulers.empty()) {
     return xla::InvalidArgument(
         "Invalid scheduler specified. Options are 'LOOK_AHEAD',"
         " 'POST_ORDER', 'CLUSTERING' and 'SHORTEST_PATH'.");
   }
+
   return schedulers;
 }
 

tensorflow/compiler/plugin/poplar/tests/conv_graph_caching_test.py
@@ -25,7 +25,7 @@ from tensorflow.compiler.tests import xla_test
 from tensorflow.python.platform import googletest
 from tensorflow.python.framework import constant_op
 from tensorflow.python.framework import ops
-from tensorflow.python.ipu.config import IPUConfig
+from tensorflow.python.ipu.config import IPUConfig, SchedulingAlgorithm
 from tensorflow.python.keras import layers
 from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import gen_array_ops
@@ -215,7 +215,8 @@ class ConvGraphCachingTest(xla_test.XLATestCase):
         optimizer = gradient_descent.GradientDescentOptimizer(0.1)
         train = optimizer.minimize(loss)
 
-      report = tu.ReportJSON(self, sess)
+      report = tu.ReportJSON(
+          self, sess, scheduling_algorithm=SchedulingAlgorithm.POST_ORDER)
 
       sess.run(variables.global_variables_initializer())
 
@@ -271,7 +272,8 @@ class ConvGraphCachingTest(xla_test.XLATestCase):
         optimizer = gradient_descent.GradientDescentOptimizer(lr)
         train = optimizer.minimize(loss)
 
-      report = tu.ReportJSON(self, sess)
+      report = tu.ReportJSON(
+          self, sess, scheduling_algorithm=SchedulingAlgorithm.POST_ORDER)
 
       sess.run(variables.global_variables_initializer())
 

tensorflow/compiler/plugin/poplar/tests/fuse_ops_test.py
@@ -26,7 +26,7 @@ from tensorflow.compiler.tests import xla_test
 from tensorflow.python.platform import googletest
 from tensorflow.python.framework import ops
 from tensorflow.python.framework import dtypes
-from tensorflow.python.ipu.config import IPUConfig
+from tensorflow.python.ipu.config import IPUConfig, SchedulingAlgorithm
 from tensorflow.python.layers import normalization as layers_norm
 from tensorflow.python.keras import layers
 from tensorflow.python.ops import array_ops
@@ -455,6 +455,7 @@ class IpuFuseOpsTest(xla_test.XLATestCase):
     report_helper = tu.ReportHelper()
     report_helper.set_autoreport_options(cfg)
     cfg.ipu_model.compile_ipu_code = False
+    cfg.scheduling.algorithm = SchedulingAlgorithm.POST_ORDER
     cfg.configure_ipu_system()
 
     with self.session() as sess:

tensorflow/compiler/plugin/poplar/tests/norm_graph_caching_sharded_test.py
@@ -25,6 +25,7 @@ from tensorflow.compiler.tests import xla_test
 from tensorflow.python import ipu
 from tensorflow.python.platform import googletest
 from tensorflow.python.framework import ops
+from tensorflow.python.ipu.config import SchedulingAlgorithm
 from tensorflow.python.layers import convolutional
 from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import init_ops
@@ -42,6 +43,7 @@ class NormGraphCachingTest(xla_test.XLATestCase):
     report_helper.set_autoreport_options(cfg)
     cfg.ipu_model.compile_ipu_code = False
     cfg.auto_select_ipus = 2
+    cfg.scheduling.algorithm = SchedulingAlgorithm.POST_ORDER
     cfg.configure_ipu_system()
 
     with self.session() as sess:

tensorflow/compiler/plugin/poplar/tests/norm_graph_caching_test.py
@@ -28,6 +28,7 @@ from tensorflow.python.framework import constant_op
 from tensorflow.python.framework import ops
 from tensorflow.python.ipu import ipu_compiler
 from tensorflow.python.ipu.config import IPUConfig
+from tensorflow.python.ipu.config import SchedulingAlgorithm
 from tensorflow.python.layers import convolutional
 from tensorflow.python.layers import normalization as layers_norm
 from tensorflow.python.ops import array_ops
@@ -182,6 +183,7 @@ class NormGraphCachingTest(xla_test.XLATestCase):
     report_helper = tu.ReportHelper()
     report_helper.set_autoreport_options(cfg)
     cfg.ipu_model.compile_ipu_code = False
+    cfg.scheduling.algorithm = SchedulingAlgorithm.POST_ORDER
     cfg.configure_ipu_system()
 
     with self.session() as sess:

tensorflow/compiler/plugin/poplar/tests/size_speed_tests/resnet_size_test.py
@@ -255,7 +255,7 @@ class Resnet18_No_Batchnorm(xla_test.XLATestCase):
       sess.run(train, feed_dict={x: data, y_: labels})
 
     report = pva.openReport(report_helper.find_report())
-    self.assert_total_tile_memory(report, 43635227)
+    self.assert_total_tile_memory(report, 41595004)
 
   def testTrainingMomentumInLoop(self):
     cfg = ipu.utils.IPUConfig()
@@ -294,7 +294,7 @@ class Resnet18_No_Batchnorm(xla_test.XLATestCase):
       sess.run(train, feed_dict={x: data, y_: labels})
 
     report = pva.openReport(report_helper.find_report())
-    self.assert_total_tile_memory(report, 44571221)
+    self.assert_total_tile_memory(report, 43679590)
 
   def testTrainingInLoopWithGradientAccumulation(self):
     cfg = ipu.utils.IPUConfig()
@@ -335,7 +335,7 @@ class Resnet18_No_Batchnorm(xla_test.XLATestCase):
       sess.run(train, feed_dict={x: data, y_: labels})
 
     report = pva.openReport(report_helper.find_report())
-    self.assert_total_tile_memory(report, 44641269)
+    self.assert_total_tile_memory(report, 45535722)
 
   def testTrainingMomentumInLoopWithGradientAccumulation(self):
     cfg = ipu.utils.IPUConfig()
@@ -375,7 +375,7 @@ class Resnet18_No_Batchnorm(xla_test.XLATestCase):
       sess.run(train, feed_dict={x: data, y_: labels})
 
     report = pva.openReport(report_helper.find_report())
-    self.assert_total_tile_memory(report, 44637881)
+    self.assert_total_tile_memory(report, 45699624)
 
 
 if __name__ == "__main__":

tensorflow/compiler/plugin/poplar/tests/test_utils.py
@@ -463,7 +463,8 @@ class ReportJSON(object):
                triangular_solve_expander_block_size=0,
                minimum_remote_tensor_size=128,
                use_hw=False,
-               num_io_tiles=0):
+               num_io_tiles=0,
+               scheduling_algorithm=None):
     self.report = None
     self.test = test
     self.sess = sess
@@ -515,6 +516,9 @@ class ReportJSON(object):
         opts.io_tiles.place_ops_on_io_tiles = True
 
       opts.norms.use_stable_statistics = use_stable_norm_statistics
+
+      if scheduling_algorithm is not None:
+        opts.scheduling.algorithm = scheduling_algorithm
 
       if use_hw:
         add_hw_ci_connection_options(opts)

commit 0c87b10b0bdeb1cdb52357893d4ea3e52e1b2fab

Add alias analysis cache
Summary:
The scheduler is run for each computation, while the alias analysis is
performed for the whole module. Before we would re-run the analysis for
each computation. This commit adds a cache that allows re-using the
analysis across the computations to save time.

Fix compile time regression from D48010.

Ref. T42424.

Test Plan:
Tested BERT-Large compilation:

```
python run_pretraining.py --config configs/pretrain_large_128_phase1.json --generated-data --compile-only
```
Before: IpuScheduler took 177.39 seconds
After: IpuScheduler took 5.76686 seconds

Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, jakeh, georgep, markf

Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep

Maniphest Tasks: T42424

Differential Revision: https://phabricator.sourcevertex.net/D48838

tensorflow/compiler/plugin/poplar/driver/schedulers/ipu_scheduler.cc
@@ -70,6 +70,32 @@ StatusOr<HloSchedule> IpuScheduleModule(
 
   return std::move(schedule);
 }
+
+/*
+ * The scheduler is run for each computation, while the alias analysis is
+ * performed for the whole module. This cache allows re-using the analysis
+ * across the computations to save time. It assumes that the module is not
+ * modified while in use.
+ */
+class AliasAnalysisCache {
+ public:
+  using Container =
+      absl::flat_hash_map<const HloModule*, std::unique_ptr<HloAliasAnalysis>>;
+
+  const HloAliasAnalysis& FindOrRun(const HloModule* module) {
+    const auto it = cache_.lazy_emplace(
+        module, [module](const Container::constructor& ctor) {
+          auto result = HloAliasAnalysis::Run(module);
+          TF_CHECK_OK(result.status());
+          ctor(module, std::move(result.ValueOrDie()));
+        });
+    return *it->second;
+  }
+
+ private:
+  Container cache_;
+};
+
 }  // namespace
 
 IpuSchedulerAlgorithm MemorySchedulerAlgorithmToIPU(
@@ -108,14 +134,15 @@ StatusOr<IpuSchedulerAlgorithm> BestIpuSchedule(
   }
 
   return IpuSchedulerAlgorithm{
-      [algorithms](
+      [algorithms,
+       alias_analysis_cache = std::make_shared<AliasAnalysisCache>()](
           HloComputation* computation,
           const TuplePointsToAnalysis& tuple_points_to_analysis,
           const LogicalBuffer::SizeFunction& size_function,
           const absl::flat_hash_map<const HloComputation*, int64>&
               memory_by_computation) -> StatusOr<HloInstructionSequence> {
-        TF_ASSIGN_OR_RETURN(std::unique_ptr<HloAliasAnalysis> alias_analysis,
-                            HloAliasAnalysis::Run(computation->parent()));
+        const HloAliasAnalysis& alias_analysis =
+            alias_analysis_cache->FindOrRun(computation->parent());
 
         struct ScheduleResult {
           int64 schedule_memory;
@@ -141,7 +168,7 @@ StatusOr<IpuSchedulerAlgorithm> BestIpuSchedule(
           // TODO(T9494): Replace the heap simulator.
           TF_ASSIGN_OR_RETURN(const int64 schedule_memory,
                               HeapSimulator::MinimumMemoryForComputation(
-                                  *computation, schedule, *alias_analysis,
+                                  *computation, schedule, alias_analysis,
                                   size_function, &memory_by_computation));
 
           VLOG(2) << "Scheduler " << algorithm.name