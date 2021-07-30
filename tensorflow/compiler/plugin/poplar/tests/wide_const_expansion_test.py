c4dadab1b84fe573f6027ac028092958e4533797
George Pawelczak <grzegorzp@graphcore.ai>
2021-07-30 18:08:11 +0100

    Adjust bounds
    Summary:
    Ref T16509
    TF1.15 Only
    
    Test Plan: CI
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, samuelh, jakeh, simonl
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, samuelh
    
    Maniphest Tasks: T16509
    
    Differential Revision: https://phabricator.sourcevertex.net/D49094

--- tensorflow/compiler/plugin/poplar/tests/wide_const_expansion_test.py
+++ tensorflow/compiler/plugin/poplar/tests/wide_const_expansion_test.py
@@ -115,8 +115,8 @@ class WideConstExpansionTest(xla_test.XLATestCase):
         'Mean/reduce', 'Mean/multiply', 'add*/add*/Add', 'add_*/fusion/Op/Add'
     ]
     self.assert_all_compute_sets_and_list(report, ok)
-    self.assert_max_tile_memory(report, 8824)
-    self.assert_always_live_memory(report, 169636)
+    self.assert_max_tile_memory(report, 8956, tolerance=0.2)
+    self.assert_always_live_memory(report, 280212, tolerance=0.2)
 
   def testCheckMaxTileSizePadding(self):
     cfg = IPUConfig()
