1a9399db2f2b3f768b6b5f0fa0f51a1b9dadfada
Samuel Hornby <samuelh@graphcore.ai>
2021-08-13 13:54:35 +0100

    Correct indexing for modified resource updates
    Summary:
    The compilation result returned from compile function stores
    indices into the original arguments vector. We use these when modifying
    the resource outputs but these indices are wrong if CompileFunction
    also removes the constant arguments. To get round this subtract
    number of constants from indexing
    
    Also adds some CHECKS for thing that the PipeliningOp and passes that work
    on it later assume but aren't obvious they are required on first reading
    
    Test Plan: Test modified will fail without these changes
    
    Reviewers: hakons, georgep, #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved
    
    Reviewed By: georgep, #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved
    
    Maniphest Tasks: T41151
    
    Differential Revision: https://phabricator.sourcevertex.net/D49551

--- tensorflow/python/ipu/tests/pipelining_test.py
+++ tensorflow/python/ipu/tests/pipelining_test.py
@@ -1151,7 +1151,7 @@ class PipeliningTest(test_util.TensorFlowTestCase):
 
     with tu.ipu_session() as sess:
 
-      def stage1(x):
+      def stage1(_, x):
         with variable_scope.variable_scope("stage1", use_resource=True):
           w = variable_scope.get_variable(name="w", initializer=1.0)
           return w * x
@@ -1169,7 +1169,7 @@ class PipeliningTest(test_util.TensorFlowTestCase):
       def my_net(x):
         return pipelining_ops.pipeline([stage1, identity, identity, identity],
                                        gradient_accumulation_count=8,
-                                       inputs=[x],
+                                       inputs=[10, x],
                                        outfeed_queue=outfeed_queue,
                                        optimizer_function=optimizer_function,
                                        outfeed_loss=True)
