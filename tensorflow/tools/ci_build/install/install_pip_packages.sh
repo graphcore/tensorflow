3dffed663a86a0d62925e28bac12cffccaa2d8c7
Jack Haughton <jackha@graphcore.ai>
2021-09-16 16:53:32 +0100

    Upgrade pip using get-pip.py, as easy_install is disabled by the view's configure script
    Summary: TF1.15 Only
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep
    
    Maniphest Tasks: T37698
    
    Differential Revision: https://phabricator.sourcevertex.net/D51578

--- tensorflow/tools/ci_build/install/install_pip_packages.sh
+++ tensorflow/tools/ci_build/install/install_pip_packages.sh
@@ -17,8 +17,10 @@
 set -e
 
 # Get the latest version of pip so it recognize manylinux2010
-easy_install3 -U pip
-easy_install -U pip
+wget https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py
+python get-pip.py
+rm -f get-pip.py
 
 # Install pip packages from whl files to avoid the time-consuming process of
 # building from source.
