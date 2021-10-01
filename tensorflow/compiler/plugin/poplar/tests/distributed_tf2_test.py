cf0fda2031c558f50aa2aaf1874a2b7e3e84211b
Christian aan de Wiel <christiana@graphcore.ai>
2021-10-01 15:39:32 +0200

    Add extra testing for distributed TF2
    Summary: TF2.4 Only
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, hakons, georgep
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, hakons, georgep
    
    Subscribers: hakons
    
    Maniphest Tasks: T46807
    
    Differential Revision: https://phabricator.sourcevertex.net/D52572

--- tensorflow/compiler/plugin/poplar/tests/distributed_tf2_test.py
+++ tensorflow/compiler/plugin/poplar/tests/distributed_tf2_test.py
@@ -23,6 +23,7 @@ from tensorflow.python.framework import constant_op, test_util
 from tensorflow.python.ipu.horovod import ipu_multi_replica_strategy
 from tensorflow.python.platform import test
 from tensorflow.python.ipu import horovod as hvd
+from tensorflow.python.ipu import ipu_strategy
 
 
 class DistributedTF2Test(test_util.TensorFlowTestCase):
@@ -32,15 +33,34 @@ class DistributedTF2Test(test_util.TensorFlowTestCase):
     root_tensor = hvd.broadcast(local_tensor, root_rank=0)
     np.testing.assert_equal(local_value, root_tensor.numpy(), name)
 
-  def test_tf2_distributed(self):
-    config = ipu.config.IPUConfig()
-    popdist.tensorflow.set_ipu_config(config, ipus_per_replica=1)
-    config.configure_ipu_system()
+  def assert_all_instances_not_equal(self, local_value):
+    local_tensor = constant_op.constant(local_value)
+    root_tensor = hvd.broadcast(local_tensor, root_rank=0)
 
-    hvd.init()
+    if hvd.local_rank() == 0:
+      return
 
-    strategy = ipu_multi_replica_strategy.IPUMultiReplicaStrategy()
+    assert not np.equal(local_value, root_tensor.numpy()).any()
 
+  def prepare_model(self):
+    # Make sure we have different parameters on each index
+    bias = tf.keras.initializers.Constant(value=popdist.getInstanceIndex())
+
+    return tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(32,
+                               3,
+                               activation='relu',
+                               bias_initializer=bias,
+                               name='test_bias'),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Conv2D(32, 3, activation='relu'),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(32, activation='relu'),
+        tf.keras.layers.Dense(10),
+    ])
+
+  def prepare_dataset(self):
     def generator():
       for _ in range(100):
         yield np.random.rand(32, 32, 1), np.random.randint(1, 10, size=1)
@@ -52,7 +72,7 @@ class DistributedTF2Test(test_util.TensorFlowTestCase):
     )
 
     options = tf.data.Options()
-    options.experimental_distribute.auto_shard_policy =\
+    options.experimental_distribute.auto_shard_policy = \
       tf.data.experimental.AutoShardPolicy.OFF
     dataset = dataset.with_options(options)
 
@@ -60,23 +80,66 @@ class DistributedTF2Test(test_util.TensorFlowTestCase):
                             index=popdist.getInstanceIndex())
     dataset = dataset.batch(10, drop_remainder=True)
 
-    with strategy.scope():
-      model = tf.keras.models.Sequential([
-          tf.keras.layers.Conv2D(32, 3, activation='relu'),
-          tf.keras.layers.MaxPooling2D(),
-          tf.keras.layers.Conv2D(32, 3, activation='relu'),
-          tf.keras.layers.MaxPooling2D(),
-          tf.keras.layers.Flatten(),
-          tf.keras.layers.Dense(32, activation='relu'),
-          tf.keras.layers.Dense(10),
-      ])
+    return dataset
 
+  def test_tf2_distributed_ipu_strategy(self):
+    config = ipu.config.IPUConfig()
+    popdist.tensorflow.set_ipu_config(config, ipus_per_replica=1)
+    config.configure_ipu_system()
+
+    hvd.init()
+
+    strategy = ipu_strategy.IPUStrategy()
+
+    with strategy.scope():
+      dataset = self.prepare_dataset()
+      model = self.prepare_model()
       optimizer = tf.keras.optimizers.SGD(learning_rate=0.01)
       loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
       model.compile(optimizer=optimizer,
                     loss=loss_fn,
                     steps_per_execution=popdist.getNumTotalReplicas())
+
+      # Build the model separately so we can assert that the biases are broadcasted properly before training.
+      model.build((1, 32, 32, 1))
+
+      layer = model.get_layer(name='test_bias')
+      self.assert_all_instances_not_equal(layer.get_weights()[1])
+
+      history = model.fit(dataset,
+                          steps_per_epoch=popdist.getNumTotalReplicas(),
+                          epochs=1)
+
+      # Make sure the losses and weights are not equal
+      self.assert_all_instances_not_equal(history.history['loss'])
+      self.assert_all_instances_not_equal(layer.get_weights()[1])
+
+  def test_tf2_distributed_ipu_multi_replica_strategy(self):
+    config = ipu.config.IPUConfig()
+    popdist.tensorflow.set_ipu_config(config, ipus_per_replica=1)
+    config.configure_ipu_system()
+
+    hvd.init()
+
+    strategy = ipu_multi_replica_strategy.IPUMultiReplicaStrategy()
+
+    with strategy.scope():
+      dataset = self.prepare_dataset()
+      model = self.prepare_model()
+      optimizer = tf.keras.optimizers.SGD(learning_rate=0.01)
+      loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+      model.compile(optimizer=optimizer,
+                    loss=loss_fn,
+                    steps_per_execution=popdist.getNumTotalReplicas())
+
+      # Build the model separately so we can assert that the biases are broadcasted properly before training.
+      model.build((1, 32, 32, 1))
+
+      layer = model.get_layer(name='test_bias')
+      self.assert_all_instances_equal(layer.get_weights()[1])
+
       history = model.fit(dataset,
                           steps_per_epoch=popdist.getNumTotalReplicas(),
                           epochs=1)
