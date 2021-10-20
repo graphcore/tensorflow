5739d225ea453e895451ed243e92ed334f4a15bd
Gautham Ganapathy <gauthamg@graphcore.ai>
2021-10-20 14:03:56 +0100

    all_to_all_finder support for cross_replica_mean
    Summary: REF T47313
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep, babakk
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep, babakk
    
    Subscribers: babakk
    
    Maniphest Tasks: T47313
    
    Differential Revision: https://phabricator.sourcevertex.net/D53696

--- tensorflow/compiler/plugin/poplar/tests/all_to_all_finder_test.cc
+++ tensorflow/compiler/plugin/poplar/tests/all_to_all_finder_test.cc
@@ -235,6 +235,141 @@ ENTRY main {
   AllToAllFinder ataf(annotations, 4);
   EXPECT_FALSE(ataf.Run(module).ValueOrDie());
 }
+
+TEST_F(AllToAllFinderTest, ReplaceReduceMeanMultiUpdate) {
+  const string& hlo_string = R"(
+HloModule main
+
+add {
+  acc = f32[] parameter(0)
+  x = f32[] parameter(1)
+  norm_x = f32[] custom-call(x), custom_call_target="ReplicationNormalise", backend_config="{}\n"
+  add = f32[] add(acc, norm_x)
+}
+
+ENTRY main {
+  offsets = s32[24,1] parameter(0)
+  updates = f32[24,16] parameter(1)
+  zero = f32[] constant(0)
+  big_zero = f32[1000,16] broadcast(zero), dimensions={}
+  operand = f32[1000,16] custom-call(big_zero, offsets, updates), custom_call_target="MultiUpdate", backend_config="{\"indices_are_sorted\":false}\n"
+  ROOT operand_all = f32[1000,16] all-reduce(operand), to_apply=add
+}
+)";
+  HloModuleConfig config;
+  config.set_debug_options(GetDebugOptionsForTest());
+
+  auto module_or_status = ParseAndReturnVerifiedModule(hlo_string, config);
+  EXPECT_TRUE(module_or_status.ok());
+
+  auto* module = module_or_status.ValueOrDie().get();
+
+  CompilerAnnotations annotations(module);
+
+  CustomOpReplacer custom_op_replacer;
+  EXPECT_TRUE(custom_op_replacer.Run(module).ValueOrDie());
+  AllToAllFinder ataf(annotations, 4);
+  EXPECT_TRUE(ataf.Run(module).ValueOrDie());
+
+  HloInstruction* root = module->entry_computation()->root_instruction();
+  EXPECT_TRUE(IsPoplarInstruction(PoplarOp::MultiUpdate)(root));
+  auto* multi_update = Cast<HloMultiUpdateInstruction>(root);
+  EXPECT_EQ(multi_update->GetSerializationFactor(), 1);
+
+  HloInstruction* big_zero = FindInstruction(module, "big_zero");
+  HloInstruction* offsets = FindInstruction(module, "offsets");
+  HloInstruction* updates = FindInstruction(module, "updates");
+
+  EXPECT_EQ(root->operand(0), big_zero);
+
+  auto* reshape_offsets = root->operand(1);
+  EXPECT_EQ(reshape_offsets->opcode(), HloOpcode::kReshape);
+
+  auto* all_gather_offsets = reshape_offsets->operand(0);
+  EXPECT_TRUE(IsPoplarInstruction(PoplarOp::AllGather)(all_gather_offsets));
+  EXPECT_EQ(all_gather_offsets->operand(0), offsets);
+
+  auto* replication_normalise = root->operand(2);
+  EXPECT_TRUE(IsPoplarInstruction(PoplarOp::ReplicationNormalise)(
+      replication_normalise));
+
+  auto* reshape_updates = replication_normalise->operand(0);
+  EXPECT_EQ(reshape_updates->opcode(), HloOpcode::kReshape);
+
+  auto* all_gather_updates = reshape_updates->operand(0);
+  EXPECT_TRUE(IsPoplarInstruction(PoplarOp::AllGather)(all_gather_updates));
+  EXPECT_EQ(all_gather_updates->operand(0), updates);
+}
+
+TEST_F(AllToAllFinderTest, ReplaceReduceMeanMultiUpdateAdd) {
+  const string& hlo_string = R"(
+HloModule main
+
+add {
+  acc = f32[] parameter(0)
+  x = f32[] parameter(1)
+  norm_x = f32[] custom-call(x), custom_call_target="ReplicationNormalise", backend_config="{}\n"
+  add = f32[] add(acc, norm_x)
+}
+
+ENTRY main {
+  offsets = s32[24,1] parameter(0)
+  updates = f32[24,16] parameter(1)
+  zero = f32[] constant(0)
+  scale = f32[] constant(1)
+  big_zero = f32[1000,16] broadcast(zero), dimensions={}
+  operand = f32[1000,16] custom-call(big_zero, offsets, updates, scale), custom_call_target="MultiUpdateAdd", backend_config="{\"indices_are_sorted\":false}\n"
+  ROOT operand_all = f32[1000,16] all-reduce(operand), to_apply=add
+}
+)";
+  HloModuleConfig config;
+  config.set_debug_options(GetDebugOptionsForTest());
+
+  auto module_or_status = ParseAndReturnVerifiedModule(hlo_string, config);
+  EXPECT_TRUE(module_or_status.ok());
+
+  auto* module = module_or_status.ValueOrDie().get();
+
+  CompilerAnnotations annotations(module);
+
+  CustomOpReplacer custom_op_replacer;
+  EXPECT_TRUE(custom_op_replacer.Run(module).ValueOrDie());
+  AllToAllFinder ataf(annotations, 4);
+  EXPECT_TRUE(ataf.Run(module).ValueOrDie());
+
+  HloInstruction* root = module->entry_computation()->root_instruction();
+  EXPECT_TRUE(IsPoplarInstruction(PoplarOp::MultiUpdateAdd)(root));
+  auto* multi_update = Cast<HloMultiUpdateInstruction>(root);
+  EXPECT_EQ(multi_update->GetSerializationFactor(), 1);
+
+  HloInstruction* big_zero = FindInstruction(module, "big_zero");
+  HloInstruction* offsets = FindInstruction(module, "offsets");
+  HloInstruction* updates = FindInstruction(module, "updates");
+  HloInstruction* scale = FindInstruction(module, "scale");
+
+  EXPECT_EQ(root->operand(0), big_zero);
+
+  auto* reshape_offsets = root->operand(1);
+  EXPECT_EQ(reshape_offsets->opcode(), HloOpcode::kReshape);
+
+  auto* all_gather_offsets = reshape_offsets->operand(0);
+  EXPECT_TRUE(IsPoplarInstruction(PoplarOp::AllGather)(all_gather_offsets));
+  EXPECT_EQ(all_gather_offsets->operand(0), offsets);
+
+  auto* replication_normalise = root->operand(2);
+  EXPECT_TRUE(IsPoplarInstruction(PoplarOp::ReplicationNormalise)(
+      replication_normalise));
+
+  auto* reshape_updates = replication_normalise->operand(0);
+  EXPECT_EQ(reshape_updates->opcode(), HloOpcode::kReshape);
+
+  auto* all_gather_updates = reshape_updates->operand(0);
+  EXPECT_TRUE(IsPoplarInstruction(PoplarOp::AllGather)(all_gather_updates));
+  EXPECT_EQ(all_gather_updates->operand(0), updates);
+
+  EXPECT_EQ(root->operand(3), scale);
+}
+
 }  // namespace
 }  // namespace poplarplugin
 }  // namespace xla
