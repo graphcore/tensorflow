4d18bbd5486cff60b670e19cdfde65c6e25b9bbc
George Pawelczak <georgep@graphcore.ai>
2022-01-10 15:06:57 +0000

    TF2.5 - Fix convolutions test
    Summary:
    Ref T51853
    TF2.5 Only
    
    Reviewers: christiana, #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved
    
    Reviewed By: christiana, #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved
    
    Maniphest Tasks: T51853
    
    Differential Revision: https://phabricator.sourcevertex.net/D58526

--- tensorflow/compiler/xla/tests/convolution_test.cc
+++ tensorflow/compiler/xla/tests/convolution_test.cc
@@ -55,7 +55,7 @@ class ConvolutionTest : public ClientLibraryTestBase {
 
 using TestTypes = ::testing::Types<
 // TODO(b/183565702): Support integer convs on GPU.
-#if !XLA_TEST_BACKEND_GPU
+#if !(XLA_TEST_BACKEND_GPU || XLA_TEST_BACKEND_POPLAR)
     int32,
 #endif
 #ifndef XLA_BACKEND_DOES_NOT_SUPPORT_FLOAT16
