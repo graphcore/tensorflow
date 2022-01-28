8ca0a8379ceb3edfe91180563b139684a531a4c5
Sam Hornby <samuelh@graphcore.ai>
2022-01-28 15:40:45 +0000

    Add code to detect while loop broadcasts
    Summary:
    This is the start of the code to remove tensor list broadcasts.
    This is just the detection phases where it finds inefficient
    broadcasts that are only used by slice instructions. The remove function
    is blank.
    
    Note this is not valid detection yet as it does not check that the slice index
    is less than the indexs updated into.
    
    Test Plan:
    included 2 but will focus most testing in diff that removes the instructions as
    both need to work together
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, vladimirm, babakk, georgep
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, vladimirm, babakk
    
    Maniphest Tasks: T50781
    
    Differential Revision: https://phabricator.sourcevertex.net/D58541

---
+++ tensorflow/compiler/plugin/poplar/driver/passes/while_loop_optimiser.cc
@@ -0,0 +1,447 @@
+/* Copyright 2022 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "tensorflow/compiler/plugin/poplar/driver/passes/while_loop_optimiser.h"
+
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include "tensorflow/compiler/plugin/poplar/driver/tools/matcher_predicates.h"
+#include "tensorflow/compiler/plugin/poplar/driver/tools/slice_util.h"
+#include "tensorflow/compiler/plugin/poplar/driver/tools/util.h"
+#include "tensorflow/compiler/xla/service/call_graph.h"
+#include "tensorflow/compiler/xla/service/hlo_module.h"
+#include "tensorflow/compiler/xla/shape_util.h"
+
+#include "absl/container/flat_hash_set.h"
+#include "tensorflow/compiler/xla/literal_util.h"
+#include "tensorflow/compiler/xla/service/hlo_creation_utils.h"
+#include "tensorflow/compiler/xla/service/pattern_matcher.h"
+#include "tensorflow/compiler/xla/service/while_util.h"
+
+namespace xla {
+
+namespace m = match;
+
+namespace poplarplugin {
+
+namespace {
+
+void FindLoopConstants(HloComputation* comp,
+                       absl::flat_hash_set<HloInstruction*>& result) {
+  // we are not finding the dropout ones because this constant looking
+  // is too restrictive. TODO fix this
+  for (auto* inst : comp->MakeInstructionPostOrder()) {
+    switch (inst->opcode()) {
+      // Note this is meant to match NotWorthHoistingIndividually
+      // inside WhileLoopInvariantCodeMotion
+      case HloOpcode::kConstant:
+      case HloOpcode::kBitcast:
+      case HloOpcode::kBroadcast:
+      case HloOpcode::kIota:
+      case HloOpcode::kReshape:
+      case HloOpcode::kReverse:
+      case HloOpcode::kSlice:
+      case HloOpcode::kTranspose:
+      case HloOpcode::kTuple: {
+        const bool all_const_inputs = absl::c_all_of(
+            inst->operands(),
+            [&](HloInstruction* operand) { return result.contains(operand); });
+        if (all_const_inputs) {
+          result.emplace(inst);
+        }
+        break;
+      }
+      default: {}
+    }
+  }
+}
+
+absl::flat_hash_set<HloInstruction*> FindLoopConstants(HloComputation* comp) {
+  absl::flat_hash_set<HloInstruction*> result;
+  auto gtes = WhileUtil::GetInvariantGTEsForWhileBody(*comp);
+  result.insert(gtes.begin(), gtes.end());
+  FindLoopConstants(comp, result);
+  return result;
+}
+
+std::vector<HloInstruction*> FindSingleUseParameters(HloComputation* comp) {
+  CHECK_EQ(comp->num_parameters(), 1);
+  HloInstruction* input_tuple = comp->parameter_instruction(0);
+  if (!input_tuple->shape().IsTuple()) {
+    return {};
+  }
+  std::vector<HloInstruction*> result(
+      ShapeUtil::TupleElementCount(input_tuple->shape()));
+  std::vector<int64> use_count(result.size(), 0);
+  for (auto* inst : input_tuple->users()) {
+    if (inst->opcode() == HloOpcode::kGetTupleElement) {
+      int64 index = inst->tuple_index();
+      result[index] = inst;
+      ++use_count[index];
+    } else {
+      return {};
+    }
+  }
+  for (int64 i = 0; i < result.size(); ++i) {
+    if (use_count[i] != 1) {
+      result[i] = nullptr;
+    }
+  }
+  return result;
+}
+
+struct SliceAndIndex {
+  int64 input_index;
+  HloInstruction* dynamic_update;
+  SliceAndIndex(int64 input_index, HloInstruction* dynamic_update)
+      : input_index(input_index), dynamic_update(dynamic_update) {}
+  std::string ToString() const {
+    return absl::StrCat("{", input_index, ", ", dynamic_update->name(), ",",
+                        dynamic_update->shape().ToString(), "}");
+  }
+};
+
+struct UsesAndIntermediates {
+  absl::InlinedVector<HloInstruction*, 1> uses;
+  absl::InlinedVector<HloInstruction*, 4> intermediates;
+};
+
+// Formatter that converts to string by calling the name attribute,
+// works for pointes as well as references
+class NameFormatter {
+  template <class T>
+  void dispatch(std::string* out, const T* const t, std::true_type) const {
+    return absl::AlphaNumFormatter()(out, t->name());
+  }
+  template <class T>
+  void dispatch(std::string* out, const T& t, std::false_type) const {
+    return absl::AlphaNumFormatter()(out, t.name());
+  }
+
+ public:
+  template <class T>
+  void operator()(std::string* out, const T& t) const {
+    return dispatch(out, t, std::is_pointer<T>());
+  }
+};
+
+// Formatter that converts to string by calling the ToString attribute,
+// works for pointes as well as references
+class ToStringFormatter {
+  template <class T>
+  void dispatch(std::string* out, const T* const t, std::true_type) const {
+    return absl::AlphaNumFormatter()(out, t->ToString());
+  }
+  template <class T>
+  void dispatch(std::string* out, const T& t, std::false_type) const {
+    return absl::AlphaNumFormatter()(out, t.ToString());
+  }
+
+ public:
+  template <class T>
+  void operator()(std::string* out, const T& t) const {
+    return dispatch(out, t, std::is_pointer<T>());
+  }
+};
+
+struct BroadcastAndSlice {
+  SliceAndIndex broadcast;
+  UsesAndIntermediates uses;
+  std::string ToString() const {
+    return absl::StrCat("{", broadcast.ToString(), ", ",
+                        absl::StrJoin(uses.uses, ", ", NameFormatter()));
+  }
+  BroadcastAndSlice(SliceAndIndex broadcast, UsesAndIntermediates uses)
+      : broadcast(std::move(broadcast)), uses(std::move(uses)) {}
+};
+
+bool InputIsLoopInvariant(
+    HloInstruction* input,
+    const absl::flat_hash_set<HloInstruction*>& constants) {
+  return constants.contains(input);
+}
+
+absl::flat_hash_set<HloInstruction*> FindTripCounters(
+    HloInstruction* while_op) {
+  auto* body = while_op->while_body();
+  auto* root = body->root_instruction();
+  auto* while_init = while_op->mutable_operand(0);
+  absl::flat_hash_set<HloInstruction*> result;
+  for (int64 i = 0; i < root->operand_count(); ++i) {
+    if (!while_init->operand(i)->IsConstant() ||
+        !while_init->operand(i)->literal().IsAll(0)) {
+      continue;
+    }
+    HloInstruction* gte;
+    if (!Match(root->mutable_operand(i),
+               m::AddAnyOrder(m::GetTupleElement(&gte, m::Parameter(), i),
+                              m::ConstantScalar(1)))) {
+      continue;
+    }
+    result.emplace(gte);
+  }
+  return result;
+}
+
+bool UnexpectedWhileLoopForm(HloInstruction* while_op) {
+  HloComputation* comp = while_op->while_body();
+  return comp->root_instruction()->opcode() != HloOpcode::kTuple ||
+         comp->num_parameters() != 1 ||
+         while_op->mutable_operand(0)->opcode() != HloOpcode::kTuple;
+}
+
+std::vector<SliceAndIndex> FindTensorListBroadcasts(HloInstruction* while_op) {
+  if (UnexpectedWhileLoopForm(while_op)) {
+    VLOG(10) << "While loop in unexpected form " << while_op->name();
+    return {};  // Can't do any optimisations
+  }
+  HloComputation* comp = while_op->while_body();
+  const auto loop_invariants = FindLoopConstants(comp);
+  const auto trip_counters = FindTripCounters(while_op);
+  std::vector<SliceAndIndex> result;
+  for (auto* candidate_gte : FindSingleUseParameters(comp)) {
+    if (candidate_gte == nullptr) {
+      continue;
+    }
+    // We are looking for the pattern
+    // Root(
+    //      dynamic-update(gte
+    //                     loop_invariant,
+    //                     loop_counter))
+
+    // TODO(samuelh) would be better to do thus using instruction->Match with a
+    // matcher as much more readable
+
+    // Must only be used my a dynamic update and must be the operand updated
+    // into (eg operand 0)
+    auto candidate_update = candidate_gte->users();
+    if (candidate_update.size() != 1U ||
+        candidate_update[0]->opcode() != HloOpcode::kDynamicUpdateSlice ||
+        candidate_update[0]->mutable_operand(0) != candidate_gte) {
+      VLOG(10) << "Instruction used multiple times or not my dynamic update "
+               << candidate_update.size();
+      continue;
+    }
+    auto* updated_data = candidate_update[0];
+    auto* root = comp->root_instruction();
+    // Check no other instruction inside the while body are using the update
+    if (updated_data->users().size() != 1U ||
+        updated_data->users()[0] != root) {
+      VLOG(10) << "Dynamic update is used else where in the body";
+      continue;
+    }
+    // We have the only user of the gte element as a dynamic update, now
+    // check that this is fed to the root tuple at the same index as the gte
+    int64 parameter_index = candidate_gte->tuple_index();
+    if (root->mutable_operand(parameter_index) != updated_data) {
+      VLOG(10) << "Not at correct index to be an inplace update "
+               << parameter_index;
+      continue;
+    }
+    // We must now check that the slice is into the outer most dimension,
+    // that the input is a loop invariant, and that the index is the loop
+    // counter.
+    auto* input_slice = updated_data->mutable_operand(1);
+    if (!InputIsLoopInvariant(input_slice, loop_invariants)) {
+      VLOG(10) << "Input is not a loop invariant " << input_slice->name();
+      continue;
+    }
+    // Need to check the dimensions are correct, we are only expecting a slice
+    // into the first dimension.
+    if (ShapeUtil::DeleteDimension(0, updated_data->shape()) !=
+            ShapeUtil::DeleteDimension(0, input_slice->shape()) ||
+        ShapeUtil::GetDimension(input_slice->shape(), 0) != 1) {
+      VLOG(10) << "Update slice is wrong dimensions are wrong";
+      continue;
+    }
+    // We now know a loop invariant is being updated into the tensor
+    // list at every iteration, now need to confirm that the index of
+    // update is the loop counter
+    auto* index = updated_data->mutable_operand(2);
+    if (!trip_counters.contains(index)) {
+      VLOG(10) << "Update index is not a loop index counter " << index->name();
+      continue;
+    }
+    VLOG(10) << updated_data->name() << " is a tensor list broadcast";
+    result.emplace_back(candidate_gte->tuple_index(), updated_data);
+  }
+  return result;
+}
+
+template <class ArgsTemplate>
+void FindNonTrivialUses(HloInstruction* inst, ArgsTemplate& args,
+                        std::stack<int64>& tuple_indices) {
+  for (auto* user : inst->users()) {
+    if (user == user->parent()->root_instruction()) {
+      // If is root instruction treat as a non trivial use
+      // and exit out
+      args.DefaultAction(user);
+      continue;
+    }
+    VLOG(10) << "Visiting " << user->name();
+    switch (user->opcode()) {
+      case HloOpcode::kGetTupleElement: {
+        if (tuple_indices.top() == user->tuple_index()) {
+          args.GTEAction(user);
+          std::stack<int64> sub_tuple_indices = tuple_indices;
+          sub_tuple_indices.pop();
+          FindNonTrivialUses(user, args, sub_tuple_indices);
+        }
+        break;
+      }
+      case HloOpcode::kTuple: {
+        for (int64 i = 0; i < user->operands().size(); ++i) {
+          args.TupleAction(user);
+          if (user->mutable_operand(i) == inst) {
+            std::stack<int64> sub_tuple_indices = tuple_indices;
+            sub_tuple_indices.push(i);
+            FindNonTrivialUses(user, args, sub_tuple_indices);
+          }
+        }
+        break;
+      }
+      case HloOpcode::kWhile: {
+        CHECK_EQ(user->operands().size(), 1U);
+        args.WhileAction(user);
+        FindNonTrivialUses(user->while_body()->parameter_instruction(0), args,
+                           tuple_indices);
+        FindNonTrivialUses(user->while_condition()->parameter_instruction(0),
+                           args, tuple_indices);
+        // Don't need to check the users of the while because if the parameter
+        // gets to the root instruction of the while body it will be marked
+        // as non trivial and not be hit by AreAllSlices. If we wanted to make
+        // this function more general we would have to edit uses of root
+        // instructions to jump to instructions that call the computation
+        break;
+      }
+      default: {
+        args.DefaultAction(user);
+        break;
+      }
+    }
+  }
+}
+
+struct FindUsesTemplate {
+  absl::InlinedVector<HloInstruction*, 1> result;
+  absl::InlinedVector<HloInstruction*, 4> path;
+
+  void DefaultAction(HloInstruction* user) {
+    result.emplace_back(user);
+    path.emplace_back(user);
+  }
+  void GTEAction(HloInstruction* user) { path.emplace_back(user); }
+  void TupleAction(HloInstruction* user) { path.emplace_back(user); }
+  void WhileAction(HloInstruction* user) { path.emplace_back(user); }
+};
+
+UsesAndIntermediates FindNonTrivialUses(HloInstruction* inst,
+                                        std::stack<int64> starting_gte_stack) {
+  FindUsesTemplate args;
+  FindNonTrivialUses(inst, args, starting_gte_stack);
+  return {std::move(args.result), std::move(args.path)};
+}
+
+bool AllAreSlices(const absl::InlinedVector<HloInstruction*, 1>& vec) {
+  if (vec.empty()) {
+    // no uses so can just let other passes optimise away
+    return false;
+  }
+  return absl::c_all_of(vec, [](const HloInstruction* inst) {
+    return inst->opcode() == HloOpcode::kDynamicSlice;
+  });
+}
+
+bool SliceDimensionsAreCorrect(
+    const absl::InlinedVector<HloInstruction*, 1>& vec) {
+  return absl::c_all_of(vec, [](const HloInstruction* slice) {
+    return Is1DSliceInFirstDimension(slice);
+  });
+}
+
+std::vector<BroadcastAndSlice> FindBroadcastsOnlyUsedBySlices(
+    HloInstruction* while_loop, std::vector<SliceAndIndex> broadcasts) {
+  std::vector<BroadcastAndSlice> result;
+  result.reserve(broadcasts.size());
+  for (auto& broadcast : broadcasts) {
+    std::stack<int64> start;
+    start.push(0);
+    start.push(broadcast.input_index);
+    auto users = FindNonTrivialUses(while_loop, start);
+    if (!AllAreSlices(users.uses)) {
+      continue;
+    }
+    if (!SliceDimensionsAreCorrect(users.uses)) {
+      continue;
+    }
+    result.emplace_back(std::move(broadcast), std::move(users));
+  }
+  return result;
+}
+
+void RemoveTensorListBroadcasts(HloInstruction* inst,
+                                const std::vector<BroadcastAndSlice>& params) {
+  // Does nothing for now, as this diff only adds detection phase
+}
+
+}  // namespace
+
+StatusOr<bool> PoplarWhileLoopOptimiser::Run(HloModule* module) {
+  bool changed = false;
+  for (auto* comp : module->MakeComputationPostOrder()) {
+    for (auto* inst : comp->MakeInstructionPostOrder()) {
+      if (inst->opcode() != HloOpcode::kWhile) {
+        continue;
+      }
+      // Detect loop constants being pushed into tensor list
+      auto broadcast_params = FindTensorListBroadcasts(inst);
+      // Filter these tensor lists down to ones only used
+      // by slice instructions
+      auto slice_only_broadcast_params =
+          FindBroadcastsOnlyUsedBySlices(inst, std::move(broadcast_params));
+      if (slice_only_broadcast_params.empty()) {
+        continue;
+      }
+      RemoveTensorListBroadcasts(inst, slice_only_broadcast_params);
+      changed = true;
+    }
+  }
+  return changed;
+}
+
+int64 PoplarWhileLoopOptimiser::CountOptimisations(HloModule* module) const {
+  int64 result = 0;
+  for (auto* comp : module->MakeComputationPostOrder()) {
+    for (auto* inst : comp->MakeInstructionPostOrder()) {
+      if (inst->opcode() != HloOpcode::kWhile) {
+        continue;
+      }
+      auto broadcast_params = FindTensorListBroadcasts(inst);
+      auto slice_only_broadcast_params =
+          FindBroadcastsOnlyUsedBySlices(inst, std::move(broadcast_params));
+      VLOG(1) << "Can eliminate for " << inst->name() << ": {"
+              << absl::StrJoin(slice_only_broadcast_params, ", ",
+                               ToStringFormatter())
+              << "}";
+      result += slice_only_broadcast_params.size();
+    }
+  }
+  return result;
+}
+
+}  // namespace poplarplugin
+}  // namespace xla
---
+++ tensorflow/compiler/plugin/poplar/driver/passes/while_loop_optimiser.h
@@ -0,0 +1,40 @@
+/* Copyright 2022 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef TENSORFLOW_COMPILER_PLUGIN_POPLAR_DRIVER_PASSES_WHILE_LOOP_OPTIMISER_H_
+#define TENSORFLOW_COMPILER_PLUGIN_POPLAR_DRIVER_PASSES_WHILE_LOOP_OPTIMISER_H_
+
+#include "tensorflow/compiler/xla/service/hlo_pass_interface.h"
+
+namespace xla {
+
+class HloModule;
+
+namespace poplarplugin {
+
+class PoplarWhileLoopOptimiser : public HloModulePass {
+ public:
+  absl::string_view name() const override {
+    return "poplar-while-loop-optimiser";
+  }
+  StatusOr<bool> Run(HloModule* module) override;
+  // for testing only
+  int64 CountOptimisations(HloModule* module) const;
+};
+
+}  // namespace poplarplugin
+}  // namespace xla
+
+#endif  // TENSORFLOW_COMPILER_PLUGIN_POPLAR_DRIVER_PASSES_WHILE_LOOP_OPTIMISER_H_
