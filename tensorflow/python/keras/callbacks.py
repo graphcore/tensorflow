6d87f4baa3f567883038b6335f96173e250eb0f8
Jack Hunt <jackh@graphcore.ai>
2022-02-08 16:31:14 +0000

    Tensorboard example
    Summary: TF2.4 Only
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, yanislavd, georgep
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, yanislavd, georgep
    
    Subscribers: georgep
    
    Maniphest Tasks: T51864
    
    Differential Revision: https://phabricator.sourcevertex.net/D57504

--- tensorflow/python/keras/callbacks.py
+++ tensorflow/python/keras/callbacks.py
@@ -2240,7 +2240,12 @@ class TensorBoard(Callback, version_utils.TensorBoardVersionSelector):
     """Writes Keras model train_function graph to TensorBoard."""
     with self._train_writer.as_default():
       with summary_ops_v2.record_if(True):
-        train_fn = self.model.train_function
+        # Begin IPU specific changes.
+        if hasattr(self.model, '_ipu_train_function'):
+          train_fn = self.model._ipu_train_function
+        else:
+          train_fn = self.model.train_function
+        # End IPU specific changes.
         # If the train_function is a `tf.function`, we can write out a graph
         if hasattr(train_fn, 'function_spec'):
           summary_ops_v2.graph(train_fn._concrete_stateful_fn.graph)  # pylint: disable=protected-access
