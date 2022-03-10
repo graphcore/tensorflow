2a321d1b18e364a6d41beaee1beb6ccc63fc7646
yanislavd <yanislavd@graphcore.ai>
2022-03-10 15:54:44 +0000

    Add a StaticMultiSlice HLO instruction
    Summary: Add a StaticMultiSlice HLO instruction
    
    Test Plan: Numerical test
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, vladimirm, alfiee, jackh
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, alfiee
    
    Subscribers: harrym
    
    Maniphest Tasks: T54057
    
    Differential Revision: https://phabricator.sourcevertex.net/D62944

--- tensorflow/compiler/plugin/poplar/driver/tools/custom_ops/multi_slice.h
+++ tensorflow/compiler/plugin/poplar/driver/tools/custom_ops/multi_slice.h
@@ -16,6 +16,8 @@ limitations under the License.
 #ifndef TENSORFLOW_COMPILER_PLUGIN_POPLAR_DRIVER_TOOLS_CUSTOM_OPS_MULTI_SLICE_H_
 #define TENSORFLOW_COMPILER_PLUGIN_POPLAR_DRIVER_TOOLS_CUSTOM_OPS_MULTI_SLICE_H_
 
+#include <vector>
+
 #include "tensorflow/compiler/plugin/poplar/driver/tools/custom_ops/hlo_poplar_instruction.h"
 
 namespace xla {
@@ -49,6 +51,34 @@ class HloMultiSliceInstruction : public HloPoplarInstruction {
       const HloPrintOptions& options) const override;
 
   const bool indices_are_sorted_;
+
+ private:
+  std::unique_ptr<HloInstruction> CloneWithNewOperandsImpl(
+      const Shape& shape, absl::Span<HloInstruction* const>,
+      HloCloneContext*) const override;
+};
+
+class HloStaticMultiSliceInstruction : public HloPoplarInstruction {
+ public:
+  explicit HloStaticMultiSliceInstruction(const Shape& shape,
+                                          HloInstruction* const input,
+                                          absl::Span<const int64> indices);
+  absl::flat_hash_set<int64> AllocatingIndices() const override;
+  bool AllocatingOutput() const override;
+  absl::flat_hash_map<int64, int64> LayoutDependencies() const override;
+  HloPoplarUseDescriptions GetUseDescriptions() const override;
+  HloPoplarBufferDescriptions GetBufferDescriptions() const override;
+  const FindConsumersExtensionResults FindConsumers(
+      FindConsumersExtensionParams params) const override;
+  bool AllowNonInplaceLowering() const override;
+  bool IsPopOpsElementwise() const override;
+  const std::vector<int64>& GetIndices() const { return indices_; }
+
+ protected:
+  std::vector<std::string> ExtraPoplarAttributesToStringImpl(
+      const HloPrintOptions& options) const override;
+
+  const std::vector<int64> indices_;
 
  private:
   std::unique_ptr<HloInstruction> CloneWithNewOperandsImpl(
@@ -104,6 +134,10 @@ class HloMultiUpdateAddInstruction : public HloMultiUpdateInstruction {
 std::unique_ptr<HloInstruction> CreateMultiSlice(
     const Shape& shape, HloInstruction* const input,
     HloInstruction* const indices, bool indices_are_sorted = false);
+
+std::unique_ptr<HloInstruction> CreateStaticMultiSlice(
+    const Shape& shape, HloInstruction* const input,
+    absl::Span<const int64> indices);
 
 std::unique_ptr<HloInstruction> CreateMultiUpdate(
     const Shape& shape, absl::Span<HloInstruction* const> operands,
