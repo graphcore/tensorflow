1ae50994a9c6319fa8b672101ac5619afccc8d96
yanislavd <yanislavd@graphcore.ai>
2022-03-11 14:34:56 +0000

    Add a std::hash specialisation for `absl::Span<const T>` [TF1]
    Summary:
    Add a std::hash specialisation for `absl::Span<const T>`
    
    TF1.15 Only.
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, vladimirm, babakk
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, vladimirm, babakk
    
    Subscribers: babakk
    
    Maniphest Tasks: T54057
    
    Differential Revision: https://phabricator.sourcevertex.net/D62983

--- tensorflow/compiler/plugin/poplar/driver/tools/hash.h
+++ tensorflow/compiler/plugin/poplar/driver/tools/hash.h
@@ -1,4 +1,4 @@
-/* Copyright 2019 TensorFlow Ltd
+/* Copyright 2022 The TensorFlow Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -12,8 +12,12 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
+
 #ifndef TENSORFLOW_COMPILER_PLUGIN_POPLAR_DRIVER_TOOLS_HASH_H_
 #define TENSORFLOW_COMPILER_PLUGIN_POPLAR_DRIVER_TOOLS_HASH_H_
+
+#include "absl/types/span.h"
+
 #include "tensorflow/core/lib/hash/hash.h"
 
 namespace xla {
@@ -27,6 +31,15 @@ std::size_t hash(const T& v) {
   return std::hash<T>()(v);
 }
 
+template <typename T>
+std::size_t hash(const absl::Span<const T>& span) {
+  std::size_t seed = span.size();
+  for (const auto& v : span) {
+    seed = tensorflow::Hash64Combine(seed, hash<T>(v));
+  }
+  return seed;
+}
+
 template <typename T, typename... Args>
 std::size_t hash(const T& first, const Args&... args) {
   return tensorflow::Hash64Combine(hash(first), hash(args...));
@@ -35,5 +48,13 @@ std::size_t hash(const T& first, const Args&... args) {
 }  // namespace hash_util
 }  // namespace poplarplugin
 }  // namespace xla
+
+// Inject `std::hash<absl::Span<const T>>` in the `std` namespace.
+template <typename T>
+struct std::hash<absl::Span<const T>> {
+  std::size_t operator()(absl::Span<const T> const& span) const noexcept {
+    return xla::poplarplugin::hash_util::hash<T>(span);
+  }
+};
 
 #endif
