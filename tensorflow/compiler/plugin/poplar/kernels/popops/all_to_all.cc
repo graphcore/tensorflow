ce31e952e0f75978d268c6fa8c62e2a4ab649148
Christian aan de Wiel <christiana@graphcore.ai>
2022-03-17 16:38:32 +0100

    Move AllGather to its own file in poplar/kernels/popops
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep
    
    Maniphest Tasks: T58724
    
    Differential Revision: https://phabricator.sourcevertex.net/D63473

--- tensorflow/compiler/plugin/poplar/kernels/popops/all_to_all.cc
+++ tensorflow/compiler/plugin/poplar/kernels/popops/all_to_all.cc
@@ -23,64 +23,6 @@ limitations under the License.
 
 namespace tensorflow {
 
-class PopopsAllGather : public XlaOpKernel, IpuOpKernel {
- public:
-  using XlaOpKernel::XlaOpKernel;
-
-  explicit PopopsAllGather(OpKernelConstruction* ctx) : XlaOpKernel(ctx) {
-    OP_REQUIRES_OK(ctx,
-                   ctx->GetAttr("replication_factor", &replica_group_size_));
-  }
-
-  void Compile(XlaOpKernelContext* ctx) override {
-    attribute_map_.AddAttribute("replica_group_size", replica_group_size_);
-
-    // Build xla output shape.
-    std::vector<xla::Shape> xla_output_shapes;
-    xla_output_shapes.reserve(ctx->num_inputs());
-    for (int64 i = 0; i < ctx->num_inputs(); i++) {
-      // The output should be [replicationFactor][inputShape] shaped.
-      TensorShape output_shape = ctx->InputShape(i);
-      output_shape.InsertDim(0, replica_group_size_);
-
-      // Get the element type of the input.
-      xla::PrimitiveType input_type;
-      OP_REQUIRES_OK(ctx,
-                     DataTypeToPrimitiveType(ctx->input_type(i), &input_type));
-
-      // Convert the output shape into the XLA format.
-      xla_output_shapes.push_back(
-          TensorShapeToXLAShape(input_type, output_shape));
-    }
-    // Combine output shapes into tuple.
-    xla::Shape xla_output_shape =
-        xla::ShapeUtil::MakeTupleShape(xla_output_shapes);
-
-    xla::XlaBuilder& b = *ctx->builder();
-
-    std::vector<xla::XlaOp> input_values;
-    std::vector<TensorShape> input_shapes;
-    OP_REQUIRES_OK(ctx, ctx->InputList("inputs", &input_values, &input_shapes));
-
-    xla::XlaOp call_output =
-        xla::CustomCall(&b, PoplarOp_Name(PoplarOp::AllGather), input_values,
-                        xla_output_shape, attribute_map_.Serialise());
-
-    // Get each output value with a GTE.
-    for (int64 i = 0; i != ctx->num_inputs(); ++i) {
-      ctx->SetOutput(i, xla::GetTupleElement(call_output, i));
-    }
-  }
-
- private:
-  TF_DISALLOW_COPY_AND_ASSIGN(PopopsAllGather);
-
-  tensorflow::int64 replica_group_size_;
-};
-
-REGISTER_XLA_OP(Name("IpuAllGather").Device(DEVICE_IPU_XLA_JIT),
-                PopopsAllGather);
-
 class PopopsAllToAll : public XlaOpKernel {
  public:
   using XlaOpKernel::XlaOpKernel;
