390fc1fa8d5c44b693f76f0ca64703cfe0beeaf8
Sam Hornby <samuelh@graphcore.ai>
2022-03-24 15:20:18 +0000

    Add popit executor class
    Summary: Add the stream executor interface
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, alfiee, yanislavd, georgep, babakk, jackh
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep
    
    Maniphest Tasks: T53816
    
    Differential Revision: https://phabricator.sourcevertex.net/D63264

---
+++ tensorflow/compiler/plugin/poplar/driver/popit_backend/popit_executor.cc
@@ -0,0 +1,155 @@
+/* Copyright 2022 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#include "tensorflow/compiler/plugin/poplar/driver/popit_backend/popit_executor.h"
+
+#define NOT_IMPLEMENTED LOG(FATAL) << "Popit Not implemented";
+namespace xla {
+namespace poplarplugin {
+
+Status PopItExecutor::Init(int device_ordinal,
+                           se::DeviceOptions device_options) {
+  NOT_IMPLEMENTED;
+}
+
+se::DeviceMemoryBase PopItExecutor::Allocate(uint64 size, int64 memory_space) {
+  NOT_IMPLEMENTED;
+}
+void* PopItExecutor::GetSubBuffer(se::DeviceMemoryBase* parent, uint64 offset,
+                                  uint64 size) {
+  NOT_IMPLEMENTED;
+}
+void PopItExecutor::Deallocate(se::DeviceMemoryBase* mem) { NOT_IMPLEMENTED; }
+void* PopItExecutor::HostMemoryAllocate(uint64 size) { NOT_IMPLEMENTED; }
+void PopItExecutor::HostMemoryDeallocate(void* mem) { NOT_IMPLEMENTED; }
+bool PopItExecutor::HostMemoryRegister(void* mem, uint64 size) {
+  NOT_IMPLEMENTED;
+}
+bool PopItExecutor::HostMemoryUnregister(void* mem) { NOT_IMPLEMENTED; }
+bool PopItExecutor::SynchronizeAllActivity() { NOT_IMPLEMENTED; }
+Status PopItExecutor::SynchronousMemZero(se::DeviceMemoryBase* location,
+                                         uint64 size) {
+  NOT_IMPLEMENTED;
+}
+Status PopItExecutor::SynchronousMemSet(se::DeviceMemoryBase* location,
+                                        int value, uint64 size) {
+  NOT_IMPLEMENTED;
+}
+Status PopItExecutor::SynchronousMemcpy(se::DeviceMemoryBase* gpu_dst,
+                                        const void* host_src, uint64 size) {
+  NOT_IMPLEMENTED;
+}
+Status PopItExecutor::SynchronousMemcpy(void* host_dst,
+                                        const se::DeviceMemoryBase& gpu_src,
+                                        uint64 size) {
+  NOT_IMPLEMENTED;
+}
+Status PopItExecutor::SynchronousMemcpyDeviceToDevice(
+    se::DeviceMemoryBase* gpu_dst, const se::DeviceMemoryBase& gpu_src,
+    uint64 size) {
+  NOT_IMPLEMENTED;
+}
+Status PopItExecutor::MemZero(se::Stream* stream,
+                              se::DeviceMemoryBase* location, uint64 size) {
+  NOT_IMPLEMENTED;
+}
+Status PopItExecutor::Memset32(se::Stream* stream,
+                               se::DeviceMemoryBase* location, uint32 pattern,
+                               uint64 size) {
+  NOT_IMPLEMENTED;
+}
+bool PopItExecutor::Memcpy(se::Stream* stream, void* host_dst,
+                           const se::DeviceMemoryBase& gpu_src, uint64 size) {
+  NOT_IMPLEMENTED;
+}
+bool PopItExecutor::Memcpy(se::Stream* stream, se::DeviceMemoryBase* gpu_dst,
+                           const void* host_src, uint64 size) {
+  NOT_IMPLEMENTED;
+}
+bool PopItExecutor::MemcpyDeviceToDevice(se::Stream* stream,
+                                         se::DeviceMemoryBase* gpu_dst,
+                                         const se::DeviceMemoryBase& gpu_src,
+                                         uint64 size) {
+  NOT_IMPLEMENTED;
+}
+bool PopItExecutor::HostCallback(se::Stream* stream,
+                                 std::function<void()> callback) {
+  NOT_IMPLEMENTED;
+}
+bool PopItExecutor::HostCallback(se::Stream* stream,
+                                 std::function<Status()> callback) {
+  NOT_IMPLEMENTED;
+}
+Status PopItExecutor::AllocateEvent(se::Event* event) { NOT_IMPLEMENTED; }
+Status PopItExecutor::DeallocateEvent(se::Event* event) { NOT_IMPLEMENTED; }
+Status PopItExecutor::RecordEvent(se::Stream* stream, se::Event* event) {
+  NOT_IMPLEMENTED;
+}
+Status PopItExecutor::WaitForEvent(se::Stream* stream, se::Event* event) {
+  NOT_IMPLEMENTED;
+}
+se::Event::Status PopItExecutor::PollForEventStatus(se::Event* event) {
+  NOT_IMPLEMENTED;
+}
+bool PopItExecutor::AllocateStream(se::Stream* stream) { NOT_IMPLEMENTED; }
+void PopItExecutor::DeallocateStream(se::Stream* stream) { NOT_IMPLEMENTED; }
+bool PopItExecutor::CreateStreamDependency(se::Stream* dependent,
+                                           se::Stream* other) {
+  NOT_IMPLEMENTED;
+}
+bool PopItExecutor::AllocateTimer(se::Timer* timer) { NOT_IMPLEMENTED; }
+void PopItExecutor::DeallocateTimer(se::Timer* timer) { NOT_IMPLEMENTED; }
+bool PopItExecutor::StartTimer(se::Stream* stream, se::Timer* timer) {
+  NOT_IMPLEMENTED;
+}
+bool PopItExecutor::StopTimer(se::Stream* stream, se::Timer* timer) {
+  NOT_IMPLEMENTED;
+}
+Status PopItExecutor::BlockHostUntilDone(se::Stream* stream) {
+  NOT_IMPLEMENTED;
+}
+int PopItExecutor::PlatformDeviceCount() { NOT_IMPLEMENTED; }
+Status PopItExecutor::EnablePeerAccessTo(
+    se::internal::StreamExecutorInterface* other) {
+  NOT_IMPLEMENTED;
+}
+bool PopItExecutor::CanEnablePeerAccessTo(
+    se::internal::StreamExecutorInterface* other) {
+  NOT_IMPLEMENTED;
+}
+StatusOr<std::unique_ptr<se::DeviceDescription>>
+PopItExecutor::CreateDeviceDescription() const {
+  NOT_IMPLEMENTED;
+}
+std::unique_ptr<se::internal::EventInterface>
+PopItExecutor::CreateEventImplementation() {
+  NOT_IMPLEMENTED;
+}
+std::unique_ptr<se::internal::KernelInterface>
+PopItExecutor::CreateKernelImplementation() {
+  NOT_IMPLEMENTED;
+}
+std::unique_ptr<se::internal::StreamInterface>
+PopItExecutor::GetStreamImplementation() {
+  NOT_IMPLEMENTED;
+}
+std::unique_ptr<se::internal::TimerInterface>
+PopItExecutor::GetTimerImplementation() {
+  NOT_IMPLEMENTED;
+}
+
+}  // namespace poplarplugin
+}  // namespace xla
+
+#undef NOT_IMPLEMENTED
---
+++ tensorflow/compiler/plugin/poplar/driver/popit_backend/popit_executor.h
@@ -0,0 +1,103 @@
+/* Copyright 2022 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef TENSORFLOW_COMPILER_PLUGIN_POPLAR_DRIVER_POPIT_BACKEND_POPIT_EXECUTOR_H_
+#define TENSORFLOW_COMPILER_PLUGIN_POPLAR_DRIVER_POPIT_BACKEND_POPIT_EXECUTOR_H_
+
+#include <memory>
+
+#include "tensorflow/compiler/xla/statusor.h"
+#include "tensorflow/compiler/xla/types.h"
+#include "tensorflow/core/framework/types.pb.h"
+#include "tensorflow/stream_executor/platform.h"
+#include "tensorflow/stream_executor/stream_executor_internal.h"
+
+namespace se = stream_executor;
+
+namespace xla {
+namespace poplarplugin {
+
+class PopItExecutor : public se::internal::StreamExecutorInterface {
+ public:
+  Status Init(int device_ordinal, se::DeviceOptions device_options) override;
+  se::DeviceMemoryBase Allocate(uint64 size, int64 memory_space) override;
+  void* GetSubBuffer(se::DeviceMemoryBase* parent, uint64 offset,
+                     uint64 size) override;
+  void Deallocate(se::DeviceMemoryBase* mem) override;
+  void* HostMemoryAllocate(uint64 size) override;
+  void HostMemoryDeallocate(void* mem) override;
+  bool HostMemoryRegister(void* mem, uint64 size) override;
+  bool HostMemoryUnregister(void* mem) override;
+  bool SynchronizeAllActivity() override;
+  Status SynchronousMemZero(se::DeviceMemoryBase* location,
+                            uint64 size) override;
+  Status SynchronousMemSet(se::DeviceMemoryBase* location, int value,
+                           uint64 size) override;
+  Status SynchronousMemcpy(se::DeviceMemoryBase* gpu_dst, const void* host_src,
+                           uint64 size) override;
+  Status SynchronousMemcpy(void* host_dst, const se::DeviceMemoryBase& gpu_src,
+                           uint64 size) override;
+  Status SynchronousMemcpyDeviceToDevice(se::DeviceMemoryBase* gpu_dst,
+                                         const se::DeviceMemoryBase& gpu_src,
+                                         uint64 size) override;
+  Status MemZero(se::Stream* stream, se::DeviceMemoryBase* location,
+                 uint64 size) override;
+  Status Memset32(se::Stream* stream, se::DeviceMemoryBase* location,
+                  uint32 pattern, uint64 size) override;
+  bool Memcpy(se::Stream* stream, void* host_dst,
+              const se::DeviceMemoryBase& gpu_src, uint64 size) override;
+  bool Memcpy(se::Stream* stream, se::DeviceMemoryBase* gpu_dst,
+              const void* host_src, uint64 size) override;
+  bool MemcpyDeviceToDevice(se::Stream* stream, se::DeviceMemoryBase* gpu_dst,
+                            const se::DeviceMemoryBase& gpu_src,
+                            uint64 size) override;
+  bool HostCallback(se::Stream* stream, std::function<void()> callback);
+  bool HostCallback(se::Stream* stream,
+                    std::function<Status()> callback) override;
+  Status AllocateEvent(se::Event* event) override;
+  Status DeallocateEvent(se::Event* event) override;
+  Status RecordEvent(se::Stream* stream, se::Event* event) override;
+  Status WaitForEvent(se::Stream* stream, se::Event* event) override;
+  se::Event::Status PollForEventStatus(se::Event* event) override;
+  bool AllocateStream(se::Stream* stream) override;
+  void DeallocateStream(se::Stream* stream) override;
+  bool CreateStreamDependency(se::Stream* dependent,
+                              se::Stream* other) override;
+  bool AllocateTimer(se::Timer* timer) override;
+  void DeallocateTimer(se::Timer* timer) override;
+  bool StartTimer(se::Stream* stream, se::Timer* timer) override;
+  bool StopTimer(se::Stream* stream, se::Timer* timer) override;
+  Status BlockHostUntilDone(se::Stream* stream) override;
+  int PlatformDeviceCount() override;
+  Status EnablePeerAccessTo(
+      se::internal::StreamExecutorInterface* other) override;
+  bool CanEnablePeerAccessTo(
+      se::internal::StreamExecutorInterface* other) override;
+  StatusOr<std::unique_ptr<se::DeviceDescription>> CreateDeviceDescription()
+      const override;
+  std::unique_ptr<se::internal::EventInterface> CreateEventImplementation()
+      override;
+  std::unique_ptr<se::internal::KernelInterface> CreateKernelImplementation()
+      override;
+  std::unique_ptr<se::internal::StreamInterface> GetStreamImplementation()
+      override;
+  std::unique_ptr<se::internal::TimerInterface> GetTimerImplementation()
+      override;
+};
+
+}  // namespace poplarplugin
+}  // namespace xla
+
+#endif  // TENSORFLOW_COMPILER_PLUGIN_POPLAR_DRIVER_POPIT_BACKEND_POPIT_EXECUTOR_H_
