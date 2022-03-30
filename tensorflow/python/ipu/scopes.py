6738786ad00911e751d14d797e9e5a90de45409e
Babak Khataee <babakk@graphcore.ai>
2022-03-30 10:25:14 +0100

    TF2 - Use sharded gradient functions for sharded ops with tf.gradients
    Summary:
    This change adds sharded gradient functions to ops that are created with an `scopes.ipu_shard` context. Backwards ops created from the forward ones will inherit their sharding attribute and so use the same device. One subtlety with this is that intermediate Ops created during the gradients call,but not from a specific forward Op, e.g. `AddN` used for accumulation of partials, will not be sharded.
    
    This only applies when using `tf.gradients`. `GradientTape` uses a different mechanism to call the gradient function (since it uses `MockOps` instead of real ones) and so will be addressed separately.
    
    TF2.5 Only (the op._gradient_function API doesn't exist in TF1.)
    
    TF1 - D63685
    
    Test Plan: CI + New tests
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep
    
    Subscribers: georgep
    
    Maniphest Tasks: T48430
    
    Differential Revision: https://phabricator.sourcevertex.net/D63353

--- tensorflow/python/ipu/scopes.py
+++ tensorflow/python/ipu/scopes.py
@@ -21,6 +21,7 @@ import numpy as np
 from tensorflow.compiler.xla import xla_data_pb2
 from tensorflow.core.framework import attr_value_pb2
 from tensorflow.python.framework import ops
+from tensorflow.python.ipu import sharding
 from tensorflow.python.ops import control_flow_util
 from tensorflow.python.ops.variable_scope import variable_scope
 from tensorflow.python.util import tf_contextlib
@@ -128,7 +129,10 @@ def ipu_shard(index):
   """Control sharding for a set of operations.
 
   Provides a scope which targets operations onto a particular shard (IPU) of a
-  multi-IPU sharded device.
+  multi-IPU sharded device. Gradients created from these operations will
+  also be put onto the same shard. Consequently an `ipu_shard` scope
+  enclosing a call to `tf.gradients` or `tf.GradientTape.gradient` won't change
+  the sharding of the backwards ops.
 
   Args:
     index: The index of the IPU on which to place the enclosed operations.
@@ -144,13 +148,23 @@ def ipu_shard(index):
 
   proto = xla_data_pb2.OpSharding(type=xla_data_pb2.OpSharding.MAXIMAL,
                                   tile_assignment_devices=ipus)
-
   attr_value = attr_value_pb2.AttrValue(s=proto.SerializeToString())
   attrs = {"_XlaSharding": attr_value}
 
   # pylint: disable=protected-access
+
+  old_operations = ops.get_default_graph().get_operations()
+
   with ops.get_default_graph()._attr_scope(attrs):
     yield
+
+  operations = ops.get_default_graph().get_operations()
+  new_operations = [op for op in operations if op not in old_operations]
+  # Make sure that the gradients of the new Ops also have the same sharding.
+  # There's a context manager that does the same thing, but this way we dont
+  # accidentally override what would have been the intended gradient function.
+  _make_gradients_sharded(new_operations)
+
   # pylint: enable=protected-access
 
 
@@ -242,3 +256,47 @@ def partials_type(override_type):
       xla_data_pb2.PrimitiveType.Name(xla_type),
       xla_data_pb2.PrimitiveType.Name(xla_data_pb2.PRIMITIVE_TYPE_INVALID)):
     yield
+
+
+class _ShardedGradientWrapper:  # pylint: disable=missing-type-doc,missing-param-doc
+  """ Utility type for wrapping a call to an ops gradient function.
+  """
+  def __init__(self, gradient_fn):
+    self._gradient_fn = gradient_fn
+
+  def __call__(self, op, *args, **kwargs):
+    shard = sharding.get_sharding(op)
+    assert shard is not None, "Expected forward Op to be sharded."
+
+    with ipu_shard(shard):
+      return self._gradient_fn(op, *args, **kwargs)
+
+
+def _make_gradients_sharded(operations):
+  for op in operations:
+    gradient_fn = _get_gradient_function(op)
+    has_gradient = gradient_fn is not None
+
+    # Pipeline backward stage cant be explicitly sharded, its implicitly
+    # sharded based on the corresponding forward stage.
+    if has_gradient and op.type != "PipelineStage":
+
+      # An Op might already have a shared gradient if ipu_shard scopes
+      # are nested.
+      already_sharded = isinstance(gradient_fn, _ShardedGradientWrapper)
+      if not already_sharded:
+        # Using _ShardedGradientWrapper instead of a lambd/functools.partial so we can
+        # easily check whether something is already wrapped.
+        # There are several ways of setting the gradient function for an Op
+        # but this method has the highest precedence.
+        op._gradient_function = _ShardedGradientWrapper(  # pylint: disable=protected-access
+            gradient_fn)
+
+
+def _get_gradient_function(op):
+  try:
+    gradient_fn = ops.get_gradient_function(op)
+  except LookupError:
+    return None
+
+  return gradient_fn
