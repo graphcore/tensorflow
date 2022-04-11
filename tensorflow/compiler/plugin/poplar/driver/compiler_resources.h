519902b7f7337f332edb5888d3658f4855f33849
Samuel Hornby <samuelh@graphcore.ai>
2022-04-11 17:07:15 +0100

    Start splitting poplar_compiler into smaller functions popit can use
    Summary:
    So that we can share code between popit and poplar compilers try split
    shared parts into functions. This diff does this for the transform hlo
    passes. It also changes compiler resources to only provide the resources
    it uses to this function by creating a HloResources struct. This is mainly
    just to ensure no one uses the graph in any of these passes.
    
    This is a non functional change
    
    TF2.5 only
    
    Reviewers: jackh, vladimirm, #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, babakk
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, babakk
    
    Maniphest Tasks: T57888
    
    Differential Revision: https://phabricator.sourcevertex.net/D64987

--- tensorflow/compiler/plugin/poplar/driver/compiler_resources.h
+++ tensorflow/compiler/plugin/poplar/driver/compiler_resources.h
@@ -49,9 +49,91 @@ class CallGraph;
 namespace poplarplugin {
 class PartitionedElementwiseClusterVisitor;
 
+struct HloResources {
+  HloResources(
+      HloModule* module, CompilerInformation information,
+      IpuOptions::FloatingPointBehaviour global_floating_point_behaviour,
+      uint32 replication_factor, uint32 local_replication_factor,
+      uint32 partition_replication_factor, bool merge_infeed_io_copies,
+      bool always_rearrange_copies_on_host,
+      IpuSchedulingAlgorithm scheduler_selection, bool recomputation_enabled,
+      int64 experimental_distributed_batch_norm_replica_group_size,
+      bool remote_memory_supported, bool enable_experimental_prng_stability,
+      bool enable_fast_math, int64 num_io_tiles,
+      double io_tile_available_memory_proportion, bool enable_progress_bar)
+      : annotations(module),
+        information(std::move(information)),
+        global_floating_point_behaviour(
+            std::move(global_floating_point_behaviour)),
+        replication_factor(replication_factor),
+        local_replication_factor(local_replication_factor),
+        partition_replication_factor(partition_replication_factor),
+        merge_infeed_io_copies(merge_infeed_io_copies),
+        always_rearrange_copies_on_host(always_rearrange_copies_on_host),
+        scheduler_selection(std::move(scheduler_selection)),
+        recomputation_enabled(recomputation_enabled),
+        experimental_distributed_batch_norm_replica_group_size(
+            experimental_distributed_batch_norm_replica_group_size),
+        remote_memory_supported(remote_memory_supported),
+        enable_experimental_prng_stability(enable_experimental_prng_stability),
+        enable_fast_math(enable_fast_math),
+        num_io_tiles(num_io_tiles),
+        io_tile_available_memory_proportion(
+            io_tile_available_memory_proportion) {
+    if (enable_progress_bar) {
+      progress_bar = absl::make_unique<ProgressBar>(module);
+    } else {
+      progress_bar = absl::make_unique<NoProgressBar>();
+    }
+  }
+
+  std::vector<unsigned> shard_to_ipu_id;
+
+  CompilerAnnotations annotations;
+
+  const CompilerInformation information;
+
+  const IpuOptions::FloatingPointBehaviour global_floating_point_behaviour;
+  /* The global number of replicas that we are compiling for. */
+  uint32 replication_factor;
+  /* The local number of replicas owned by this process. This is the number of
+   * replicas that we are responsible for at run-time in this process. This is
+   * only different from the `replication_factor` when using multi-replica
+   * distribution with the Poplar "runtime replica subset" feature. */
+  uint32 local_replication_factor;
+  /* The number of replicas that we perform replica partitioning across. */
+  uint32 partition_replication_factor;
+
+  bool merge_infeed_io_copies;
+
+  bool always_rearrange_copies_on_host;
+
+  IpuSchedulingAlgorithm scheduler_selection;
+
+  bool recomputation_enabled;
+
+  int64 experimental_distributed_batch_norm_replica_group_size;
+
+  bool remote_memory_supported;
+
+  bool enable_experimental_prng_stability;
+
+  bool enable_fast_math;
+
+  int64 num_io_tiles;
+
+  double io_tile_available_memory_proportion;
+
+  absl::flat_hash_map<const HloInstruction*, std::uint64_t>
+      hlo_instruction_to_debug_id_mapping;
+
+  // The implementation of the progress bar.
+  std::unique_ptr<ProgressBarBase> progress_bar;
+};
+
 // This structure contains additional information required to lower the graph
 // from an XLA graph to a poplar graph.
-struct CompilerResources {
+struct CompilerResources : public HloResources {
   std::unique_ptr<DriverGraph> main_graph;
 
   // If IO tiles are not in use, these are nullopt (use main_graph instead).
@@ -62,8 +144,6 @@ struct CompilerResources {
   std::vector<DriverGraph> shard_compute_graphs;
   std::vector<DriverGraph> shard_io_graphs;
 
-  std::vector<unsigned> shard_to_ipu_id;
-
   absl::flat_hash_map<const HloInstruction*, const popops::SlicePlan*>
       slice_plan_mappings;
 
@@ -73,15 +153,9 @@ struct CompilerResources {
 
   absl::flat_hash_map<const HloInstruction*, const popnn::ctc::Plan> ctc_plans;
 
-  CompilerAnnotations annotations;
-
-  const CompilerInformation information;
-
   poplin::PlanningCache convolution_cache;
 
   poplin::matmul::PlanningCache matmul_cache;
-
-  const IpuOptions::FloatingPointBehaviour global_floating_point_behaviour;
 
   const poplar::OptionFlags default_conv_options;
 
@@ -91,25 +165,9 @@ struct CompilerResources {
 
   const poplar::OptionFlags default_slice_options;
 
-  bool clear_matmul_pass_type;
+  bool clear_matmul_pass_type = false;
 
-  bool disable_graph_outlining;
-
-  /* The global number of replicas that we are compiling for. */
-  uint32 replication_factor;
-
-  /* The local number of replicas owned by this process. This is the number of
-   * replicas that we are responsible for at run-time in this process. This is
-   * only different from the `replication_factor` when using multi-replica
-   * distribution with the Poplar "runtime replica subset" feature. */
-  uint32 local_replication_factor;
-
-  /* The number of replicas that we perform replica partitioning across. */
-  uint32 partition_replication_factor;
-
-  bool merge_infeed_io_copies;
-
-  bool always_rearrange_copies_on_host;
+  bool disable_graph_outlining = false;
 
   TensorMaps tensor_maps;
 
@@ -133,15 +191,7 @@ struct CompilerResources {
 
   std::stack<ExecutionCounters*> execution_counter_scopes;
 
-  IpuSchedulingAlgorithm scheduler_selection;
-
-  bool recomputation_enabled;
-
   bool use_stable_norm_statistics;
-
-  int64 experimental_distributed_batch_norm_replica_group_size;
-
-  bool remote_memory_supported;
 
   poplar::OptionFlags gcl_options;
 
@@ -156,14 +206,6 @@ struct CompilerResources {
 
   bool enable_experimental_remote_buffer_embedding;
 
-  bool enable_experimental_prng_stability;
-
-  bool enable_fast_math;
-
-  int64 num_io_tiles;
-
-  double io_tile_available_memory_proportion;
-
   absl::flat_hash_set<std::string> custom_codelets_in_graph;
 
   absl::flat_hash_map<std::string,
@@ -177,13 +219,7 @@ struct CompilerResources {
   // TODO(T28772): remove this mapping and the extra copy.
   absl::flat_hash_map<std::string, poplar::Tensor> remote_buffer_layouts;
 
-  absl::flat_hash_map<const HloInstruction*, std::uint64_t>
-      hlo_instruction_to_debug_id_mapping;
-
   PartitionedElementwiseClusterVisitor* current_cluster_visitor;
-
-  // The implementation of the progress bar.
-  std::unique_ptr<ProgressBarBase> progress_bar;
 
   // Two flags for SR. One to keep track of whether its currently enabled
   // and another to keep track of whether we enabled it at all and so
@@ -196,64 +232,47 @@ struct CompilerResources {
 
   CompilerResources(
       HloModule* module, const CompilerInformation& information,
-      const poplar::OptionFlags& conv_options,
-      const poplar::OptionFlags& matmul_options,
-      const poplar::OptionFlags& pooling_options,
-      const poplar::OptionFlags& slice_options, bool clear_matmul_pass_type,
-      bool disable_graph_outlining, bool merge_infeed_io_copies,
-      uint32 replication_factor, uint32 local_replication_factor,
-      uint32 partition_replication_factor,
+      poplar::OptionFlags conv_options, poplar::OptionFlags matmul_options,
+      poplar::OptionFlags pooling_options, poplar::OptionFlags slice_options,
+      bool clear_matmul_pass_type, bool disable_graph_outlining,
+      bool merge_infeed_io_copies, uint32 replication_factor,
+      uint32 local_replication_factor, uint32 partition_replication_factor,
       const IpuOptions::FloatingPointBehaviour& floating_point_behaviour,
       bool always_rearrange_copies_on_host,
       IpuSchedulingAlgorithm scheduler_selection, bool recomputation_enabled,
       bool use_stable_norm_statistics,
       int64 experimental_distributed_batch_norm_replica_group_size,
-      bool remote_memory_supported, const poplar::OptionFlags& gcl_options,
+      bool remote_memory_supported, poplar::OptionFlags gcl_options,
       int64 triangular_solve_expander_block_size, int64 cholesky_block_size,
       bool enable_experimental_remote_buffer_embedding,
       bool enable_experimental_prng_stability, bool enable_fast_math,
       int64 num_io_tiles, double io_tile_available_memory_proportion,
       bool enable_progress_bar)
-      : annotations(module),
-        information(information),
-        global_floating_point_behaviour(floating_point_behaviour),
-        default_conv_options(conv_options),
-        default_matmul_options(matmul_options),
-        default_pooling_options(pooling_options),
-        default_slice_options(slice_options),
+      : HloResources(
+            module, information, floating_point_behaviour, replication_factor,
+            local_replication_factor, partition_replication_factor,
+            merge_infeed_io_copies, always_rearrange_copies_on_host,
+            std::move(scheduler_selection), recomputation_enabled,
+            experimental_distributed_batch_norm_replica_group_size,
+            remote_memory_supported, enable_experimental_prng_stability,
+            enable_fast_math, num_io_tiles, io_tile_available_memory_proportion,
+            enable_progress_bar),
+        default_conv_options(std::move(conv_options)),
+        default_matmul_options(std::move(matmul_options)),
+        default_pooling_options(std::move(pooling_options)),
+        default_slice_options(std::move(slice_options)),
         clear_matmul_pass_type(clear_matmul_pass_type),
         disable_graph_outlining(disable_graph_outlining),
-        replication_factor(replication_factor),
-        local_replication_factor(local_replication_factor),
-        partition_replication_factor(partition_replication_factor),
-        merge_infeed_io_copies(merge_infeed_io_copies),
-        always_rearrange_copies_on_host(always_rearrange_copies_on_host),
-        scheduler_selection(scheduler_selection),
-        recomputation_enabled(recomputation_enabled),
         use_stable_norm_statistics(use_stable_norm_statistics),
-        experimental_distributed_batch_norm_replica_group_size(
-            experimental_distributed_batch_norm_replica_group_size),
-        remote_memory_supported(remote_memory_supported),
         gcl_options(gcl_options),
         triangular_solve_expander_block_size(
             triangular_solve_expander_block_size),
         cholesky_block_size(cholesky_block_size),
         enable_experimental_remote_buffer_embedding(
             enable_experimental_remote_buffer_embedding),
-        enable_experimental_prng_stability(enable_experimental_prng_stability),
-        enable_fast_math(enable_fast_math),
-        num_io_tiles(num_io_tiles),
-        io_tile_available_memory_proportion(
-            io_tile_available_memory_proportion),
         current_cluster_visitor(nullptr),
         stochastic_rounding_enabled(floating_point_behaviour.esr()),
         stochastic_rounding_used(stochastic_rounding_enabled) {
-    if (enable_progress_bar) {
-      progress_bar = absl::make_unique<ProgressBar>(module);
-    } else {
-      progress_bar = absl::make_unique<NoProgressBar>();
-    }
-
     CHECK_EQ(enable_prng_seed_consistency_checks, false)
         << "PRNG seed consistency checks are only intended for use when "
            "testing.";
@@ -299,40 +318,33 @@ struct CompilerResources {
       HloModule* module, bool enable_prng_seed_consistency_checks,
       const IpuOptions::FloatingPointBehaviour& floating_point_behaviour,
       const CompilerInformation& information)
-      : annotations(module),
-        information(information),
-        global_floating_point_behaviour(floating_point_behaviour),
-        default_conv_options(),
-        default_matmul_options(),
-        default_pooling_options(),
-        default_slice_options(),
-        clear_matmul_pass_type(false),
-        disable_graph_outlining(false),
-        replication_factor(1),
-        local_replication_factor(1),
-        partition_replication_factor(1),
-        merge_infeed_io_copies(false),
-        always_rearrange_copies_on_host(false),
-        scheduler_selection(IpuSchedulingAlgorithm::CHOOSE_BEST),
-        recomputation_enabled(false),
-        use_stable_norm_statistics(false),
-        experimental_distributed_batch_norm_replica_group_size(1),
-        remote_memory_supported(false),
-        gcl_options(),
-        triangular_solve_expander_block_size(0),
-        cholesky_block_size(9),
-        enable_experimental_remote_buffer_embedding(false),
-        enable_experimental_prng_stability(false),
-        enable_fast_math(false),
-        num_io_tiles(0),
-        io_tile_available_memory_proportion(0.9),
-        current_cluster_visitor(nullptr),
-        stochastic_rounding_enabled(floating_point_behaviour.esr()),
-        stochastic_rounding_used(stochastic_rounding_enabled),
-        enable_prng_seed_consistency_checks(
-            enable_prng_seed_consistency_checks) {
-    progress_bar = absl::make_unique<NoProgressBar>();
-  }
+      : CompilerResources(
+            module, information,
+            /*conv_options=*/{},
+            /*matmul_options=*/{},
+            /*pooling_options=*/{},
+            /*slice_options=*/{},
+            /*clear_matmul_pass_type=*/false,
+            /*disable_graph_outlining=*/false,
+            /*merge_infeed_io_copies=*/false,
+            /*replication_factor=*/1,
+            /*local_replication_factor=*/1,
+            /*partition_replication_factor=*/1, floating_point_behaviour,
+            /*always_rearrange_copies_on_host=*/false,
+            IpuSchedulingAlgorithm::CHOOSE_BEST,
+            /*recomputation_enabled=*/false,
+            /*use_stable_norm_statistics=*/false,
+            /*experimental_distributed_batch_norm_replica_group_size=*/1,
+            /*remote_memory_supported=*/false,
+            /*gcl_options=*/{},
+            /*triangular_solve_expander_block_size=*/0,
+            /*cholesky_block_size=*/9,
+            /*enable_experimental_remote_buffer_embedding=*/false,
+            /*enable_experimental_prng_stability=*/false,
+            /*enable_fast_math=*/false,
+            /*num_io_tiles=*/0,
+            /*io_tile_available_memory_proportion=*/0.9,
+            /*enable_progress_bar=*/false) {}
 };
 
 }  // namespace poplarplugin
