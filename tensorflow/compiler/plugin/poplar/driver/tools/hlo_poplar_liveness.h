c055c0cd552a9214a60a7c030eaab33d5c1260d3
Babak Khataee <babakk@graphcore.ai>
2022-04-19 15:36:35 +0100

    Hlo liveness analysis.
    Summary: Initial version of our liveness analysis. Simple algorithm that works ontop of the output of the dataflow analysis, which does most of the work, to determine when buffers become live/dead. Goes backwards from a schedule, marking buffers alive from last read to the point they first get defined.
    
    Test Plan: New tests.
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, samuelh, vladimirm
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, vladimirm
    
    Maniphest Tasks: T9494
    
    Differential Revision: https://phabricator.sourcevertex.net/D65131

---
+++ tensorflow/compiler/plugin/poplar/driver/tools/hlo_poplar_liveness.h
@@ -0,0 +1,44 @@
+/* Copyright 2022 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef TENSORFLOW_COMPILER_PLUGIN_POPLAR_DRIVER_TOOLS_HLO_POPLAR_LIVENESS_H_
+#define TENSORFLOW_COMPILER_PLUGIN_POPLAR_DRIVER_TOOLS_HLO_POPLAR_LIVENESS_H_
+
+#include <unordered_map>
+#include <vector>
+
+#include "tensorflow/compiler/plugin/poplar/driver/tools/hlo_poplar_buffer.h"
+
+#include "tensorflow/compiler/xla/service/hlo_instruction.h"
+
+namespace xla {
+namespace poplarplugin {
+
+// Find all the HloPoplarBuffer objects read/written to by each instruction in
+// the given module.
+HloInstructionMap<HloPoplarBufferSet> FindUsedBuffers(
+    const HloModule* module,
+    const InstructionBufferSets& instruction_buffer_sets);
+
+// Generate a liveness map for the instructions in the given schedule. This
+// describes which buffers are live at each instruction of the schedule.
+HloInstructionMap<HloPoplarBufferIdSet> GenerateProgramLiveness(
+    const std::vector<HloInstruction*>& flat_schedule,
+    const HloInstructionMap<HloPoplarBufferSet>& buffer_usages);
+
+}  // namespace poplarplugin
+}  // namespace xla
+
+#endif  // TENSORFLOW_COMPILER_PLUGIN_POPLAR_DRIVER_TOOLS_HLO_POPLAR_LIVENESS_H_
