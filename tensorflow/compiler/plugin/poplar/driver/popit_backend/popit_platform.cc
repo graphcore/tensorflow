a69b04e69b30a644cf19dd705937e16ce722d85c
Sam Hornby <samuelh@graphcore.ai>
2022-04-26 15:10:49 +0100

    Add popit platform class
    Summary: Add the popit platform class and implement methods that are trivial
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, babakk, vladimirm, yanislavd, alfiee
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, babakk
    
    Maniphest Tasks: T53816
    
    Differential Revision: https://phabricator.sourcevertex.net/D65785

---
+++ tensorflow/compiler/plugin/poplar/driver/popit_backend/popit_platform.cc
@@ -0,0 +1,102 @@
+/* Copyright 2022 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#include "tensorflow/compiler/plugin/poplar/driver/popit_backend/popit_platform.h"
+
+#include <poplar/Device.hpp>
+#include <poplar/Graph.hpp>
+
+#include "tensorflow/compiler/plugin/poplar/driver/popit_backend/popit_executor.h"
+#include "tensorflow/compiler/plugin/poplar/driver/popit_backend/popit_platform_id.h"
+#include "tensorflow/compiler/plugin/poplar/driver/poplar_executor.h"
+#include "tensorflow/compiler/plugin/poplar/driver/tools/poplar_util.h"
+
+namespace se = ::stream_executor;
+
+namespace xla {
+namespace poplarplugin {
+
+PopItPlatform::PopItPlatform() : name_("PopIt") {
+  CheckPoplarPackageHash();
+  VLOG(1) << "Poplar version: " << poplar::versionString()
+          << " Poplar package: " << poplar::packageHash();
+}
+
+PopItPlatform::~PopItPlatform() {}
+
+se::Platform::Id PopItPlatform::id() const { return kPopItPlatformId; }
+
+int PopItPlatform::VisibleDeviceCount() const {
+  int num_devices = PoplarExecutor::GetDeviceManager()
+                        .getDevices(poplar::TargetType::IPU, 1)
+                        .size();
+
+  if (num_devices == 0) {
+    // Allow for 2 virtual devices
+    num_devices = 2;
+  }
+
+  return num_devices;
+}
+
+const std::string& PopItPlatform::Name() const { return name_; }
+
+StatusOr<se::StreamExecutor*> PopItPlatform::ExecutorForDevice(int ordinal) {
+  return ExecutorForDeviceWithPluginConfig(ordinal, se::PluginConfig());
+}
+
+StatusOr<se::StreamExecutor*> PopItPlatform::ExecutorForDeviceWithPluginConfig(
+    int device_ordinal, const se::PluginConfig& plugin_config) {
+  se::StreamExecutorConfig config;
+  config.ordinal = device_ordinal;
+  config.plugin_config = plugin_config;
+  config.device_options = se::DeviceOptions::Default();
+  return GetExecutor(config);
+}
+
+StatusOr<se::StreamExecutor*> PopItPlatform::GetExecutor(
+    const se::StreamExecutorConfig& config) {
+  return executor_cache_.GetOrCreate(
+      config, [&]() { return GetUncachedExecutor(config); });
+}
+
+StatusOr<std::unique_ptr<se::StreamExecutor>>
+PopItPlatform::GetUncachedExecutor(const se::StreamExecutorConfig& config) {
+  auto executor = absl::make_unique<se::StreamExecutor>(
+      this, absl::make_unique<PopItExecutor>(), config.ordinal);
+  TF_RETURN_IF_ERROR(executor->Init(config.device_options));
+
+  return executor;
+}
+
+void PopItPlatform::RegisterTraceListener(
+    std::unique_ptr<se::TraceListener> listener) {
+  LOG(FATAL) << "not yet implemented: register Poplar trace listener";
+}
+
+void PopItPlatform::UnregisterTraceListener(se::TraceListener* listener) {
+  LOG(FATAL) << "not yet implemented: unregister Poplar trace listener";
+}
+
+StatusOr<std::unique_ptr<se::DeviceDescription>>
+PopItPlatform::DescriptionForDevice(int ordinal) const {
+  se::internal::DeviceDescriptionBuilder builder;
+  builder.set_name("PopIt");
+  builder.set_platform_version("");
+
+  return builder.Build();
+}
+
+}  // namespace poplarplugin
+}  // namespace xla
---
+++ tensorflow/compiler/plugin/poplar/driver/popit_backend/popit_platform.h
@@ -0,0 +1,94 @@
+/* Copyright 2022 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef TENSORFLOW_COMPILER_PLUGIN_POPLAR_DRIVER_POPIT_BACKEND_POPIT_PLATFORM_H_
+#define TENSORFLOW_COMPILER_PLUGIN_POPLAR_DRIVER_POPIT_BACKEND_POPIT_PLATFORM_H_
+
+#include <list>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "absl/base/thread_annotations.h"
+#include "absl/synchronization/mutex.h"
+#include "tensorflow/compiler/plugin/poplar/driver/config.pb.h"
+#include "tensorflow/compiler/xla/statusor.h"
+#include "tensorflow/core/platform/mutex.h"
+#include "tensorflow/stream_executor/device_description.h"
+#include "tensorflow/stream_executor/executor_cache.h"
+#include "tensorflow/stream_executor/multi_platform_manager.h"
+#include "tensorflow/stream_executor/platform.h"
+#include "tensorflow/stream_executor/platform/port.h"
+#include "tensorflow/stream_executor/stream_executor_pimpl.h"
+#include "tensorflow/stream_executor/trace_listener.h"
+
+namespace se = stream_executor;
+
+using int64 = tensorflow::int64;
+
+namespace tensorflow {
+class IpuTraceEvent;
+}
+
+namespace poplar {
+class Engine;
+}
+
+namespace xla {
+namespace poplarplugin {
+
+class PopItPlatform : public se::Platform {
+ public:
+  PopItPlatform();
+  ~PopItPlatform() override;
+
+  Platform::Id id() const override;
+
+  int VisibleDeviceCount() const override;
+
+  const std::string& Name() const override;
+
+  StatusOr<se::StreamExecutor*> ExecutorForDevice(int ordinal) override;
+
+  StatusOr<se::StreamExecutor*> ExecutorForDeviceWithPluginConfig(
+      int ordinal, const se::PluginConfig& config) override;
+
+  StatusOr<se::StreamExecutor*> GetExecutor(
+      const se::StreamExecutorConfig& config) override;
+
+  StatusOr<std::unique_ptr<se::StreamExecutor>> GetUncachedExecutor(
+      const se::StreamExecutorConfig& config) override;
+
+  void RegisterTraceListener(std::unique_ptr<se::TraceListener>) override;
+
+  void UnregisterTraceListener(se::TraceListener* listener) override;
+
+  StatusOr<std::unique_ptr<se::DeviceDescription>> DescriptionForDevice(
+      int ordinal) const override;
+
+ private:
+  // This platform's name.
+  std::string name_;
+
+  // Cache of created StreamExecutors.
+  se::ExecutorCache executor_cache_;
+
+  SE_DISALLOW_COPY_AND_ASSIGN(PopItPlatform);
+};
+
+}  // namespace poplarplugin
+}  // namespace xla
+
+#endif  // TENSORFLOW_COMPILER_PLUGIN_POPLAR_DRIVER_POPIT_BACKEND_POPIT_PLATFORM_H_
