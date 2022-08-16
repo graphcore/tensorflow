0e3e839dcbd77a753fb990adb84ec1b48269f47f
Frederik Mellbye <frederikm@graphcore.ai>
2022-08-16 09:21:54 +0100

    Remove IPUMultiWorkerExtended inheritance from PopDistExtended (TF1)
    Summary: TF1.15 Only
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, christiana
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, christiana
    
    Maniphest Tasks: T65016
    
    Differential Revision: https://phabricator.sourcevertex.net/D73286

--- tensorflow/python/ipu/distributed/popdist_strategy.py
+++ tensorflow/python/ipu/distributed/popdist_strategy.py
@@ -14,15 +14,19 @@
 # ==============================================================================
 import popdist
 
+from tensorflow.core.framework import attr_value_pb2
+from tensorflow.python.distribute import collective_all_reduce_strategy
+from tensorflow.python.distribute import cross_device_ops as cross_device_ops_lib
 from tensorflow.python.distribute import device_util
 from tensorflow.python.distribute import distribute_lib
 from tensorflow.python.distribute import reduce_util
 from tensorflow.python.distribute import values
 from tensorflow.python.distribute.cluster_resolver import cluster_resolver as cluster_resolver_lib
 from tensorflow.python.framework import device as tf_device
-from tensorflow.python.ipu.ipu_multi_worker_strategy import \
-    IPUMultiWorkerExtended
+from tensorflow.python.framework import ops
 from tensorflow.python.ipu.ops import cross_replica_ops
+from tensorflow.python.ops import variable_scope
+from tensorflow.python.util import tf_contextlib
 from tensorflow.python.training import server_lib
 from tensorflow.compiler.plugin.poplar.ops import gen_poputil_ops
 from tensorflow.python.ipu.horovod import Sum, Average, \
@@ -83,15 +87,155 @@ class PopDistStrategy(distribute_lib.StrategyV1):
       popdist.getInstanceIndex()
 
 
-class PopDistExtendedV1(IPUMultiWorkerExtended):
+def _is_inside_compilation():
+  graph = ops.get_default_graph()
+  attrs = graph._attr_scope_map  # pylint: disable=protected-access
+
+  is_in_xla_context = control_flow_util.GraphOrParentsInXlaContext(graph)
+  is_outside_compilation = scopes.OUTSIDE_COMPILATION_NAME in attrs
+
+  return is_in_xla_context and not is_outside_compilation
+
+
+@tf_contextlib.contextmanager
+def _outside_compilation_scope_if_needed(name):
+  if _is_inside_compilation():
+    with scopes.outside_compilation_scope(name):
+      yield
+  else:
+    yield
+
+
+def _ipu_device_for_host(ipu_device_string, host_device_string):
+  ipu_device = tf_device.DeviceSpec.from_string(ipu_device_string)
+  host_device = tf_device.DeviceSpec.from_string(host_device_string)
+
+  # Take distributed info from the host and device info from the IPU.
+  ipu_for_host = tf_device.DeviceSpec(job=host_device.job,
+                                      replica=host_device.replica,
+                                      task=host_device.task,
+                                      device_type=ipu_device.device_type,
+                                      device_index=ipu_device.device_index)
+
+  return ipu_for_host.to_string()
+
+
+def _make_identity_op(v):
+  name = v.name.replace(":", "_")
+  return array_ops.identity(v, name=name)
+
+
+class IPUSyncOnReadVariable(values.SyncOnReadVariable):  # pylint: disable=abstract-method
+  pass
+
+
+class IPUMirroredVariable(values.MirroredVariable):  # pylint: disable=abstract-method
+  pass
+
+
+class PopDistExtendedV1(
+    collective_all_reduce_strategy.CollectiveAllReduceExtended):
   def __init__(self, container_strategy, cluster_resolver, ipu_device,
                add_ipu_cross_replica_reductions):
-    super().__init__(container_strategy,
-                     cluster_resolver,
-                     ipu_device,
-                     variables_on_host=False)
+
+    super().__init__(
+        container_strategy,
+        communication=cross_device_ops_lib.CollectiveCommunication.RING,
+        cluster_resolver=cluster_resolver)
+
     self._num_workers = popdist.getNumInstances()
     self._add_ipu_cross_replica_reductions = add_ipu_cross_replica_reductions
+
+    host_devices = self._device_map.all_devices
+    if len(host_devices) != 1:
+      raise ValueError("Expected one host device per worker")
+
+    self._host_device = host_devices[0]
+    self._ipu_device = _ipu_device_for_host(ipu_device, self._host_device)
+    self._variables_on_host = False
+
+    self._variable_device = self._ipu_device
+
+  def _get_variable_creator_initial_value(self, replica_id, device,
+                                          primary_var, **kwargs):
+    assert replica_id == 0
+    assert device is not None
+    assert primary_var is None
+
+    def initial_value_fn():  # pylint: disable=g-missing-docstring
+      # Override colocation and XLA attributes for initializers.
+      colocation_list = attr_value_pb2.AttrValue.ListValue(s=[b'loc:@cpu'])
+      attrs = {
+          "_class": attr_value_pb2.AttrValue(list=colocation_list),
+          "_XlaCompile": attr_value_pb2.AttrValue(b=False),
+          "_XlaScope": attr_value_pb2.AttrValue(s=b''),
+      }
+      with ops.device(device), ops.get_default_graph()._attr_scope(attrs):  # pylint: disable=protected-access
+        initial_value = kwargs["initial_value"]
+        if callable(initial_value):
+          initial_value = initial_value()
+        assert not callable(initial_value)
+        initial_value = ops.convert_to_tensor(initial_value,
+                                              dtype=kwargs.get("dtype", None))
+        return self._broadcast_implementation(initial_value, device)
+
+    return initial_value_fn
+
+  def _create_variable(self, next_creator, *args, **kwargs):
+    colocate_with = kwargs.pop("colocate_with", None)
+    if colocate_with is None:
+      device_map = values.ReplicaDeviceMap([self._variable_device])
+      logical_device = 0
+    elif isinstance(colocate_with, numpy_dataset.SingleDevice):
+      with ops.device(colocate_with.device):
+        return next_creator(*args, **kwargs)
+    else:
+      device_map = colocate_with.device_map
+      logical_device = colocate_with.logical_device
+
+    def _real_creator(devices, *args, **kwargs):
+      assert len(devices) == 1
+      assert devices[0] == self._variable_device
+
+      # The chief worker will initialize and broadcast the value to
+      # the other workers. Always done on the host.
+      kwargs["initial_value"] = self._get_variable_creator_initial_value(
+          replica_id=0,  # First (and only) replica on each worker.
+          device=self._host_device,
+          primary_var=None,
+          **kwargs)
+
+      # We always place sync-on-read variables on the IPU. They will
+      # be transfered and reduced on the hosts only when read.
+      synchronization = kwargs.get("synchronization")
+      if (not self._variables_on_host or
+          synchronization == variable_scope.VariableSynchronization.ON_READ):
+        with ops.device(self._ipu_device):
+          return [next_creator(*args, **kwargs)]
+
+      # Cache a snapshot of the variable on the IPU device,
+      # otherwise the XLA cluster containing the ops consuming the
+      # variable might be moved to the host to be colocated with it.
+      kwargs["caching_device"] = self._ipu_device
+
+      # In case we are inside an ipu_jit_scope, we need to override it
+      # to disable XLA for variable initialization on the host.
+      disable_xla = {
+          "_XlaCompile": attr_value_pb2.AttrValue(b=False),
+          "_XlaScope": attr_value_pb2.AttrValue(s=b''),
+      }
+
+      graph = ops.get_default_graph()
+      with ops.device(self._host_device), graph._attr_scope(disable_xla):  # pylint: disable=protected-access
+        return [next_creator(*args, **kwargs)]
+
+    # For tf2: use values.create_mirrored_variable
+    return distribute_lib.create_mirrored_variable(
+        self._container_strategy(), device_map, logical_device, _real_creator,
+        IPUMirroredVariable, IPUSyncOnReadVariable, *args, **kwargs)
+
+  def read_var(self, var):
+    return var.read_value()
 
   def _reduce_to(self, reduce_op, value, destinations):
     del destinations
@@ -122,6 +266,25 @@ class PopDistExtendedV1(IPUMultiWorkerExtended):
         self.reduce_to(reduce_op, t, destinations=v)
         for t, v in value_destination_pairs
     ]
+
+  def _call_for_each_replica(self, fn, args, kwargs):
+    with distribute_lib.ReplicaContext(
+        self._container_strategy(), replica_id_in_sync_group=0), \
+        ops.device(self._ipu_device):
+      return fn(*args, **kwargs)
+
+  def _validate_colocate_with_variable(self, colocate_with_variable):
+    if colocate_with_variable.device != self._variable_device:
+      raise ValueError("Unexpected colocated variable device: {}".format(
+          colocate_with_variable.device))
+
+  def _reduce_implementation(self, reduce_op, value, destinations):
+    # This is an extension point for overriding, try to keep a stable API.
+    return super()._reduce_to(reduce_op, value, destinations)
+
+  def _batch_reduce_implementation(self, reduce_op, value_destination_pairs):
+    # This is an extension point for overriding, try to keep a stable API.
+    return super()._batch_reduce_to(reduce_op, value_destination_pairs)
 
   def _broadcast_implementation(self, initial_value, device):
     if tf_device.DeviceSpec.from_string(device).device_type != "CPU":
--- tensorflow/python/ipu/distributed/popdist_strategy_test.py
+++ tensorflow/python/ipu/distributed/popdist_strategy_test.py
@@ -17,20 +17,27 @@ import popdist
 
 from tensorflow.python import ipu
 from tensorflow.python.client import session
+from tensorflow.python.data.ops import dataset_ops
 from tensorflow.python.distribute import distribution_strategy_context
 from tensorflow.python.distribute.reduce_util import ReduceOp
 from tensorflow.python.framework import constant_op
+from tensorflow.python.framework import ops
 from tensorflow.python.framework import test_util
 from tensorflow.python.ipu import horovod as hvd
-from tensorflow.python.ipu.horovod import popdist_strategy
-from tensorflow.python.ipu.ipu_multi_worker_strategy import IPUSyncOnReadVariable
+from tensorflow.python.ipu import ipu_compiler
+from tensorflow.python.ipu import ipu_infeed_queue
+from tensorflow.python.ipu import loops
+from tensorflow.python.ipu.distributed import popdist_strategy
+from tensorflow.python.ipu.scopes import ipu_scope
+from tensorflow.python.keras.layers.normalization import BatchNormalization
 from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import variable_scope
 from tensorflow.python.ops import variables
 from tensorflow.python.platform import test
+from tensorflow.python.training.monitored_session import MonitoredTrainingSession
 
 
-class IPUMultiReplicaStrategyTest(test_util.TensorFlowTestCase):  # pylint: disable=abstract-method
+class PopDistStrategyTest(test_util.TensorFlowTestCase):  # pylint: disable=abstract-method
   @classmethod
   def setUpClass(cls):
     hvd.init()
@@ -46,10 +53,10 @@ class IPUMultiReplicaStrategyTest(test_util.TensorFlowTestCase):  # pylint: disa
     strategy.update_ipu_config(config)
     self.assertEqual(
         config.experimental.multi_replica_distribution.process_count,
-        hvd.size())
+        popdist.getNumInstances())
     self.assertEqual(
         config.experimental.multi_replica_distribution.process_index,
-        hvd.rank())
+        popdist.getInstanceIndex())
 
   @test_util.deprecated_graph_mode_only
   def test_strategy(self):
@@ -57,7 +64,8 @@ class IPUMultiReplicaStrategyTest(test_util.TensorFlowTestCase):  # pylint: disa
 
     with strategy.scope():
 
-      v = variables.Variable(initial_value=hvd.rank() + 1, dtype=np.float32)
+      v = variables.Variable(initial_value=popdist.getInstanceIndex() + 1,
+                             dtype=np.float32)
       self.assertEndsWith(v.device, "/device:IPU:0")
 
       def per_replica_fn(x):
@@ -94,7 +102,8 @@ class IPUMultiReplicaStrategyTest(test_util.TensorFlowTestCase):  # pylint: disa
         self.assertEqual(sess.run(v), 1.0)
 
         # There should be one allreduce sum of the values.
-        self.assertEqual(sess.run(value_allreduced), hvd.size() * 2.0)
+        self.assertEqual(sess.run(value_allreduced),
+                         popdist.getNumInstances() * 2.0)
 
   @test_util.deprecated_graph_mode_only
   def test_strategy_without_ipu_reduction(self):
@@ -129,10 +138,10 @@ class IPUMultiReplicaStrategyTest(test_util.TensorFlowTestCase):  # pylint: disa
       def per_replica_fn(x):
         w0 = variable_scope.get_variable(
             name="w0",
-            initializer=float(hvd.rank() + 1),
+            initializer=float(popdist.getInstanceIndex() + 1),
             synchronization=variable_scope.VariableSynchronization.ON_READ,
             aggregation=variable_scope.VariableAggregation.MEAN)
-        self.assertIsInstance(w0, IPUSyncOnReadVariable)
+        self.assertIsInstance(w0, popdist_strategy.IPUSyncOnReadVariable)
         return w0.assign_add(x)
 
       inputs = array_ops.placeholder(dtype=np.float32, shape=())
@@ -146,9 +155,160 @@ class IPUMultiReplicaStrategyTest(test_util.TensorFlowTestCase):  # pylint: disa
         sess.run(variables.global_variables_initializer())
         # Both should have initial value from first worker
         self.assertEqual([1.0], sess.run(variables.global_variables()))
-        sess.run(assign_add_op, feed_dict={inputs: hvd.rank() + 1})
+        sess.run(assign_add_op,
+                 feed_dict={inputs: popdist.getInstanceIndex() + 1})
         # mean(1 + 1, 1 + 2) = 2.5
         self.assertEqual([2.5], sess.run(variables.global_variables()))
+
+  @test_util.deprecated_graph_mode_only
+  def test_all_reduce(self):
+    strategy = popdist_strategy.PopDistStrategy()
+
+    with strategy.scope():
+
+      def per_replica_fn(x):
+        return x * x
+
+      inputs = array_ops.placeholder(dtype=np.float32, shape=())
+      per_replica_y = strategy.experimental_run_v2(per_replica_fn,
+                                                   args=[inputs])
+      sum_y = strategy.reduce(ReduceOp.SUM, per_replica_y, axis=None)
+
+      with session.Session() as sess:
+        config = ipu.config.IPUConfig()
+        config.auto_select_ipus = 1
+        config.configure_ipu_system()
+        sess.run(variables.global_variables_initializer())
+        out = sess.run(sum_y,
+                       feed_dict={inputs: popdist.getInstanceIndex() + 1})
+        self.assertEqual(5.0, out)  # 1*1 + 2*2
+
+  @test_util.deprecated_graph_mode_only
+  def test_mirrored_variable(self):
+    strategy = popdist_strategy.PopDistStrategy()
+
+    with strategy.scope():
+
+      def per_replica_fn():
+        with ops.device("/device:IPU:0"):
+          w0 = variable_scope.get_variable(
+              name="w0", initializer=popdist.getInstanceIndex() + 1)
+          self.assertIsInstance(w0, popdist_strategy.IPUMirroredVariable)
+          ret = w0 * w0
+          return ret
+
+      per_replica_ret = strategy.experimental_run_v2(per_replica_fn, args=[])
+      sum_ret = strategy.reduce(ReduceOp.SUM, per_replica_ret, axis=None)
+
+      with session.Session() as sess:
+        config = ipu.config.IPUConfig()
+        config.auto_select_ipus = 1
+        config.configure_ipu_system()
+        sess.run(variables.global_variables_initializer())
+        # Both should have initial value from first worker
+        self.assertEqual([1.0], sess.run(variables.global_variables()))
+        self.assertEqual(2.0, sess.run(sum_ret))  # 1*1 + 1*1
+
+  @test_util.deprecated_graph_mode_only
+  def test_monitored_training_session(self):
+    config = ipu.config.IPUConfig()
+    config.auto_select_ipus = 1
+    config.configure_ipu_system()
+
+    strategy = popdist_strategy.PopDistStrategy()
+
+    with strategy.scope():
+
+      def step_fn(x):
+        w = variable_scope.get_variable("w", initializer=2.0)
+        y = w * x
+        return y
+
+      inputs = array_ops.placeholder(dtype=np.float32, shape=())
+      per_replica_y = strategy.experimental_run_v2(step_fn, args=[inputs])
+      sum_y = strategy.reduce(ReduceOp.SUM, per_replica_y, axis=None)
+
+      with MonitoredTrainingSession() as sess:
+        out = sess.run(sum_y,
+                       feed_dict={inputs: popdist.getInstanceIndex() + 1})
+        self.assertEqual(6.0, out)  # 2*1 + 2*2
+
+  @test_util.deprecated_graph_mode_only
+  def test_batch_normalization(self):
+    config = ipu.config.IPUConfig()
+    config.auto_select_ipus = 1
+    config.configure_ipu_system()
+
+    strategy = popdist_strategy.PopDistStrategy()
+
+    with strategy.scope():
+      batch_norm = BatchNormalization(momentum=0.0)
+
+      def per_replica_fn(x):
+        y = batch_norm(x, training=True)
+
+        self.assertIsInstance(batch_norm.beta,
+                              popdist_strategy.IPUMirroredVariable)
+        self.assertIsInstance(batch_norm.gamma,
+                              popdist_strategy.IPUMirroredVariable)
+
+        self.assertIsInstance(batch_norm.moving_mean,
+                              popdist_strategy.IPUSyncOnReadVariable)
+        self.assertIsInstance(batch_norm.moving_variance,
+                              popdist_strategy.IPUSyncOnReadVariable)
+
+        return y
+
+      def compiled_per_replica_fn(inputs):
+        [out] = ipu_compiler.compile(per_replica_fn, inputs=[inputs])
+        return out
+
+      inputs = array_ops.placeholder(dtype=np.float32, shape=(2, 1))
+      per_replica_y = strategy.experimental_run_v2(compiled_per_replica_fn,
+                                                   args=[inputs])
+      sum_y = strategy.reduce(ReduceOp.SUM, per_replica_y, axis=None)
+
+      with session.Session() as sess:
+        sess.run(variables.global_variables_initializer())
+        sess.run(sum_y,
+                 feed_dict={
+                     inputs: [[2.0 * (popdist.getInstanceIndex() + 1)], [0.0]]
+                 })
+
+        # mean(mean(2, 0), mean(4, 0)) = mean(1, 3) = 1.5
+        global_mean = batch_norm.moving_mean
+        self.assertAllEqual([1.5], sess.run(global_mean))
+
+        # mean(var(2, 0), var(4, 0)) = mean(1, 4) = 2.5
+        self.assertAllEqual([2.5], sess.run(batch_norm.moving_variance))
+
+  @test_util.deprecated_graph_mode_only
+  def test_dataset_infeed(self):
+    config = ipu.config.IPUConfig()
+    config.auto_select_ipus = 1
+    config.configure_ipu_system()
+    strategy = popdist_strategy.PopDistStrategy()
+
+    with strategy.scope():
+      dataset = dataset_ops.Dataset.from_tensor_slices([0.0]).repeat()
+      # Test with a dataset host op.
+      dataset = dataset.map(lambda x: x + popdist.getInstanceIndex())
+      infeed_queue = ipu_infeed_queue.IPUInfeedQueue(dataset)
+
+      def body(v, x):
+        v += x
+        return v
+
+      def my_net():
+        r = loops.repeat(10, body, [0.0], infeed_queue)
+        return r
+
+      with ipu_scope("/device:IPU:0"):
+        [res] = ipu_compiler.compile(my_net, inputs=[])
+
+      with session.Session() as sess:
+        sess.run(infeed_queue.initializer)
+        self.assertEqual(popdist.getInstanceIndex() * 10.0, sess.run(res))
 
 
 if __name__ == "__main__":
