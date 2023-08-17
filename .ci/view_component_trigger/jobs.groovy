e0c4bcb27d761b39a6f0671ae50b13c189e1c35a
Tim Van Ellemeet <timv@graphcore.ai>
2023-08-17 14:48:03 +0100

    github-migration/poplar/r1.15/release (#1)
    'Auto Generated PR for github-migration/poplar/r1.15/release. See
    https://graphcore.atlassian.net/l/cp/qD91gp3B'
    
    ---------
    
    Co-authored-by: Dan Hewitt <danh@graphcore.ai>

---
+++ .ci/view_component_trigger/jobs.groovy
@@ -0,0 +1,4 @@
+[
+    [job: '/tensorflow/tensorflow_pr', parameters: [string(name: 'GCCI_BRANCH', value: 'poplar/r1/release')]],
+    [job: '/tensorflow_serving/tensorflow_serving_pr', parameters: [string(name: 'GCCI_BRANCH', value: 'poplar/r1/release')]],
+]
